60e85cb9f55d54175d53a8727618d993a8ff86d0
Jose Valerio <contact@joswayski.com>
2021-08-22 06:58:26 -0400

    fix: Removed syntax errors preventing deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -540,7 +540,7 @@ export default function Example() {
                             <path
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                              stroke-width="2"
+                              strokeWidth="2"
                               d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                             ></path>
                           </svg>
@@ -578,9 +578,9 @@ export default function Example() {
                           aria-hidden="true"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
                           ></path>
                         </svg>
@@ -610,9 +610,9 @@ export default function Example() {
                             aria-hidden="true"
                           >
                             <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
                             ></path>
                           </svg>
