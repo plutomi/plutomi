e10ff5a5ba1b72ff5f8cf0462b08bf477b91f70f
Jose Valerio <contact@joswayski.com>
2021-08-23 05:43:33 -0400

    refactor(sls-env-variables): added the domain name to an environment variable

--- next.config.js
+++ next.config.js
@@ -6,5 +6,6 @@ module.exports = {
     DEFAULT_LAMBDA_NAME: process.env.DEFAULT_LAMBDA_NAME,
     API_LAMBDA_NAME: process.env.API_LAMBDA_NAME,
     IMAGE_LAMBDA_NAME: process.env.IMAGE_LAMBDA_NAME,
+    DOMAIN_NAME: process.env.DOMAIN_NAME,
   },
 };
