dcf9aecd9a9b3da04839d98f3c254eb0a2891153
Jose Valerio <contact@joswayski.com>
2021-08-23 05:40:52 -0400

    refactor(sls-env-variables): switched to using environment variables for lambda names in sls .yml
    n

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   env: {
     WEBSITE_BUCKET_NAME: process.env.WEBSITE_BUCKET_NAME,
     CLOUDFRONT_ID: process.env.CLOUDFRONT_ID,
+    DEFAULT_LAMBDA_NAME: process.env.DEFAULT_LAMBDA_NAME,
+    API_LAMBDA_NAME: process.env.API_LAMBDA_NAME,
+    IMAGE_LAMBDA_NAME: process.env.IMAGE_LAMBDA_NAME,
   },
 };
