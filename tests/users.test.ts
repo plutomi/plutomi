c13350d9d1f0db0e598e2ad275ca9ca7282c1491
Jose Valerio <contact@joswayski.com>
2021-08-24 04:41:31 -0400

    Changed GSI1SK to use name so we can do sorting by name on Dynamo

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -34,16 +34,23 @@ export default function Users() {
       user_role,
       user_id,
       is_sub_user,
+      org_join_date,
+      GSI1PK,
+      GSI1SK,
     }: NewUserOutput = data.user;
 
     expect(PK).toMatch(`USER#`);
     expect(PK).toHaveLength(ID_LENGTH + 5); // Accounting for USER#
     expect(SK).toBe(PK);
+
+    expect(GSI1PK).toBe(`ORG#NO_ORG_ASSIGNED`);
+    expect(GSI1SK).toBe(`USER#${name}`); // Allows for 'Get all users in an org sorted by name
     expect(name).toBe(new_user.name);
     expect(email).toBe(new_user.email);
     expect(entity_type).toBe("USER");
     expect(created_at).toContain(dayjs().toISOString().slice(0, 9)); // Today's date, YYYY-MM-DD
     expect(org).toBe("NO_ORG_ASSIGNED");
+    expect(org_join_date).toBe("NO_ORG_ASSIGNED");
     expect(user_role).toBe("BASIC");
     expect(user_id).toHaveLength(ID_LENGTH);
     expect(is_sub_user).toBe(false);
