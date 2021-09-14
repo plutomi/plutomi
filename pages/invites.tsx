1ab66a0549666788f060fa8b901c0e987602fddf
Jose Valerio <contact@joswayski.com>
2021-09-14 05:33:28 -0400

    chore: Added basic invites page

--- pages/invites.tsx
+++ pages/invites.tsx
@@ -1,10 +1,14 @@
 import { useSession } from "next-auth/client";
-import SignIn from "../components/SignInHomepage";
+import SignIn from "../components/SignIn";
 import useUser from "../SWR/useUser";
 import useOrgInvites from "../SWR/useOrgInvites";
 import { GetRelativeTime } from "../utils/time";
 import axios from "axios";
 import { useSWRConfig } from "swr";
+
+import UserProfileCard from "../components/UserProfileCard";
+import SignedInNav from "../components/Navbar/SignedInNav";
+
 export default function Invites() {
   const { mutate } = useSWRConfig();
   const [session]: CustomSession = useSession();
@@ -12,10 +16,6 @@ export default function Invites() {
   const { invites, isInvitesLoading, isInvitesError } = useOrgInvites(
     session?.user_id
   );
-
-  if (!session) {
-    return <SignIn />;
-  }
 
   const acceptInvite = async (invite) => {
     try {
@@ -39,43 +39,73 @@ export default function Invites() {
   };
 
   return (
-    <ul
-      role="list"
-      className="divide-y divide-gray-200 mx-auto max-w-xl flex-col space-y-4 p-20 border rounded-md shadow-md"
-    >
-      {invites?.length > 0 && user ? (
-        invites.map((invite: CreateOrgInviteInput) => (
-          <li
-            key={invite.expires_at}
-            className="py-4 flex border rounded-md w-full"
-          >
-            <div className="ml-3">
-              <p className="text-sm font-medium text-blue-gray-900">
-                {invite.invited_by.first_name} {invite.invited_by.last_name}
-              </p>
-              <p className="text-sm font-medium text-blue-gray-500">
-                {invite.invited_by.user_email}
-              </p>
-              <p className="text-sm text-blue-gray-500">
-                Expires {GetRelativeTime(invite.expires_at)}
-              </p>
-              <p className="text-sm text-blue-gray-500">
-                Org ID: {invite.org_id}
-              </p>
-            </div>
-            <button
-              onClick={() => acceptInvite(invite)}
-              className="px-4 py-3 border border-transparent bg-blue-gray-900 text-white rounded-md m-4"
-            >
-              Accept
-            </button>
-          </li>
-        ))
+    <>
+      {isUserLoading ? (
+        <p className="mx-auto text-center text-blue-gray-600 text-lg">
+          Loading user... {/* Loading skeleton PLACEHOLDER */}
+        </p>
+      ) : !session || isUserError ? (
+        <SignIn
+          callbackUrl={`${process.env.NEXTAUTH_URL}/invites`} // Actual URL
+          desiredPage={"your invites"} // 'your dashboard' // 'your settings'
+        />
       ) : (
-        <h1 className="mt-4 text-lg font-bold">
-          You don&apos;t have any invites
-        </h1>
+        <div className="min-h-screen bg-white">
+          <SignedInNav current={"PLACEHOLDER"} user={user}/>
+          <div className="py-10">
+            <header>
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                {/* Content */}
+              </div>
+            </header>
+            <UserProfileCard user={user} /> {/* Debugging */}
+            <main>
+              <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                {/* Content */}
+                <ul
+                  role="list"
+                  className="divide-y divide-gray-200 mx-auto max-w-xl flex-col space-y-4 p-20 border rounded-md shadow-md"
+                >
+                  {invites?.length > 0 && user ? (
+                    invites.map((invite: CreateOrgInviteInput) => (
+                      <li
+                        key={invite.expires_at}
+                        className="py-4 flex border rounded-md w-full"
+                      >
+                        <div className="ml-3">
+                          <p className="text-sm font-medium text-blue-gray-900">
+                            {invite.invited_by.first_name}{" "}
+                            {invite.invited_by.last_name}
+                          </p>
+                          <p className="text-sm font-medium text-blue-gray-500">
+                            {invite.invited_by.user_email}
+                          </p>
+                          <p className="text-sm text-blue-gray-500">
+                            Expires {GetRelativeTime(invite.expires_at)}
+                          </p>
+                          <p className="text-sm text-blue-gray-500">
+                            Org ID: {invite.org_id}
+                          </p>
+                        </div>
+                        <button
+                          onClick={() => acceptInvite(invite)}
+                          className="px-4 py-3 border border-transparent bg-blue-gray-900 text-white rounded-md m-4"
+                        >
+                          Accept
+                        </button>
+                      </li>
+                    ))
+                  ) : (
+                    <h1 className="mt-4 text-lg font-bold">
+                      You don&apos;t have any invites
+                    </h1>
+                  )}
+                </ul>
+              </div>
+            </main>
+          </div>
+        </div>
       )}
-    </ul>
+    </>
   );
 }
