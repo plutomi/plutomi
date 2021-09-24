a748ea583793844f196aa0011d7c7b509f8bf413
Jose Valerio <contact@joswayski.com>
2021-09-24 05:35:53 -0400

    chore: Removed some console logs

--- pages/api/orgs/[org_id]/openings.ts
+++ pages/api/orgs/[org_id]/openings.ts
@@ -13,11 +13,9 @@ const handler = async (req: CustomRequest, res: NextApiResponse) => {
   if (method === "GET") {
     try {
       const all_openings = await GetAllOpeningsInOrg(org_id as string);
-      console.log("AllOpeings", all_openings);
       const only_public = all_openings.filter(
         (opening): DynamoOpening => opening.is_public
       );
-      console.log("PUBLIC", only_public);
 
       return res.status(200).json(only_public);
     } catch (error) {
