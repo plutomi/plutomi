ed9064f1f343dd525c7022efea171673bffcbbf0
Jose Valerio <contact@joswayski.com>
2021-09-28 06:18:41 -0400

    Added some comments on vars to set when creating new pages

--- components/Navbar/SignedInNav.tsx
+++ components/Navbar/SignedInNav.tsx
@@ -75,7 +75,7 @@ export default function SignedInNav({ current, user }: ValidNavigation) {
                             current === item.name
                               ? "border-indigo-500 text-gray-900"
                               : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                            "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                            "inline-flex items-center px-1 pt-1 border-b-2 text-md font-medium"
                           )}
                           aria-current={
                             current === item.name ? "page" : undefined
@@ -99,7 +99,7 @@ export default function SignedInNav({ current, user }: ValidNavigation) {
                   {/* Profile dropdown */}
                   <Menu as="div" className="ml-3 relative">
                     <div>
-                      <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      <Menu.Button className="max-w-xs bg-white flex items-center text-md rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <span className="sr-only">Open user menu</span>
                         {/* <img
                         className="h-8 w-8 rounded-full"
@@ -132,7 +132,7 @@ export default function SignedInNav({ current, user }: ValidNavigation) {
                                 onClick={item.event ? item.event : null}
                                 className={classNames(
                                   active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-gray-700"
+                                  "block px-4 py-2 text-md text-gray-700"
                                 )}
                               >
                                 {item.name}
@@ -189,7 +189,7 @@ export default function SignedInNav({ current, user }: ValidNavigation) {
                     <div className="text-base font-medium text-gray-800">
                       {user.first_name}
                     </div>
-                    <div className="text-sm font-medium text-gray-500">
+                    <div className="text-md font-medium text-gray-500">
                       {user.user_email}
                     </div>
                   </div>
--- pages/NewPage.tsx
+++ pages/NewPage.tsx
@@ -1,3 +1,4 @@
+// Template for creating new pages
 import Layout from "../components/Layout";
 import GoBack from "../components/GoBackButton";
 import SignedInNav from "../components/Navbar/SignedInNav";
@@ -24,13 +25,14 @@ export default function NewPage() {
   if (!session || isUserError) {
     return (
       <SignIn
-        callbackUrl={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/dashboard`}
-        desiredPage={"your openings"}
+        callbackUrl={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/dashboard`} // TODO set this
+        desiredPage={"your openings"} // TODO set this
       />
     );
   }
 
   if (isUserLoading) {
+    // TODO set this
     return (
       <div className="mx-auto p-20 flex justify-center items-center">
         <h1 className="text-4xl text-dark font-medium">Loading...</h1>
@@ -47,9 +49,15 @@ export default function NewPage() {
 
   return (
     <>
+      {" "}
+      {/* SET THIS */}
       <SignedInNav current="PLACEHOLDER" user={user ? user : null} />
       <GoBack />
-      <Layout header={"New Page"} main={<div>Testing</div>} />
+      <Layout
+        headerText={"New Page"}
+        main={<div>Testing</div>}
+        footer={<h1>Footer</h1>}
+      />
     </>
   );
 }
