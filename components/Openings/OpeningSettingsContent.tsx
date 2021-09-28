2d90889f6f0b67d7e70826f9dfafbf115f9194d5
Jose Valerio <contact@joswayski.com>
2021-09-28 14:49:19 -0400

    Added layout for opening settings

--- components/Openings/OpeningSettingsContent.tsx
+++ components/Openings/OpeningSettingsContent.tsx
@@ -32,14 +32,33 @@ export default function OpeningSettingsContent() {
 
   return (
     <>
-      {stages?.map((stage: DynamoStage) => (
-        <div
-          key={stage?.stage_id}
-          className="my-4 p-4 text-md shadow-md border max-w-md rounded-md"
-        >
-          {stage?.GSI1SK}
+      {/* 3 column wrapper */}
+      <div className="flex-grow w-full border mx-auto xl:px-8 lg:flex">
+        {/* Left sidebar & main wrapper */}
+        <div className="flex-1 min-w-0 bg-white xl:flex">
+          <div className="border-b border-gray-200 xl:border-b-0 xl:flex-shrink-0 xl:w-64 xl:border-r xl:border-gray-200 bg-white">
+            <div className="h-full pl-4 pr-6 py-6 sm:pl-6 lg:pl-8 xl:pl-0">
+              {/* Start left column area */}
+              <div className="h-full relative" style={{ minHeight: "12rem" }}>
+                <div className="absolute inset-0 border-2 border-gray-200 border-dashed rounded-lg" />
+              </div>
+              {/* End left column area */}
+            </div>
+          </div>
+
+          <div className="bg-white lg:min-w-0 lg:flex-1">
+            <div className="h-full py-6 px-4 sm:px-6 lg:px-8">
+              {/* Start main area*/}
+              <div className="relative h-full" style={{ minHeight: "36rem" }}>
+                <div className="absolute inset-0 border-2 border-gray-200 border-dashed rounded-lg" />
+              </div>
+              {/* End main area */}
+            </div>
+          </div>
         </div>
-      ))}
+
+  
+      </div>
     </>
   );
 }
