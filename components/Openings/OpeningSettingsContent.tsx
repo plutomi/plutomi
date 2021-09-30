9b02d5f41a401a837a0699b404302d09d0f0d91d
Jose Valerio <contact@joswayski.com>
2021-09-30 05:57:07 -0400

    Some styling around label

--- components/Openings/OpeningSettingsContent.tsx
+++ components/Openings/OpeningSettingsContent.tsx
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/client";
 import useOpenings from "../../SWR/useOpenings";
 import { useRouter } from "next/router";
 import { mutate } from "swr";
+import { GetRelativeTime } from "../../utils/time";
 import difference from "../../utils/getObjectDifference";
 import Link from "next/dist/client/link";
 import StageCard from "../Stages/StageCard";
@@ -233,29 +234,36 @@ export default function OpeningSettingsContent() {
               {/* Start main area*/}
               <div className="relative h-full" style={{ minHeight: "36rem" }}>
                 <div className=" inset-0  border-gray-200 rounded-lg">
-                  <h1 className="text-center text-2xl font-semibold mb-4">
-                    {opening?.GSI1SK} Settings
-                  </h1>
-                  <span
-                    className={`mt-2 inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium ${
-                      opening?.is_public
-                        ? "bg-green-100"
-                        : "bg-blue-gray-100"
-                    }`}
-                  >
-                    <svg
-                      className={`-ml-0.5 mr-1.5 h-2 w-2 ${
-                        opening?.is_public
-                          ? "text-green-800"
-                          : "text-blue-gray-800"
-                      }`}
-                      fill="currentColor"
-                      viewBox="0 0 8 8"
-                    >
-                      <circle cx={4} cy={4} r={3} />
-                    </svg>
-                    {opening?.is_public ? "Public" : "Private"}
-                  </span>
+                  <div className="flex flex-col justify-center items-center">
+                    <h1 className="text-center text-2xl font-semibold mb-4">
+                      {opening?.GSI1SK} Settings
+                    </h1>
+                    <div className="flex justify-center space-x-4 py-2 items-center">
+                      <span
+                        className={` inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium ${
+                          opening?.is_public
+                            ? "bg-green-100"
+                            : "bg-blue-gray-100"
+                        }`}
+                      >
+                        <svg
+                          className={`-ml-0.5 mr-1.5 h-2 w-2 ${
+                            opening?.is_public
+                              ? "text-green-800"
+                              : "text-blue-gray-800"
+                          }`}
+                          fill="currentColor"
+                          viewBox="0 0 8 8"
+                        >
+                          <circle cx={4} cy={4} r={3} />
+                        </svg>
+                        {opening?.is_public ? "Public" : "Private"}
+                      </span>
+                      <p className="text-md text-light text-center">
+                        Created {GetRelativeTime(opening?.created_at)}
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
               {/* End main area */}
