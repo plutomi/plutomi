917b1a836cf8a6700b8ea710c3b00c69e03d7e8b
Jose Valerio <contact@joswayski.com>
2021-10-01 09:14:38 -0400

    Fixed application link

--- components/Openings/OpeningsList.tsx
+++ components/Openings/OpeningsList.tsx
@@ -77,7 +77,7 @@ export default function OpeningList() {
                       <p className="mt-2 flex items-center text-lg text-normal sm:mt-0 sm:ml-6">
                         <ClickToCopy
                           showText={"Application Link"}
-                          copyText={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/${user?.org_id}/openings/${opening?.opening_id}/apply`}
+                          copyText={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/${user?.org_id}/${opening?.opening_id}/apply`}
                         />
                       </p>
                     </div>
