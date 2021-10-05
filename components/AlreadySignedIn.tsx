c85f9907879a8047933c8c120cdea417f30dda14
Jose Valerio <contact@joswayski.com>
2021-10-05 10:27:32 -0400

    Fixed #93

--- components/AlreadySignedIn.tsx
+++ components/AlreadySignedIn.tsx
@@ -6,20 +6,20 @@ export default function AlreadySignedIn({ email }) {
 
   return (
     <section id="login" className="flex  justify-center mx-auto ">
-      <div className="mx-auto flex-col text-center space-y-2 md:space-y-0 md:flex justify-center space-x-2  items-center text-lg text-blue-gray-600 ">
+      <div className="mx-auto  flex-col md:flex-wrap text-center space-y-2 md:space-y-0  justify-center space-x-2  items-center text-lg text-blue-gray-600 ">
         <p className="px-2"> Signed in as {email}</p>
         {router.route === "/" && (
           <Link href="/dashboard">
             <a
               type="button"
-              className="px-4 py-2 bg-blue-gray-900 text-white rounded-md"
+              className="px-4 py-2 bg-normal hover:bg-dark  transition ease-in-out duration-200 text-white rounded-md"
             >
               Go to Dashboard &rarr;
             </a>
           </Link>
         )}
         <button
-          className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-dark bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          className=" items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-dark bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           onClick={() => signOut()}
         >
           Sign out
