4f18c88f6912614d5e283a89a8a73e31abdb782f
Jose Valerio <contact@josevalerio.com>
2021-10-26 02:31:58 -0400

    replaced openings and invites with the new templaet

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import NewPage from "../components/Templates/NewPage";
 import DashboardContent from "../components/Dashboard/DashboardContent";
-export default function NewDash() {
+export default function Dashboard() {
   return (
     <NewPage
       loggedOutPageText={"Log in to view your dashboard"}
