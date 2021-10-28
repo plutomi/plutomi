38785c603f941c1745084c643e022bea5ce300b3
Jose Valerio <contact@josevalerio.com>
2021-10-27 21:55:50 -0400

    fix: #208, grouped applicant responses by entity types

--- utils/applicants/getApplicantById.ts
+++ utils/applicants/getApplicantById.ts
@@ -7,42 +7,39 @@ import {
 } from "@aws-sdk/lib-dynamodb";
 import { Dynamo } from "../../libs/ddbDocClient";
 const { DYNAMO_TABLE_NAME } = process.env;
+import _ from "lodash";
 
-// Major refactor needs to be done here
-// TODO we should transact for each item type and query untill all items are returned
-// For example, get applicant metadata
-// Get applicant responses / files
-// Get applicant history
 export async function GetApplicantById({
   org_id,
   applicant_id,
 }: GetApplicantInput) {
-  const params: GetCommandInput = {
-    TableName: DYNAMO_TABLE_NAME,
-    Key: {
-      PK: `ORG#${org_id}#APPLICANT#${applicant_id}`,
-      SK: `APPLICANT`,
-    },
-  };
-
   const responsesParams: QueryCommandInput = {
     TableName: DYNAMO_TABLE_NAME,
     KeyConditionExpression: "PK = :PK AND begins_with(SK, :SK)",
     ExpressionAttributeValues: {
       ":PK": `ORG#${org_id}#APPLICANT#${applicant_id}`,
-      ":SK": `APPLICANT_RESPONSE`,
+      ":SK": `APPLICANT`,
     },
   };
 
   try {
     // TODO refactor for promise all / transact
-    const metadata = await Dynamo.send(new GetCommand(params));
-    const responses = await Dynamo.send(new QueryCommand(responsesParams));
-    const response = {
-      ...metadata.Item,
-      responses: responses.Items,
+    const all_applicant_info = await Dynamo.send(
+      new QueryCommand(responsesParams)
+    );
+
+    const grouped = _.groupBy(all_applicant_info.Items, "entity_type");
+
+    const metadata = grouped.APPLICANT[0];
+    const responses = grouped.APPLICANT_RESPONSE;
+    // TODO files
+
+    const applicant = {
+      ...metadata,
+      responses: responses,
+      // TODO files
     };
-    return response;
+    return applicant;
   } catch (error) {
     throw new Error(error);
   }
