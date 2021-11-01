467365a1ca4fa246d885a104e03a554225193376
Jose Valerio <contact@josevalerio.com>
2021-11-01 03:08:14 -0400

    fix: #238

--- pages/api/users/[user_id]/index.ts
+++ pages/api/users/[user_id]/index.ts
@@ -27,7 +27,7 @@ const handler = async (
       // Check that the user who made this call is in the same org as the requested user
       if (user_session.org_id != requested_user.org_id) {
         return res
-          .status(401)
+          .status(403)
           .json({ message: "You are not authorized to view this user" });
       }
 
