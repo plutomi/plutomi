1bfa26db3e9966d1ef8db01360c8122ac2b6043c
Jose Valerio <contact@joswayski.com>
2021-11-04 19:19:49 -0400

    Added basic sls cdk construct

--- bin/plutomi.ts
+++ bin/plutomi.ts
@@ -2,8 +2,19 @@
 import * as cdk from "@aws-cdk/core";
 import "source-map-support";
 
+import { Builder } from "@sls-next/lambda-at-edge";
+import { PlutomiFrontendStack } from "../lib/PlutomiFrontendStack";
 import PlutomiWebsiteStack from "../lib/PlutomiWebsiteStack";
 
-const app = new cdk.App();
+// Run the serverless builder, this could be done elsewhere in your workflow
+const builder = new Builder(".", "./build", { args: ["build"] });
 
-new PlutomiWebsiteStack(app, "PlutomiWebsiteStack");
+try {
+  builder.build(); // For SLS construct
+  const app = new cdk.App();
+  new PlutomiWebsiteStack(app, "PlutomiWebsiteStack");
+  new PlutomiFrontendStack(app, "PlutomiFrontendStack");
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
