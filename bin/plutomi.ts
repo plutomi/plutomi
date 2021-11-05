e8668311c1c1471df0132e23d1839d5f91ab26a1
Jose Valerio <contact@josevalerio.com>
2021-11-05 16:40:20 -0400

    Added custom domain at api.domainname.com

--- bin/plutomi.ts
+++ bin/plutomi.ts
@@ -2,32 +2,29 @@
 import * as cdk from "@aws-cdk/core";
 import "source-map-support";
 import { Builder } from "@sls-next/lambda-at-edge";
-import PlutomiWebsiteStack from "../lib/WebsiteStack";
 import { APIGatewayStack } from "../lib/APIGatewayStack";
 import { PublicInfoServiceStack } from "../lib/PublicInfoServiceStack";
+import { FrontendStack } from "../lib/FrontendStack";
 
 const app = new cdk.App();
 
-// Deploys the fargate website (TODO remove)
-new PlutomiWebsiteStack(app, "PlutomiWebsiteStack");
-
-// Creates API Gateway
-const API_GATEWAY = new APIGatewayStack(app, "APIGatewayStack");
-
-new PublicInfoServiceStack(app, "PublicInfoServiceStack", {
-  API: API_GATEWAY.API,
-});
-
 // Run the serverless builder, this could be done elsewhere in your workflow
-// const builder = new Builder(".", "./build", { args: ["build"] });
+const builder = new Builder(".", "./build", { args: ["build"] });
 
-// try {
-//   builder.build(); // For SLS construct
+try {
+  builder.build(); // For SLS construct
 
-//   // Deploys the website to cloudfront / S3
-//   // new PlutomiFrontendStack(app, "PlutomiFrontendStack");
+  // Deploys the website to cloudfront / S3
+  new FrontendStack(app, "FrontendStack");
 
-// } catch (error) {
-//   console.log(error);
-//   process.exit(1);
-// }
+  // Creates API Gateway
+  const API_GATEWAY = new APIGatewayStack(app, "APIGatewayStack");
+
+  // Deploys the /public route lambdas
+  new PublicInfoServiceStack(app, "PublicInfoServiceStack", {
+    API: API_GATEWAY.API,
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
