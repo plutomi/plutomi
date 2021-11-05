127b17d971ad7778020d0cd624f3ce9f7044bb86
Jose Valerio <contact@josevalerio.com>
2021-11-05 09:42:24 -0400

    Got API Gateway working

--- lib/PublicInfoServiceStack.ts
+++ lib/PublicInfoServiceStack.ts
@@ -4,7 +4,6 @@ import * as lambda from "@aws-cdk/aws-lambda";
 import * as cdk from "@aws-cdk/core";
 import * as path from "path";
 import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
-import { Architecture } from "@aws-cdk/aws-lambda";
 require("dotenv").config();
 
 interface PublicInfoServiceStackProps extends cdk.StackProps {
@@ -14,24 +13,26 @@ export class PublicInfoServiceStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: PublicInfoServiceStackProps) {
     super(scope, id, props);
 
+    const getPublicOpeningsFunction = new NodejsFunction(
+      this,
+      "public-info-service-get-public-openings",
+      {
+        memorySize: 256,
+        functionName: `public-info-service-get-public-openings`,
+        timeout: cdk.Duration.seconds(5),
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "main",
+        entry: path.join(__dirname, `/../functions/get-public-openings.ts`),
+        architecture: lambda.Architecture.ARM_64,
+      }
+    );
+
     props.API.addRoutes({
+      integration: new LambdaProxyIntegration({
+        handler: getPublicOpeningsFunction,
+      }),
       path: "/cdk",
       methods: [HttpMethod.GET],
-      integration: new LambdaProxyIntegration({
-        handler: new NodejsFunction(
-          this,
-          "public-info-service-get-public-openings",
-          {
-            memorySize: 256,
-            functionName: `public-info-service-get-public-openings`,
-            timeout: cdk.Duration.seconds(5),
-            runtime: lambda.Runtime.NODEJS_14_X,
-            handler: "main",
-            entry: path.join(__dirname, `/../functions/get-public-openings.ts`),
-            architecture: Architecture.ARM_64,
-          }
-        ),
-      }),
     });
   }
 }
