dea77e1f9c10be66e7266a41243f1504fda4ce97
mazupicua <mazupicua@gmail.com>
2021-11-10 13:14:32 -0500

    chore: created base dynamo mapping #237 (#268)
    * chore: created base dynamo mapping #237
    
    * hid vs code folder
    
    * removed unused packages

--- newTypes.d.ts
+++ newTypes.d.ts
@@ -1,11 +1,6 @@
-type CreateApplicantRequest = APIGatewayProxyEventV2 & {
-  body: {
-    orgId: string;
-    openingId: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-  };
-};
-
-type NextIronRequest = NextApiRequest & { session: Session };
+interface DynamoBase {
+  PK: string;
+  SK: string;
+  createdAt: Date;
+  orgId: string;
+}
