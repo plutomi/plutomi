080eda4b380aca2d23ed4d32ac729b28deea05c6
Jose Valerio <contact@josevalerio.com>
2021-11-10 12:27:03 -0500

    removed some console logs, fixed 259 and 265

--- middleware/withCleanOrgId.ts
+++ middleware/withCleanOrgId.ts
@@ -5,13 +5,8 @@ const UrlSafeString = require("url-safe-string"),
 // Cleans up the org name (or ID technically) to be URL safe
 export default function withCleanOrgId(handler: any) {
   return async (event: APIGatewayProxyEventV2) => {
-    console.log("In middleware event", event);
     const { orgId } = event.pathParameters;
-    console.log("Extracted org id", orgId);
-    console.log("Generated ", tagGenerator.generate(orgId));
     event.pathParameters.orgId = tagGenerator.generate(orgId);
-
-    console.log("new org id", event.pathParameters.orgId);
     return handler(event);
   };
 }
