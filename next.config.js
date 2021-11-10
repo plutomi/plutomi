8c04b19bfd666aa21eada22004b6789fb7ffcb78
Jose Valerio <contact@josevalerio.com>
2021-11-10 17:43:22 -0500

    Added pag redirects

--- next.config.js
+++ next.config.js
@@ -13,4 +13,39 @@ module.exports = {
     WEBSITE_URL: process.env.WEBSITE_URL,
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/openings/:any*",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/stages/:any*",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/profile/:any*",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/team/:any*",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/domains/:any*",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/dashboard/:any*",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
