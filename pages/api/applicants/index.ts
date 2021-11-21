b86bca24ee1a7f3226effd69040ac125da79c226
Jose Valerio <contact@josevalerio.com>
2021-11-21 03:11:08 -0500

    chore: Added Joi template for future input validation

--- pages/api/applicants/index.ts
+++ pages/api/applicants/index.ts
@@ -25,72 +25,64 @@ const handler = async (
     firstName,
     lastName,
     email,
-    stageId,
   }: CreateApplicantAPIBody = body;
 
   // Creates an applicant
   if (method === API_METHODS.POST) {
-    const opening = await getOpening({ orgId, openingId });
-
-    if (!opening) {
-      return res.status(400).json({ message: "Bad opening ID" });
-    }
-
-    const firstStage = stageId || opening.stageOrder[0];
+    const schema = Joi.object({
+      orgId: Joi.string(),
+      email: Joi.string().email(),
+      firstName: Joi.string(),
+      lastName: Joi.string(),
+      openingId: Joi.string().length(ID_LENGTHS.OPENING),
+    }).options({ presence: "required" });
 
     try {
-      const schema = Joi.object({
-        orgId: Joi.string(),
-        email: Joi.string().email(),
-        firstName: Joi.string(),
-        lastName: Joi.string(),
-        openingId: Joi.string().length(ID_LENGTHS.OPENING),
-        stageId: Joi.string().length(ID_LENGTHS.STAGE),
-      }).options({ presence: "required" });
-
-      const CreateApplicantInput = {
-        orgId: orgId,
-        email: email,
-        firstName: firstName,
-        lastName: lastName,
-        openingId: openingId,
-        stageId: firstStage,
-      };
+      await schema.validateAsync(req.body);
 
       try {
-        const value = await schema.validateAsync(CreateApplicantInput);
-        return res.status(200).json({ message: value });
+        const opening = await getOpening({ orgId, openingId });
+
+        // DO NOT REMOVE
+        // We need the first stage in this opening
+        if (!opening) {
+          return res.status(404).json({ message: "Bad opening ID" });
+        }
+
+        try {
+          const newApplicant = await createApplicant({
+            orgId: orgId,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            openingId: openingId,
+            stageId: opening.stageOrder[0],
+          });
+
+          const applicantionLink = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/${orgId}/applicants/${newApplicant.applicantId}`;
+
+          await sendEmail({
+            fromName: "Applications",
+            fromAddress: EMAILS.GENERAL,
+            toAddresses: ["newApplicant.email"], // TODO remove the quotes around this https://github.com/plutomi/plutomi/issues/342
+            subject: `Here is a link to your application!`,
+            html: `<h1><a href="${applicantionLink}" rel=noreferrer target="_blank" >Click this link to view your application!</a></h1><p>If you did not request this link, you can safely ignore it.</p>`,
+          });
+
+          return res.status(201).json({
+            message: `We've sent a link to your email to complete your application!`,
+          });
+        } catch (error) {
+          // TODO add error logger
+          return res
+            .status(400) // TODO change #
+            .json({ message: `Unable to create applicant: ${error}` });
+        }
       } catch (error) {
-        return res.status(400).json({ message: `${error.message}` });
+        return res.status(500).json({ message: `${error.message}` });
       }
-
-      const newApplicant = await createApplicant({
-        orgId: orgId,
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        openingId: openingId,
-        stageId: firstStage,
-      });
-
-      const applicantionLink = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/${orgId}/applicants/${newApplicant.applicantId}`;
-
-      await sendEmail({
-        fromName: "Applications",
-        fromAddress: EMAILS.GENERAL,
-        toAddresses: ["newApplicant.email"], // TODO remove the quotes around this https://github.com/plutomi/plutomi/issues/342
-        subject: `Here is a link to your application!`,
-        html: `<h1><a href="${applicantionLink}" rel=noreferrer target="_blank" >Click this link to view your application!</a></h1><p>If you did not request this link, you can safely ignore it.</p>`,
-      });
-
-      return res.status(201).json({
-        message: `We've sent a link to your email to complete your application!`,
-      });
     } catch (error) {
-      // TODO add error logger
-      return res
-        .status(400) // TODO change #
-        .json({ message: `Unable to create applicant: ${error}` });
+      return res.status(400).json({ message: `${error.message}` });
     }
   }
 };
