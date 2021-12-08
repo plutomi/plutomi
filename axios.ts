88aa35f35ede01b208188506098c7c7026ef39a7
Jose Valerio <contact@josevalerio.com>
2021-12-08 11:16:17 -0500

    🐛 fix: API url on the frontend (#407)

--- axios.ts
+++ axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Access API url from browser
   withCredentials: true,
 });
 
