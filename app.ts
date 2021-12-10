e28c0c6813e33fb5e825ba6fa38fb30424c157c7
Jose Valerio <contact@josevalerio.com>
2021-12-09 23:26:23 -0500

    Added different env files for  dev and deploy

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: `./.env.${process.env.NODE_ENV}` });
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
