66909639c34e12652c6c2a684d9a5acca753adc7
Jose Valerio <contact@josevalerio.com>
2021-12-14 20:39:59 -0500

    Added error formatting to invites

--- Controllers/Invites.ts
+++ Controllers/Invites.ts
@@ -1,18 +1,13 @@
 import { Request, Response } from "express";
 import Joi from "joi";
-import {
-  API_METHODS,
-  DEFAULTS,
-  EMAILS,
-  ENTITY_TYPES,
-  TIME_UNITS,
-} from "./../Config";
+import { DEFAULTS, EMAILS, ENTITY_TYPES, TIME_UNITS } from "./../Config";
 import Sanitize from "./../utils/sanitize";
 import sendEmail from "./../utils/sendEmail";
 import * as Invites from "../models/Invites/index";
 import * as Time from "./../utils/time";
 import * as Users from "../models/Users/index";
 import * as Orgs from "../models/Orgs/index";
+import errorFormatter from "../utils/errorFormatter";
 const UrlSafeString = require("url-safe-string"),
   tagGenerator = new UrlSafeString();
 
@@ -33,9 +28,11 @@ export const create = async (req: Request, res: Response) => {
   const [org, error] = await Orgs.getOrgById({ orgId: req.session.user.orgId });
 
   if (error) {
-    return res
-      .status(500)
-      .json({ message: "An error retrieving your org information" });
+    const formattedError = errorFormatter(error);
+    return res.status(error.$metadata.httpStatusCode).json({
+      message: "An error ocurred retrieving your org information",
+      ...formattedError,
+    });
   }
 
   const schema = Joi.object({
@@ -56,9 +53,11 @@ export const create = async (req: Request, res: Response) => {
   });
 
   if (recipientError) {
-    return res
-      .status(500)
-      .json({ message: "An error ocurred getting your invitee's information" });
+    const formattedError = errorFormatter(recipientError);
+    return res.status(error.$metadata.httpStatusCode).json({
+      message: "An error ocurred getting your invitee's information",
+      ...formattedError,
+    });
   }
 
   if (!recipient) {
@@ -67,8 +66,10 @@ export const create = async (req: Request, res: Response) => {
     });
 
     if (error) {
-      return res.status(500).json({
+      const formattedError = errorFormatter(error);
+      return res.status(error.$metadata.httpStatusCode).json({
         message: "An error ocurred creating an account for your invitee",
+        ...formattedError,
       });
     }
     recipient = createdUser;
@@ -83,9 +84,11 @@ export const create = async (req: Request, res: Response) => {
   });
 
   if (inviteError) {
-    return res
-      .status(500)
-      .json({ message: "An error ocurred creating your invite" });
+    const formattedError = errorFormatter(inviteError);
+    return res.status(error.$metadata.httpStatusCode).json({
+      message: "An error ocurred creating your invite",
+      ...formattedError,
+    });
   }
 
   const [emailSent, emailFailure] = await sendEmail({
@@ -98,9 +101,11 @@ export const create = async (req: Request, res: Response) => {
   }); // TODO add target=_blank and rel=noreferrer ^
 
   if (emailFailure) {
-    return res.status(500).json({
-      // TODO update this since email will be done with streams
-      message: `The invite was created, but we were not able to send an email to the user. They can log in and accept their invite at https://plutomi.com/invites - ${error}`,
+    const formattedError = errorFormatter(emailFailure);
+    return res.status(error.$metadata.httpStatusCode).json({
+      message:
+        "The invite was created, but we were not able to send an email to the user. They can log in and accept their invite!",
+      ...formattedError,
     });
   }
 
@@ -124,9 +129,11 @@ export const accept = async (req: Request, res: Response) => {
   });
 
   if (error) {
-    return res
-      .status(500)
-      .json({ message: "An error ocurred getting the info for your invite" });
+    const formattedError = errorFormatter(error);
+    return res.status(error.$metadata.httpStatusCode).json({
+      message: "An error ocurred getting the info for your invite",
+      ...formattedError,
+    });
   }
 
   if (!invite) {
@@ -139,9 +146,11 @@ export const accept = async (req: Request, res: Response) => {
   });
 
   if (joinError) {
-    return res
-      .status(500)
-      .json({ message: "We were unable to join that org, please try again" });
+    const formattedError = errorFormatter(joinError);
+    return res.status(error.$metadata.httpStatusCode).json({
+      message: "We were unable to join that org",
+      ...formattedError,
+    });
   }
 
   const [updatedUser, updatedUserFailure] = await Users.getUserById({
@@ -149,9 +158,11 @@ export const accept = async (req: Request, res: Response) => {
   });
 
   if (updatedUserFailure) {
-    return res.status(500).json({
+    const formattedError = errorFormatter(updatedUserFailure);
+    return res.status(error.$metadata.httpStatusCode).json({
       message:
         "We were able to succesfully join the org, but we were unable to update your session. Please log out and log in again!",
+      ...formattedError,
     });
   }
   req.session.user = Sanitize.clean(updatedUser, ENTITY_TYPES.USER);
@@ -170,9 +181,11 @@ export const reject = async (req: Request, res: Response) => {
   });
 
   if (error) {
-    return res
-      .status(500)
-      .json({ message: "We were unable to delete that invite :(" });
+    const formattedError = errorFormatter(error);
+    return res.status(error.$metadata.httpStatusCode).json({
+      message: "We were unable to delete that invite",
+      ...formattedError,
+    });
   }
   req.session.user.totalInvites -= 1;
   await req.session.save();
