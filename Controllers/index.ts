13a1c1c37f21495d5cb9beacc70512d368ef00d3
Jose Valerio <contact@josevalerio.com>
2022-05-30 19:08:46 -0400

    updated controllers

--- Controllers/index.ts
+++ Controllers/index.ts
@@ -1,6 +1,23 @@
 import * as Applicants from './Applicants';
 import * as Auth from './Auth';
 import * as Invites from './Invites';
+import * as Openings from './Openings';
+import * as Orgs from './Orgs';
+import * as PublicInfo from './PublicInfo';
+import * as Questions from './Questions';
+import * as Stages from './Stages';
 import * as Users from './Users';
+import * as Webhooks from './Webhooks';
 
-export { Applicants, Auth, Invites, Users };
+export default {
+  Applicants,
+  Auth,
+  Invites,
+  Users,
+  Openings,
+  Orgs,
+  PublicInfo,
+  Stages,
+  Webhooks,
+  Questions,
+};
