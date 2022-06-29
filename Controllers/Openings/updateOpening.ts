812b51ddb7bc744b796bb3dbfbed6acca1aa0727
Jose Valerio <contact@josevalerio.com>
2022-06-28 22:11:47 -0400

    Updated opening with new update format

--- Controllers/Openings/updateOpening.ts
+++ Controllers/Openings/updateOpening.ts
@@ -2,15 +2,18 @@ import { Request, Response } from 'express';
 import Joi from 'joi';
 import * as CreateError from '../../utils/createError';
 import { JOI_SETTINGS, OpeningState, LIMITS } from '../../Config';
-import { DynamoOpening } from '../../types/dynamo';
 import { UpdateOpeningInput } from '../../models/Openings/updateOpening';
 import { DB } from '../../models';
+import { DynamoOpening } from '../../types/dynamo';
 
 const schema = Joi.object({
   stageOrder: Joi.array().items(Joi.string()),
   openingName: Joi.string().max(LIMITS.MAX_OPENING_NAME_LENGTH),
   GSI1SK: Joi.string().valid(OpeningState.PUBLIC, OpeningState.PRIVATE),
 }).options(JOI_SETTINGS);
+
+export interface APIUpdateOpeningOptions
+  extends Partial<Pick<DynamoOpening, 'openingName' | 'GSI1SK' | 'stageOrder'>> {}
 
 export const updateOpening = async (req: Request, res: Response) => {
   try {
@@ -19,15 +22,9 @@ export const updateOpening = async (req: Request, res: Response) => {
     const { status, body } = CreateError.JOI(error);
     return res.status(status).json(body);
   }
-
+  let updatedValues: APIUpdateOpeningOptions = {};
   const { user } = req;
   const { openingId } = req.params;
-
-  const updateOpeningInput: UpdateOpeningInput = {
-    openingId,
-    orgId: user.orgId,
-    newValues: {},
-  };
 
   const [opening, openingError] = await DB.Openings.getOpening({
     orgId: user.orgId,
@@ -66,15 +63,30 @@ export const updateOpening = async (req: Request, res: Response) => {
           "The stageIds in the 'stageOrder' property differ from the ones in the opening, please check your request and try again.",
       });
     }
+
+    updatedValues.stageOrder = req.body.stageOrder;
+  }
+  // Public or private
+  if (req.body.GSI1SK) {
+    // TODO i think this can be moved into dynamo
+    if (req.body.GSI1SK === OpeningState.PUBLIC && opening.totalStages === 0) {
+      return res.status(403).json({
+        message: 'An opening needs to have stages before being made public',
+      });
+    }
+
+    updatedValues.GSI1SK = req.body.GSI1SK;
   }
 
-  // TODO i think this can be moved into dynamo
-  if (req.body.GSI1SK === OpeningState.PUBLIC && opening.totalStages === 0) {
-    return res.status(403).json({
-      message: 'An opening needs to have stages before being made public',
-    });
+  if (req.body.openingName) {
+    updatedValues.openingName = req.body.openingName;
   }
-  const [updatedOpening, error] = await DB.Openings.updateOpening(updateOpeningInput);
+
+  const [updatedOpening, error] = await DB.Openings.updateOpening({
+    orgId: user.orgId,
+    openingId: req.params.openingId,
+    updatedValues,
+  });
 
   if (error) {
     const { status, body } = CreateError.SDK(error, 'An error ocurred updating this opening');
--- models/Openings/updateOpening.ts
+++ models/Openings/updateOpening.ts
@@ -1,32 +1,29 @@
 import { UpdateCommandInput, UpdateCommand } from '@aws-sdk/lib-dynamodb';
 import { Dynamo } from '../../awsClients/ddbDocClient';
 import { DYNAMO_TABLE_NAME, Entities } from '../../Config';
+import { APIUpdateOpeningOptions } from '../../Controllers/Openings/updateOpening';
 import { DynamoOpening } from '../../types/dynamo';
 
-interface APIUpdateOpeningOptions
-  extends Partial<Pick<DynamoOpening, 'openingName' | 'GSI1SK' | 'stageOrder'>> {}
-
 export interface UpdateOpeningInput extends Pick<DynamoOpening, 'orgId' | 'openingId'> {
-  newValues: APIUpdateOpeningOptions;
+  updatedValues: APIUpdateOpeningOptions;
 }
 
-// TODO new udpate method https://github.com/plutomi/plutomi/issues/594
 export const updateOpening = async (
   props: UpdateOpeningInput,
 ): Promise<[null, null] | [null, any]> => {
-  const { orgId, openingId, newValues } = props;
+  const { orgId, openingId } = props;
+
+  // TODO convert this to a function
   // Build update expression
   const allUpdateExpressions: string[] = [];
   const allAttributeValues: { [key: string]: string } = {};
 
-  // https://github.com/plutomi/plutomi/issues/594
-  // eslint-disable-next-line no-restricted-syntax
-  for (const property of Object.keys(newValues)) {
+  for (const property of Object.keys(props.updatedValues)) {
     // Push each property into the update expression
     allUpdateExpressions.push(`${property} = :${property}`);
 
     // Create values for each attribute
-    allAttributeValues[`:${property}`] = newValues[property];
+    allAttributeValues[`:${property}`] = props.updatedValues[property];
   }
 
   const params: UpdateCommandInput = {
