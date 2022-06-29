0ee208830ada1b91256c54912a8b602fb32df5b1
Jose Valerio <contact@josevalerio.com>
2022-06-28 22:53:38 -0400

    Added updated at property to dynamo calls

--- utils/createDynamoUpdateExpression.ts
+++ utils/createDynamoUpdateExpression.ts
@@ -1,3 +1,5 @@
+import * as Time from '../utils/time';
+
 interface CreateDynamoUpdateExpressionProps {
   updatedValues: Object; // Any object
 }
@@ -6,6 +8,7 @@ interface CreateDynamoUpdateExpressionResponse {
   allUpdateExpressions: string[];
   allAttributeValues: { [key: string]: string };
 }
+
 export const createDynamoUpdateExpression = (
   props: CreateDynamoUpdateExpressionProps,
 ): CreateDynamoUpdateExpressionResponse => {
@@ -13,17 +16,18 @@ export const createDynamoUpdateExpression = (
   const allUpdateExpressions: string[] = [];
   const allAttributeValues: { [key: string]: string } = {};
 
-  for (const property of Object.keys(props.updatedValues)) {
-    console.log('PROPEORTY', property);
+  const updatedValuesWithDate = {
+    ...props.updatedValues,
+    updatedAt: Time.currentISO(),
+  };
+
+  for (const property of Object.keys(updatedValuesWithDate)) {
     // Push each property into the update expression
     allUpdateExpressions.push(`${property} = :${property}`);
 
     // Create values for each attribute
-    allAttributeValues[`:${property}`] = props.updatedValues[property];
+    allAttributeValues[`:${property}`] = updatedValuesWithDate[property];
   }
-
-  console.log(`UPDATE EXPIRESS`, allUpdateExpressions);
-  console.log(`UPDATE ATTRIBUTE VALUES`, allAttributeValues);
 
   return { allUpdateExpressions, allAttributeValues };
 };
