4b680f96db230c496788d4e4806103733952e93d
Jose Valerio <contact@josevalerio.com>
2022-07-25 02:12:18 -0400

    Removed search until ES is implemented

--- components/NavbarSearch/NavbarSearch.tsx
+++ components/NavbarSearch/NavbarSearch.tsx
@@ -6,14 +6,6 @@ export const NavbarSearch = () => (
       <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
         <UsersIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
       </div>
-      <input
-        type="text"
-        name="email"
-        id="email"
-        onChange={() => alert('This is not hooked up at the moment')}
-        placeholder="Search for applicants by name, email, or phone number"
-        className="focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md pl-10 sm:text-sm border-gray-300"
-      />
     </div>
   </div>
 );
