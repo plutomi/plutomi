df045487e98f02ca8df3c60dd6d8661ce187e94a
Jose Valerio <contact@josevalerio.com>
2022-08-04 21:45:32 -0400

    Created org entity

---
+++ entities/Org.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+import { DEFAULTS } from '../Config';
+import { baseSchema, IBase } from './Base';
+
+export interface IOrg extends IBase {
+  totalApplicants: number;
+  totalOpenings: number;
+  totalUsers: number;
+  totalWebhooks: number;
+  totalQuestions: number;
+  displayName: string;
+}
+
+export const orgSchema = new Schema<IOrg>({
+  ...baseSchema.obj,
+  totalApplicants: { type: Number, default: 0 },
+  totalOpenings: { type: Number, default: 0 },
+  totalUsers: { type: Number, default: 0 },
+  totalQuestions: { type: Number, default: 0 },
+  totalWebhooks: { type: Number, default: 0 },
+  displayName: String,
+});
+
+export const User = model<IOrg>('Org', orgSchema);
