1f345839fef1d62376e068e15e3357cbf093e2d8
Jose Valerio <contact@josevalerio.com>
2022-08-05 00:42:53 -0400

    Added logic for accepting invites

--- Controllers/Invites/acceptInvite.ts
+++ Controllers/Invites/acceptInvite.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import { DEFAULTS } from '../../Config';
 import * as CreateError from '../../utils/createError';
-import * as Time from '../../utils/time';
 import { DB } from '../../models';
+import { OrgInvite } from '../../entities/Invites';
+import { IOrg, Org } from '../../entities/Org';
+import { User } from '../../entities/User';
 
 export const acceptInvite = async (req: Request, res: Response) => {
   const { inviteId } = req.params;
@@ -14,44 +16,77 @@ export const acceptInvite = async (req: Request, res: Response) => {
     });
   }
 
-  const [invite, error] = await DB.Invites.getInvite({
-    inviteId,
-    userId: user.userId,
-  });
+  try {
+    const invite = await OrgInvite.findOne({ _id: inviteId }).populate<{ org: IOrg }>('org');
 
-  if (error) {
-    const { status, body } = CreateError.SDK(
-      error,
-      'An error ocurred getting the info for your invite',
-    );
+    if (!invite) {
+      return res.status(404).json({ message: 'Invite no longer exists' });
+    }
 
-    return res.status(status).json(body);
+    // Not sure how this would happen as we do a check before the invite
+    // is sent to prevent this...
+
+    await invite.populate('org');
+    if (invite.org.orgId === user.orgId) {
+      return res.status(400).json({ message: "It appears that you're already in this org!" });
+    }
+
+    if (invite.expiresAt <= new Date()) {
+      return res.status(403).json({
+        message:
+          'It looks like that invite has expired, ask the org admin to send you another one!',
+      });
+    }
+
+    // TODO accept invite,
+    // TODO needs to be a transaction
+
+    try {
+      // Delete the org invite
+
+      await OrgInvite.deleteOne({
+        _id: invite._id,
+      });
+
+      try {
+        await User.updateOne(
+          {
+            _id: user._id,
+          },
+          {
+            $set: {
+              orgId: invite.org._id,
+              orgJoinDate: new Date(),
+            },
+          },
+        );
+
+        try {
+          await Org.updateOne(
+            {
+              _id: invite.org._id,
+            },
+            {
+              $inc: {
+                totalUsers: 1,
+              },
+            },
+          );
+          return res.status(200).json({ message: 'Invite accepted!' });
+        } catch (error) {
+          return res.status(500).json({
+            message: 'Invite accepted, user updated, but unable to increment user count on org',
+          });
+        }
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: 'Invite deleted, but unable to update user org ID' });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'An error ocurred accepting your org invite: Deletion step' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving your invites' });
   }
-
-  if (!invite) {
-    return res.status(404).json({ message: 'Invite no longer exists' });
-  }
-  // Not sure how this would happen as we do a check before the invite
-  // is sent to prevent this...
-  if (invite.orgId === user.orgId) {
-    return res.status(400).json({ message: "It appears that you're already in this org!" });
-  }
-
-  if (invite.expiresAt <= Time.currentISO()) {
-    return res.status(403).json({
-      message: 'It looks like that invite has expired, ask the org admin to send you another one!',
-    });
-  }
-
-  const [joined, joinError] = await DB.Invites.acceptInvite({
-    userId: user.userId,
-    invite,
-  });
-
-  if (joinError) {
-    const { status, body } = CreateError.SDK(joinError, 'We were unable to accept that invite');
-    return res.status(status).json(body);
-  }
-
-  return res.status(200).json({ message: `You've joined the ${invite.orgName}!` });
 };
--- entities/Invites.ts
+++ entities/Invites.ts
@@ -15,9 +15,9 @@ export interface IOrgInvite extends IBase {
 
 export const orgInviteSchema = new Schema<IOrgInvite>({
   ...baseSchema.obj,
-  org: { type: Schema.Types.ObjectId, default: DEFAULTS.FIRST_NAME, ref: Org },
-  createdBy: { type: Schema.Types.ObjectId, default: DEFAULTS.FIRST_NAME, ref: User },
-  recipient: { type: Schema.Types.ObjectId, default: DEFAULTS.FIRST_NAME, ref: User },
+  org: { type: Schema.Types.ObjectId, ref: 'Org' },
+  createdBy: { type: Schema.Types.ObjectId, ref: User },
+  recipient: { type: Schema.Types.ObjectId, ref: User },
   expiresAt: { type: Date, default: () => dayjs(new Date()).add(3, 'days').toDate() },
   // TODO add TTL index?
 });
