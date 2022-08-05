08d875557277ff6a425024ac4801e6b70add341e
Jose Valerio <contact@josevalerio.com>
2022-08-05 01:48:23 -0400

    Update opening call

--- Controllers/Openings/updateOpening.ts
+++ Controllers/Openings/updateOpening.ts
@@ -4,6 +4,7 @@ import * as CreateError from '../../utils/createError';
 import { JOI_SETTINGS, OpeningState, LIMITS } from '../../Config';
 import { DB } from '../../models';
 import { DynamoOpening } from '../../types/dynamo';
+import { Opening } from '../../entities/Opening';
 
 const schema = Joi.object({
   stageOrder: Joi.array().items(Joi.string()),
@@ -21,79 +22,70 @@ export const updateOpening = async (req: Request, res: Response) => {
     const { status, body } = CreateError.JOI(error);
     return res.status(status).json(body);
   }
-  let updatedValues: APIUpdateOpeningOptions = {};
   const { user } = req;
   const { openingId } = req.params;
 
-  const [opening, openingError] = await DB.Openings.getOpening({
-    orgId: user.org,
-    openingId,
-  });
+  try {
+    const opening = await Opening.findById(openingId, {
+      org: user.org,
+    });
 
-  if (openingError) {
-    const { status, body } = CreateError.SDK(
-      openingError,
-      "An error ocurred retrieving this opening's info",
-    );
-    return res.status(status).json(body);
-  }
+    if (!opening) {
+      return res.status(404).json({ message: 'Opening not found' });
+    }
 
-  if (!opening) {
-    return res.status(404).json({ message: 'Opening not found' });
-  }
-
-  if (req.body.stageOrder) {
-    if (req.body.stageOrder.length !== opening.stageOrder.length) {
+    if (req.body.stageOrder) {
       return res.status(403).json({
         message:
-          'You cannot add / delete stages this way, please use the proper API methods for those actions',
+          'Updating stage order is currently blocked see https://github.com/plutomi/plutomi/issues/562',
       });
+      // if (req.body.stageOrder.length !== opening.stageOrder.length) {
+      //   return res.status(403).json({
+      //     message:
+      //       'You cannot add / delete stages this way, please use the proper API methods for those actions',
+      //   });
+      // }
+
+      // Check if the IDs have been modified
+      // // TODO add a test for this
+      // const containsAll = opening.stageOrder.every((stageId) =>
+      //   req.body.stageOrder.includes(stageId),
+      // );
+
+      // if (!containsAll) {
+      //   return res.status(400).json({
+      //     message:
+      //       "The stageIds in the 'stageOrder' property differ from the ones in the opening, please check your request and try again.",
+      //   });
+      // }
+
+      //   updatedValues.stageOrder = req.body.stageOrder;
+    }
+    // Public or private
+    if (req.body.GSI1SK) {
+      // TODO update property
+      if (req.body.GSI1SK === OpeningState.PUBLIC && opening.totalStages === 0) {
+        return res.status(403).json({
+          message: 'An opening needs to have stages before being made public',
+        });
+      }
+
+      opening.visibility = req.body.GSI1SK;
     }
 
-    // Check if the IDs have been modified
-    // TODO add a test for this
-    const containsAll = opening.stageOrder.every((stageId) =>
-      req.body.stageOrder.includes(stageId),
-    );
-
-    if (!containsAll) {
-      return res.status(400).json({
-        message:
-          "The stageIds in the 'stageOrder' property differ from the ones in the opening, please check your request and try again.",
-      });
+    if (req.body.openingName) {
+      opening.name = req.body.openingName;
     }
 
-    updatedValues.stageOrder = req.body.stageOrder;
-  }
-  // Public or private
-  if (req.body.GSI1SK) {
-    // TODO i think this can be moved into dynamo
-    if (req.body.GSI1SK === OpeningState.PUBLIC && opening.totalStages === 0) {
-      return res.status(403).json({
-        message: 'An opening needs to have stages before being made public',
+    try {
+      await opening.save();
+      return res.status(200).json({
+        message: 'Opening updated!',
       });
+    } catch (error) {
+      return res.status(500).json({ message: 'An error ocurred updating that opening' });
     }
-
-    updatedValues.GSI1SK = req.body.GSI1SK;
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred finding that opening' });
   }
-
-  if (req.body.openingName) {
-    updatedValues.openingName = req.body.openingName;
-  }
-
-  const [updatedOpening, error] = await DB.Openings.updateOpening({
-    orgId: user.org,
-    openingId: req.params.openingId,
-    updatedValues,
-  });
-
-  if (error) {
-    const { status, body } = CreateError.SDK(error, 'An error ocurred updating this opening');
-
-    return res.status(status).json(body);
-  }
-
-  return res.status(200).json({
-    message: 'Opening updated!',
-  });
 };
