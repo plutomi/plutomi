4352e649c4e2f7c423005ace1895c2912595c1a7
Jose Valerio <contact@josevalerio.com>
2022-08-04 22:06:51 -0400

    Added route to join and leave org

--- Controllers/Orgs/leaveAndDeleteOrg.ts
+++ Controllers/Orgs/leaveAndDeleteOrg.ts
@@ -1,35 +1,48 @@
 import { Request, Response } from 'express';
+import { DEFAULTS } from '../../Config';
+import { Org } from '../../entities/Org';
+import { User } from '../../entities/User';
 import { DB } from '../../models';
 import * as CreateError from '../../utils/createError';
 
 export const leaveAndDeleteOrg = async (req: Request, res: Response) => {
   const { user } = req;
-  const [org, error] = await DB.Orgs.getOrg({ orgId: user.orgId });
+  try {
+    const org = await Org.findById(user.orgId);
+    if (!org) {
+      // Not sure how this would happen
+      return res.status(404).json({ message: 'Org does not exist' });
+    }
 
-  if (error) {
-    const { status, body } = CreateError.SDK(error, 'Unable to retrieve org info');
-    return res.status(status).json(body);
+    if (org.totalUsers > 1) {
+      return res.status(403).json({
+        message: 'You cannot delete this org as there are other users in it',
+      });
+    }
+
+    try {
+      await Org.deleteOne({
+        _id: org.id,
+      });
+
+      try {
+        await User.updateOne(
+          { _id: user._id },
+          {
+            $set: {
+              orgId: DEFAULTS.NO_ORG,
+              orgJoinDate: undefined,
+            },
+          },
+        );
+        return res.status(200).json({ message: 'Org deleted!' });
+      } catch (error) {
+        return res.status(500).json({ message: 'Error ocurred updating user with deleted org' });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Error ocurred deleting org' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving org info' });
   }
-
-  if (!org) {
-    return res.status(404).json({ message: 'Org not found' });
-  }
-  if (org.totalUsers > 1) {
-    return res.status(403).json({
-      message: 'You cannot delete this org as there are other users in it',
-    });
-  }
-
-  // Transaction - updates the user with default org values
-  const [success, failure] = await DB.Orgs.leaveAndDeleteOrg({
-    orgId: user.orgId,
-    userId: user.userId,
-  });
-
-  if (failure) {
-    const { status, body } = CreateError.SDK(failure, 'We were unable to remove you from the org');
-    return res.status(status).json(body);
-  }
-
-  return res.status(200).json({ message: 'Org deleted!' });
 };
