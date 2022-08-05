5e96b1c603f8abf031ba2abd3054344cb251813e
Jose Valerio <contact@josevalerio.com>
2022-08-04 22:42:52 -0400

    Added query to return openings in org

--- Controllers/Openings/getOpeningsInOrg.ts
+++ Controllers/Openings/getOpeningsInOrg.ts
@@ -1,19 +1,18 @@
 import { Request, Response } from 'express';
+import { Opening } from '../../entities/Opening';
 import { DB } from '../../models';
 import * as CreateError from '../../utils/createError';
 
 export const getOpeningsInOrg = async (req: Request, res: Response) => {
   const { user } = req;
-  const [openings, openingsError] = await DB.Openings.getOpeningsInOrg({
-    orgId: user.orgId,
-  });
 
-  if (openingsError) {
-    console.error('Openings error');
-    const { status, body } = CreateError.SDK(openingsError, 'An error ocurred retrieving openings');
+  try {
+    const openings = await Opening.find({
+      orgId: user.orgId,
+    });
 
-    return res.status(status).json(body);
+    return res.status(200).json(openings);
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving openings' });
   }
-
-  return res.status(200).json(openings);
 };
