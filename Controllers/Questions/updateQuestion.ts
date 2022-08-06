fbec9b5165aafafecd2a8af0240ff82848b1bdb4
Jose Valerio <contact@josevalerio.com>
2022-08-06 04:21:20 -0400

    Question updated

--- Controllers/Questions/updateQuestion.ts
+++ Controllers/Questions/updateQuestion.ts
@@ -4,6 +4,7 @@ import * as CreateError from '../../utils/createError';
 import { JOI_SETTINGS, LIMITS } from '../../Config';
 import { DynamoQuestion } from '../../types/dynamo';
 import { DB } from '../../models';
+import { Question } from '../../entities/Question';
 
 export interface APIUpdateQuestionOptions
   extends Partial<Pick<DynamoQuestion, 'GSI1SK' | 'description'>> {}
@@ -21,34 +22,38 @@ export const updateQuestion = async (req: Request, res: Response) => {
     return res.status(status).json(body);
   }
 
-  console.log(`REQ BODY`, req.body);
-
   const { user } = req;
   const { questionId } = req.params;
-  let updatedValues: APIUpdateQuestionOptions = {};
 
-  if (req.body.GSI1SK) {
-    updatedValues.GSI1SK = req.body.GSI1SK;
-  }
-
-  if (req.body.description || req.body.description === '') {
-    updatedValues.description = req.body.description;
-  }
-  const [question, questionError] = await DB.Questions.updateQuestion({
-    orgId: user.org,
-    questionId,
-    updatedValues,
-  });
-
-  if (questionError) {
-    const { status, body } = CreateError.SDK(
-      questionError,
-      'An error ocurred updating this question',
+  try {
+    const newQuestion = await Question.findOne(
+      {
+        _id: questionId,
+      },
+      {
+        orgId: user.org,
+      },
     );
-    return res.status(status).json(body);
-  }
+    if (!newQuestion) {
+      return res.status(404).json({ message: 'Question does not exist' });
+    }
 
-  return res.status(200).json({
-    message: 'Question updated!',
-  });
+    // todo UPDATE THIS valuye
+    if (req.body.GSI1sk) {
+      newQuestion.title = req.body.GSI1SK;
+    }
+
+    if (req.body.description) {
+      newQuestion.description = req.body.description;
+    }
+
+    try {
+      await newQuestion.save();
+      return res.status(200).json({ message: 'Question updated!' });
+    } catch (error) {
+      return res.status(500).json({ message: 'An error ocurred saving that question' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'An error courred updatng the question' });
+  }
 };
