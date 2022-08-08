87e93c546a187b54a1b9b0f51b2f010280c73079
Jose Valerio <contact@josevalerio.com>
2022-08-08 00:26:43 -0400

    Retrieving users in same org

--- Controllers/Users/getUser.ts
+++ Controllers/Users/getUser.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import Joi from 'joi';
+import { Schema } from 'mongoose';
 import { JOI_SETTINGS } from '../../Config';
+import { User } from '../../entities/User';
 import { DB } from '../../models';
 import * as CreateError from '../../utils/createError';
 
@@ -24,33 +26,35 @@ export const getUser = async (req: Request, res: Response) => {
   }
 
   const { userId }: APIGetUserByIdParameters = req.params;
-  if (user.userId !== userId) {
+  if ((user._id as unknown as string) !== userId) {
+    // TODO types
     // TODO RBAC
+    // TODO 404?
     return res.status(403).json({
       message: 'You are not authorized to view this user',
     });
   }
 
+  try {
+    const requestedUser = await User.findOne(userId as unknown as Schema.Types.ObjectId);
+
+    if (!requestedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // TODO RBAC here
+    // Only allow viewing users in the same org
+    if (user.org !== requestedUser.org) {
+      return res.status(403).json({
+        message: 'You are not authorized to view this user since you are not in the same org',
+      });
+    }
+
+    return res.status(200).json(requestedUser);
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving user info' });
+  }
   const [requestedUser, error] = await DB.Users.getUserById({
     userId,
   });
-
-  if (error) {
-    const { status, body } = CreateError.SDK(error, 'An error ocurred retrieving user info by id');
-
-    return res.status(status).json(body);
-  }
-  if (!requestedUser) {
-    return res.status(404).json({ message: 'User not found' });
-  }
-
-  // TODO RBAC here
-  // Only allow viewing users in the same org
-  if (user.org !== requesteduser.org) {
-    return res.status(403).json({
-      message: 'You are not authorized to view this user since you are not in the same org',
-    });
-  }
-
-  return res.status(200).json(requestedUser);
 };
