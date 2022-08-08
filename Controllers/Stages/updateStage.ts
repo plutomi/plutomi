2c4e05e7bc772f597cd035fffeb1bec09eddb032
Jose Valerio <contact@josevalerio.com>
2022-08-08 00:23:44 -0400

    Updating stage logic

--- Controllers/Stages/updateStage.ts
+++ Controllers/Stages/updateStage.ts
@@ -4,6 +4,7 @@ import { JOI_SETTINGS, LIMITS } from '../../Config';
 import { DynamoStage } from '../../types/dynamo';
 import * as CreateError from '../../utils/createError';
 import { DB } from '../../models';
+import { Stage } from '../../entities/Stage';
 
 export interface APIUpdateStageOptions
   extends Partial<Pick<DynamoStage, 'GSI1SK' | 'questionOrder'>> {}
@@ -25,68 +26,57 @@ export const updateStage = async (req: Request, res: Response) => {
   const { user } = req;
   const { openingId, stageId } = req.params;
 
-  const [stage, stageError] = await DB.Stages.getStage({
-    openingId,
-    stageId,
-    orgId: user.org,
-  });
+  try {
+    const stage = await Stage.findById(stageId, {
+      openingId,
+      org: user.org,
+    });
 
-  if (stageError) {
-    const { status, body } = CreateError.SDK(
-      stageError,
-      'An error ocurred retrieving your stage info',
-    );
-    return res.status(status).json(body);
-  }
-
-  if (!stage) {
-    return res.status(404).json({ message: 'Stage not found' });
-  }
-
-  /**
-   * If a user is attempting to update the order of the questions
-   * but the length differs
-   */
-  if (req.body.questionOrder) {
-    if (req.body.questionOrder.length !== stage.questionOrder.length) {
-      return res.status(403).json({
-        message:
-          'You cannot add / delete questions this way, please use the proper API methods for those actions',
-      });
+    if (!stage) {
+      return res.status(404).json({ message: 'Stage not found' });
     }
 
-    // Check if the IDs have been modified
-    const containsAll = stage.questionOrder.every((questionId) =>
-      req.body.questionOrder.includes(questionId),
-    );
+    /**
+     * If a user is attempting to update the order of the questions
+     * but the length differs - // TODO update this with linked list
+     */
 
-    if (!containsAll) {
-      return res.status(400).json({
-        message:
-          "The questionIds in the 'questionOrder' property differ from the ones in the stage, please check your request and try again.",
-      });
+    if (req.body.questionOrder) {
+      if (req.body.questionOrder.length !== stage.questionOrder.length) {
+        return res.status(403).json({
+          message:
+            'You cannot add / delete questions this way, please use the proper API methods for those actions',
+        });
+      }
+
+      // Check if the IDs have been modified
+      const containsAll = stage.questionOrder.every((questionId) =>
+        req.body.questionOrder.includes(questionId),
+      );
+
+      if (!containsAll) {
+        return res.status(400).json({
+          message:
+            "The questionIds in the 'questionOrder' property differ from the ones in the stage, please check your request and try again.",
+        });
+      }
+
+      // TODO vulnerability here as a questionId that doesn't exist could be added
+      stage.questionOrder = req.body.questionOrder;
     }
 
-    updatedValues.questionOrder = req.body.questionOrder;
+    if (req.body.GSI1SK) {
+      // TODO update this to be name
+      stage.name = req.body.GSI1SK;
+    }
+
+    try {
+      await stage.save();
+      return res.status(200).json({ message: 'Stage updated!' });
+    } catch (error) {
+      return res.status(500).json({ message: 'An error ocurred updating the stage' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving stage info' });
   }
-
-  if (req.body.GSI1SK) {
-    updatedValues.GSI1SK = req.body.GSI1SK;
-  }
-
-  const [updatedStage, updateError] = await DB.Stages.updateStage({
-    orgId: user.org,
-    openingId,
-    stageId,
-    updatedValues,
-  });
-
-  if (updateError) {
-    const { status, body } = CreateError.SDK(updateError, 'An error ocurred updating this stage');
-    return res.status(status).json(body);
-  }
-
-  return res.status(200).json({
-    message: 'Stage updated!',
-  });
 };
