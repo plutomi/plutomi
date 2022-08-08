24c7e4f541569739bd198ee3b6284115de911a4a
Jose Valerio <contact@josevalerio.com>
2022-08-08 00:55:38 -0400

    Updating webhook

--- Controllers/Webhooks/updateWebhook.ts
+++ Controllers/Webhooks/updateWebhook.ts
@@ -4,6 +4,7 @@ import * as CreateError from '../../utils/createError';
 import { JOI_SETTINGS, LIMITS } from '../../Config';
 import { DynamoWebhook } from '../../types/dynamo';
 import { DB } from '../../models';
+import { Webhook } from '../../entities/Webhooks';
 
 export interface APIUpdateWebhookOptions
   extends Partial<Pick<DynamoWebhook, 'webhookName' | 'webhookUrl' | 'description'>> {}
@@ -26,30 +27,30 @@ export const updateWebhook = async (req: Request, res: Response) => {
   const { user } = req;
   const { webhookId } = req.params;
 
-  if (req.body.webhookUrl) {
-    updatedValues.webhookUrl = req.body.webhookUrl;
+  try {
+    const webhook = await Webhook.findById(webhookId, {
+      org: user.org,
+    });
+
+    if (!webhook) {
+      return res.status(404).json({ message: 'Webhook not found' });
+    }
+
+    if (req.body.webhookUrl) {
+      webhook.url = req.body.webhookUrl;
+    }
+
+    if (req.body.webhookName) {
+      webhook.name = req.body.webhookName;
+    }
+
+    if (req.body.description || req.body.description === '') {
+      webhook.description = req.body.description;
+    }
+
+    await webhook.save();
+    return res.status(200).json({ message: 'Webhook updated!' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to retrieve webhook data' });
   }
-
-  if (req.body.webhookName) {
-    updatedValues.webhookName = req.body.webhookName;
-  }
-
-  if (req.body.description || req.body.description === '') {
-    updatedValues.description = req.body.description;
-  }
-
-  const [updatedWebhook, error] = await DB.Webhooks.updateWebhook({
-    webhookId,
-    orgId: user.org,
-    updatedValues,
-  });
-
-  if (error) {
-    const { status, body } = CreateError.SDK(error, 'An error ocurred updating this webhook');
-    return res.status(status).json(body);
-  }
-
-  return res.status(200).json({
-    message: 'Webhook updated!',
-  });
 };
