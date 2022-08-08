282d7ab73276bbc95f9cbc08453d4b4001e57b9f
Jose Valerio <contact@josevalerio.com>
2022-08-08 01:00:03 -0400

    Deleting webhooks

--- Controllers/Webhooks/deleteWebhook.ts
+++ Controllers/Webhooks/deleteWebhook.ts
@@ -1,24 +1,46 @@
 import { Request, Response } from 'express';
-import { DB } from '../../models';
-import * as CreateError from '../../utils/createError';
+import { Org } from '../../entities/Org';
+import { Webhook } from '../../entities/Webhooks';
 
 export const deleteWebhook = async (req: Request, res: Response) => {
   const { user } = req;
 
-  const [success, failure] = await DB.Webhooks.deleteWebhook({
-    orgId: user.org,
-    webhookId: req.params.webhookId,
-    updateOrg: true,
-  });
+  try {
+    const webhook = await Webhook.findById(req.params.webhookId, {
+      org: user.org,
+    });
 
-  if (failure) {
-    if (failure.name === 'TransactionCanceledException') {
-      return res.status(401).json({ message: 'It seems like that webhook no longer exists' });
+    if (!webhook) {
+      return res.status(404).json({ message: 'Webhook no longer exists' });
     }
 
-    const { status, body } = CreateError.SDK(failure, 'An error ocurred deleting that webhook');
-    return res.status(status).json(body);
-  }
+    try {
+      await Webhook.deleteOne({
+        _id: webhook._id,
+        org: user.org,
+      });
 
-  return res.status(200).json({ message: 'Webhook deleted!' });
+      try {
+        await Org.updateOne(
+          {
+            _id: user.org,
+          },
+          {
+            $inc: {
+              totalWebhooks: -1,
+            },
+          },
+        );
+        return res.status(200).json({ message: 'Webhook deleted!' });
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: 'Webhook deleted but unable to decrement webhook count on org' });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Unable to delete webhook from org' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving webhook info' });
+  }
 };
