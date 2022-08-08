4d2d7cc56230f15f23e4e6c6581ea94fa34b766a
Jose Valerio <contact@josevalerio.com>
2022-08-08 00:52:54 -0400

    Returning webhooks in org

--- Controllers/Webhooks/getWebhooksInOrg.ts
+++ Controllers/Webhooks/getWebhooksInOrg.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from 'express';
+import { Webhook } from '../../entities/Webhooks';
 import { DB } from '../../models';
 import * as CreateError from '../../utils/createError';
 
 export const getWebhooksInOrg = async (req: Request, res: Response) => {
   const { user } = req;
-  const [webhooks, webhooksError] = await DB.Webhooks.getWebhooksInOrg({
-    orgId: user.org,
-  });
 
-  if (webhooksError) {
-    const { status, body } = CreateError.SDK(webhooksError, 'An error ocurred retrieving webhooks');
-
-    return res.status(status).json(body);
+  try {
+    const webhooks = await Webhook.find({
+      org: user.org,
+    });
+    return res.status(200).json(webhooks);
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to retrieve webhooks in org' });
   }
-
-  return res.status(200).json(webhooks);
 };
