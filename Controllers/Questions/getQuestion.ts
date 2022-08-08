faaf2b883b250b07187e1700c402faeab7267926
Jose Valerio <contact@josevalerio.com>
2022-08-07 22:52:27 -0400

    Get question by id

--- Controllers/Questions/getQuestion.ts
+++ Controllers/Questions/getQuestion.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from 'express';
+import { Question } from '../../entities/Question';
 import { DB } from '../../models';
 import * as CreateError from '../../utils/createError';
 
 export const getQuestion = async (req: Request, res: Response) => {
   const { user } = req;
   const { questionId } = req.params;
-  const [question, questionError] = await DB.Questions.getQuestion({
-    orgId: user.org,
-    questionId,
-  });
 
-  if (questionError) {
-    const { status, body } = CreateError.SDK(
-      questionError,
-      'An error ocurred retrieving that question',
-    );
-    return res.status(status).json(body);
+  try {
+    const question = await Question.findById(questionId, {
+      org: user.org,
+    });
+
+    if (!question) {
+      return res.status(404).json({ message: 'Question not found' });
+    }
+
+    return res.status(200).json(question);
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving that question' });
   }
-  return res.status(200).json(question);
 };
