f60f05f8c7ec07ed95f64dd9b266ed03ecf5714a
Jose Valerio <contact@josevalerio.com>
2022-08-08 00:05:21 -0400

    Get stage info

--- Controllers/Stages/getStage.ts
+++ Controllers/Stages/getStage.ts
@@ -1,29 +1,23 @@
 import { Request, Response } from 'express';
+import { Stage } from '../../entities/Stage';
 import { DB } from '../../models';
 import * as CreateError from '../../utils/createError';
 
 export const getStage = async (req: Request, res: Response) => {
   const { user } = req;
-
   const { openingId, stageId } = req.params;
-  const [stage, stageError] = await DB.Stages.getStage({
-    orgId: user.org,
-    stageId,
-    openingId,
-  });
 
-  if (stageError) {
-    const { status, body } = CreateError.SDK(
-      stageError,
-      'An error ocurred retrieving that stage info',
-    );
+  try {
+    const stage = await Stage.findById(stageId, {
+      org: user.org,
+      openingId,
+    });
 
-    return res.status(status).json(body);
+    if (!stage) {
+      return res.status(404).json({ message: 'Stage not found' });
+    }
+    return res.status(200).json(stage);
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving stage info' });
   }
-
-  if (!stage) {
-    return res.status(404).json({ message: 'Stage not found' });
-  }
-
-  return res.status(200).json(stage);
 };
