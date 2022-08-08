36d58199161ad11fd95728234c6e073015ceec9e
Jose Valerio <contact@josevalerio.com>
2022-08-08 00:17:15 -0400

    Route to retrieve stages form opening

--- Controllers/Stages/getStagesInOpening.ts
+++ Controllers/Stages/getStagesInOpening.ts
@@ -1,43 +1,42 @@
 import { Request, Response } from 'express';
+import { Opening } from '../../entities/Opening';
+import { Stage } from '../../entities/Stage';
 import { DB } from '../../models';
 import * as CreateError from '../../utils/createError';
 
 export const getStagesInOpening = async (req: Request, res: Response) => {
   const { user } = req;
-
   const { openingId } = req.params;
 
-  const [opening, openingError] = await DB.Openings.getOpening({
-    openingId,
-    orgId: user.org,
-  });
+  try {
+    const opening = await Opening.findById(openingId, {
+      org: user.org,
+    });
 
-  if (openingError) {
-    const { status, body } = CreateError.SDK(
-      openingError,
-      'An error ocurred getting your opening info',
-    );
-    return res.status(status).json(body);
+    if (!opening) {
+      return res.status(404).json({ message: 'Opening does not exist' });
+    }
+
+    if (!opening.stageOrder.length) {
+      return res.status(200).json([]);
+    }
+
+    try {
+      const allStages = await Promise.all(
+        opening.stageOrder.map(
+          async (stage) =>
+            await Stage.findById(stage, {
+              org: user.org,
+              openingId: opening._id,
+            }),
+        ),
+      );
+      return res.status(200).json(allStages);
+    } catch (error) {
+      return res.status(500).json({ message: 'An error ocurred retrieving stage info' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving opening info' });
   }
 
-  if (!opening) {
-    return res.status(404).json({ message: 'Opening does not exist' });
-  }
-  const [allCurrentStages, allStagesError] = await DB.Stages.getStagesInOpening({
-    openingId,
-    orgId: user.org,
-    stageOrder: opening.stageOrder, // To order them correctly
-  });
-
-  if (allStagesError) {
-    console.log('All stages error', allStagesError);
-    const { status, body } = CreateError.SDK(
-      allStagesError,
-      'An error ocurred retrieving all the current stages',
-    );
-
-    return res.status(status).json(body);
-  }
-
-  return res.status(200).json(allCurrentStages);
 };
