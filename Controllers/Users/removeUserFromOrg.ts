f9437c31633b6e2d459bca017c729eb945fb7a7c
Jose Valerio <contact@josevalerio.com>
2022-08-08 00:41:48 -0400

    User removed from org

--- Controllers/Users/removeUserFromOrg.ts
+++ Controllers/Users/removeUserFromOrg.ts
@@ -1,56 +1,68 @@
 import { Request, Response } from 'express';
 import * as CreateError from '../../utils/createError';
 import { DB } from '../../models';
+import { Schema } from 'mongoose';
+import { Org } from '../../entities/Org';
+import { DEFAULTS } from '../../Config';
+import { User } from '../../entities/User';
 
 export const removeUserFromOrg = async (req: Request, res: Response) => {
   const { user } = req;
   const { orgId, userId } = req.params;
 
-  if (userId === user.userId) {
+  // TODO types
+  if ((userId as unknown as Schema.Types.ObjectId) === user._id) {
     return res.status(403).json({
       message:
         "You cannot remove yourself from an org. If you're the only user, delete the org instead",
     });
   }
-  const [org, orgError] = await DB.Orgs.getOrg({
-    orgId,
-  });
 
-  if (orgError) {
-    const { status, body } = CreateError.SDK(
-      orgError,
-      'An error ocurred retrieving the info for this org',
-    );
+  try {
+    const org = await Org.findById(user.org);
 
-    return res.status(status).json(body);
-  }
-
-  if (!org) {
-    return res.status(404).json({ message: `No org found with id of '${orgId}` });
-  }
-
-  const [removed, removeError] = await DB.Users.removeUserFromOrg({
-    // if this doesn't match the createdBy ID on the org,
-    // Dynamo will error
-    createdById: user.userId,
-    orgId: org.orgId,
-    userId,
-  });
-
-  if (removeError) {
-    // TODO there are two conditional checks, we should narrow this down
-    if (removeError.name === 'TransactionCanceledException') {
-      return res.status(403).json({
-        message: 'You must be the creator of the org to remove users',
-      });
+    if (!org) {
+      return res.status(404).json({ message: 'Org not found' });
     }
 
-    const { status, body } = CreateError.SDK(
-      removeError,
-      'An error ocurred removing this user form your org',
-    );
-    return res.status(status).json(body);
-  }
+    // TODO update user
 
-  return res.status(200).json({ message: 'User removed!' });
+    // TODO needs transaction
+    try {
+      await User.updateOne(
+        {
+          _id: user._id,
+        },
+        {
+          $set: {
+            org: DEFAULTS.NO_ORG,
+          },
+        },
+      );
+
+      try {
+        await Org.updateOne(
+          {
+            _id: org._id,
+          },
+          {
+            $inc: {
+              totalApplicants: -1,
+            },
+          },
+        );
+        return res.status(200).json({ message: 'User removed from org!' });
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: 'An error ocurred decrementing the totalUsers property on org' });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'An error ocurred updating orgId for user' });
+    }
+
+    // TODO decrement total users in or
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving org info' });
+  }
 };
