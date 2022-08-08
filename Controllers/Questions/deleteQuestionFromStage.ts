33ebb15aeb253d64fb33c53545c9ad1b7f536e76
Jose Valerio <contact@josevalerio.com>
2022-08-07 23:33:09 -0400

    Added removing question from stage

--- Controllers/Questions/deleteQuestionFromStage.ts
+++ Controllers/Questions/deleteQuestionFromStage.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import { Stage } from '../../entities/Stage';
+import { StageQuestionAdjacentItem } from '../../entities/StageQuestionAdjacentItem';
 import { DB } from '../../models';
 
 import * as CreateError from '../../utils/createError';
@@ -6,48 +8,67 @@ import * as CreateError from '../../utils/createError';
 export const deleteQuestionFromStage = async (req: Request, res: Response) => {
   const { user } = req;
   const { openingId, stageId, questionId } = req.params;
-  const [stage, error] = await DB.Stages.getStage({
-    openingId,
-    stageId,
-    orgId: user.org,
-  });
 
-  if (error) {
-    const { status, body } = CreateError.SDK(
-      error,
-      'An error ocurred deleting that question from this stage',
-    );
-    return res.status(status).json(body);
-  }
-
-  if (!stage) {
-    return res.status(400).json({ message: 'Stage not found' });
-  }
-
-  // TODO add a test for this
-  if (!stage.questionOrder.includes(questionId)) {
-    return res.status(400).json({
-      message: `The question ID '${questionId}' does not exist in this stage`,
+  try {
+    const stage = await Stage.findById(stageId, {
+      org: user.org,
+      openingId,
     });
+
+    if (!stage) {
+      return res.status(404).json({ message: 'Stage not found' });
+    }
+
+    // TODO delete question from stage
+
+    const questionToRemoveIndex = stage.questionOrder.indexOf(questionId);
+    if (questionToRemoveIndex < 0) {
+      return res.status(400).json({ message: 'Question not found on stage ' });
+    }
+
+    const newQuestionOrder = stage.questionOrder.splice(questionToRemoveIndex, 1);
+
+    // TODo needs transaction
+    try {
+      await Stage.updateOne(
+        {
+          _id: stageId,
+          org: user.org,
+          openingId,
+        },
+        {
+          $set: {
+            questionOrder: newQuestionOrder,
+          },
+          $inc: {
+            totalQuestions: -1,
+          },
+        },
+      );
+
+      try {
+        await StageQuestionAdjacentItem.deleteOne({
+          stageId,
+          questionId,
+        });
+        return res.status(200).json({ message: 'Question removed from stage!' });
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: 'An error ocurred deleting the adjacent stage / question item' });
+      }
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: 'An error ocurred removing the question from the stage' });
+    }
+
+    try {
+      return res.status(200).json({ message: 'Question removed from stage!' });
+    } catch (error) {
+      return res.status(500).json({ message: 'An error ocurred deleting question from stage' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'An error ocurred retrieving stage info' });
   }
-
-  // Remove that question
-  const [updated, updateError] = await DB.Questions.deleteQuestionFromStage({
-    openingId,
-    stageId,
-    questionId,
-    orgId: user.org,
-    deleteIndex: stage.questionOrder.indexOf(questionId),
-    decrementStageCount: true,
-  });
-
-  if (updateError) {
-    const { status, body } = CreateError.SDK(
-      updateError,
-      'An error ocurred deleting that question',
-    );
-    return res.status(status).json(body);
-  }
-
-  return res.status(200).json({ message: 'Question removed from stage!' });
 };
