8d8c1142036650858e1d94fdfa5ab7ae09741b27
Jose Valerio <contact@josevalerio.com>
2022-09-04 22:27:08 -0400

    test comment

--- Controllers/Misc/index.ts
+++ Controllers/Misc/index.ts
@@ -2,4 +2,5 @@ import { jestSetup } from './jest-setup';
 import { haltOnTimeout } from './timeout';
 import { healthCheck } from './healthCheck';
 
+// test
 export { jestSetup, healthCheck, haltOnTimeout };
