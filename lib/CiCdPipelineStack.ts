42ffd5444404dccabe452bb89bc1edd224b04501
Jose Valerio <contact@josevalerio.com>
2022-09-05 01:39:35 -0400

    Building

--- lib/CiCdPipelineStack.ts
+++ lib/CiCdPipelineStack.ts
@@ -23,11 +23,13 @@ export default class AppStack extends cdk.Stack {
       'plutomi/development/env',
     );
 
+    // TODO cdk synth runs build beforehand
+
     new CodePipeline(this, `${process.env.NODE_ENV}-CiCdPipeline`, {
       pipelineName: `${process.env.NODE_ENV}-CiCdPipeline`, //
       synth: new ShellStep(`${process.env.NODE_ENV}-PlutomiSynth`, {
         input: CodePipelineSource.gitHub(`plutomi/plutomi`, 'cicd'), // TODO create new prod branch
-        commands: [`npm ci`, 'ls', `npx cdk synth`, 'ls', `npm run build`], // TODO remove ls, only for checking where cdk.out is
+        commands: [`npm ci`, 'ls', 'docker-cdk-test', `npx cdk synth`, 'ls', `npm run build`], // TODO remove ls, only for checking where cdk.out is
         // primaryOutputDirectory: 'mysubdir/cdk.out',
       }),
       codeBuildDefaults: {
