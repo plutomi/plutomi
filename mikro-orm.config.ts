79ebfdc52fb57da4398f094e4bd80b72e83cc2bb
Jose Valerio <contact@josevalerio.com>
2022-10-13 00:01:58 -0400

    Connection pooling

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -12,6 +12,10 @@ const mikroOrmOptions: Options<MongoDriver> = {
   highlighter: new MongoHighlighter(),
   debug: true,
   ensureIndexes: true,
+  pool: {
+    min: 20,
+    max: 50,
+  },
   clientUrl:
     'mongodb+srv://IupGDoGWC2J24qK0RWYi0X5Tzqk9qqc1sLXc:ywSUZLI1wmevGTCf8cjZXuu2tgnVnOHSx79V7MmW6icoVeeTrE@staging.pd4tt.mongodb.net/?retryWrites=true&w=majority',
 };
