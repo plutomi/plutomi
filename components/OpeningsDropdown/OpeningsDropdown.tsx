078413462e48f3c7b63106f2da22511b1e4e69ea
Jose Valerio <contact@josevalerio.com>
2022-10-14 19:35:04 -0400

    Cleanup

--- components/OpeningsDropdown/OpeningsDropdown.tsx
+++ components/OpeningsDropdown/OpeningsDropdown.tsx
@@ -4,16 +4,21 @@ import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 import { useRouter } from 'next/router';
 import { OpeningState, WEBSITE_URL } from '../../Config';
 import combineClassNames from '../../utils/combineClassNames';
-import { DynamoOpening } from '../../types/dynamo';
+import { Opening } from '../../entities';
+import { findInTargetArray } from '../../utils/findInTargetArray';
+import { IndexedEntities } from '../../types/main';
 
 interface OpeningsDropdownProps {
-  openings: DynamoOpening[];
+  openings: Opening[];
   index: number;
 }
 
 export const OpeningsDropdown = ({ openings, index }: OpeningsDropdownProps) => {
   const router = useRouter();
   const [selected, setSelected] = useState(openings[index]);
+
+  // TODO handle change not working
+  alert(`StageOrder on openings is disabled!`);
 
   const handleChange = (newValue) => {
     if (selected === newValue) {
@@ -28,19 +33,25 @@ export const OpeningsDropdown = ({ openings, index }: OpeningsDropdownProps) => 
     }
     router.push(`${WEBSITE_URL}/openings/${newValue.openingId}/settings`);
   };
+
+  const openingState = findInTargetArray({
+    entity: IndexedEntities.OpeningState,
+    targetArray: selected.target,
+  });
+
   return (
     <Listbox value={selected} onChange={handleChange}>
       <div className=" relative w-full ">
         <Listbox.Button className="relative w-full curse-pointer bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-lg">
           <div className="flex items-center">
             <span
-              aria-label={selected?.GSI1SK === OpeningState.PUBLIC ? 'Online' : 'Offline'}
+              aria-label={openingState === OpeningState.PUBLIC ? 'Online' : 'Offline'}
               className={combineClassNames(
-                selected?.GSI1SK === OpeningState.PUBLIC ? 'bg-green-400' : 'bg-gray-200',
+                openingState === OpeningState.PUBLIC ? 'bg-green-400' : 'bg-gray-200',
                 'flex-shrink-0 inline-block h-2 w-2 rounded-full',
               )}
             />
-            <span className="ml-3 block truncate">{selected?.openingName}</span>
+            <span className="ml-3 block truncate">{selected?.name}</span>
           </div>
           <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
             <SelectorIcon className="h-5 w-5 text-light" aria-hidden="true" />
@@ -56,7 +67,7 @@ export const OpeningsDropdown = ({ openings, index }: OpeningsDropdownProps) => 
           <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-md ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-lg">
             {openings.map((opening) => (
               <Listbox.Option
-                key={opening.openingId}
+                key={opening.id}
                 className={({ active }) =>
                   combineClassNames(
                     active ? 'text-white bg-blue-600' : 'text-dark',
@@ -65,42 +76,51 @@ export const OpeningsDropdown = ({ openings, index }: OpeningsDropdownProps) => 
                 }
                 value={opening}
               >
-                {({ selected, active }) => (
-                  <>
-                    <div className="flex items-center">
-                      <span
-                        className={combineClassNames(
-                          opening.GSI1SK === OpeningState.PUBLIC ? 'bg-green-400' : 'bg-gray-200',
-                          'flex-shrink-0 inline-block h-2 w-2 rounded-full',
-                        )}
-                        aria-hidden="true"
-                      />
-                      <span
-                        className={combineClassNames(
-                          selected ? 'font-semibold' : 'font-normal',
-                          'ml-3 block truncate',
-                        )}
-                      >
-                        {opening?.openingName}
-                        <span className="sr-only">
-                          {' '}
-                          is {opening.GSI1SK === OpeningState.PUBLIC ? 'online' : 'offline'}
+                {({ selected, active }) => {
+                  const currentOpening = opening;
+                  const currentOpeningState = findInTargetArray({
+                    entity: IndexedEntities.OpeningState,
+                    targetArray: currentOpening.target,
+                  });
+                  return (
+                    <>
+                      <div className="flex items-center">
+                        <span
+                          className={combineClassNames(
+                            currentOpeningState === OpeningState.PUBLIC
+                              ? 'bg-green-400'
+                              : 'bg-gray-200',
+                            'flex-shrink-0 inline-block h-2 w-2 rounded-full',
+                          )}
+                          aria-hidden="true"
+                        />
+                        <span
+                          className={combineClassNames(
+                            selected ? 'font-semibold' : 'font-normal',
+                            'ml-3 block truncate',
+                          )}
+                        >
+                          {opening.name}
+                          <span className="sr-only">
+                            {' '}
+                            is {currentOpeningState === OpeningState.PUBLIC ? 'online' : 'offline'}
+                          </span>
                         </span>
-                      </span>
-                    </div>
+                      </div>
 
-                    {selected && (
-                      <span
-                        className={combineClassNames(
-                          active ? 'text-white' : 'text-blue-600',
-                          'absolute inset-y-0 right-0 flex items-center pr-4',
-                        )}
-                      >
-                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                      </span>
-                    )}
-                  </>
-                )}
+                      {selected && (
+                        <span
+                          className={combineClassNames(
+                            active ? 'text-white' : 'text-blue-600',
+                            'absolute inset-y-0 right-0 flex items-center pr-4',
+                          )}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      )}
+                    </>
+                  );
+                }}
               </Listbox.Option>
             ))}
           </Listbox.Options>
