39e9f98f44a3e5b07bc8772fb3f90bd6ba8f40c2
Jose Valerio <contact@josevalerio.com>
2022-10-26 00:55:06 -0400

    Get org

--- Controllers/Orgs/getOrg.ts
+++ Controllers/Orgs/getOrg.ts
@@ -1,28 +1,34 @@
 import { Request, Response } from 'express';
-// import { Org } from '../../entities';
+import { OrgEntity } from '../../models';
+import { Filter } from 'mongodb';
 import { findInTargetArray } from '../../utils/findInTargetArray';
+import { IndexableProperties } from '../../@types/indexableProperties';
+import { collections } from '../../utils/connectToDatabase';
 
 export const getOrg = async (req: Request, res: Response) => {
   const { user } = req;
-  return res.status(200).json({ message: 'Endpoint temp disabled' });
 
-  // let org: Org;
+  let org: OrgEntity;
 
-  // const orgId = findInTargetArray({ entity: IdxTypes.Org, targetArray: user.target });
+  const orgId = findInTargetArray(IndexableProperties.Org, user);
 
-  // try {
-  //   org = await entityManager.findOne(Org, {
-  //     orgId: orgId,
-  //   });
-  // } catch (error) {
-  //   console.error(`An error ocurred returning org info`, error);
-  //   return res.status(500).json({ message: 'An error ocurred returning org info', error });
-  // }
+  if (!orgId || !user.orgJoinDate) {
+    return res.status(400).json({ message: "You don't appear to be in an org!" });
+  }
 
-  // // Not sure how this would be possible but :)
-  // if (!org) {
-  //   return res.status(404).json({ message: 'Org not found' });
-  // }
+  const orgFilter: Filter<OrgEntity> = {
+    target: { property: IndexableProperties.Id, value: orgId },
+  };
 
-  // return res.status(200).json(org);
+  try {
+    org = (await collections.orgs.findOne(orgFilter)) as OrgEntity;
+  } catch (error) {
+    const message = `An error ocurred returning org info`;
+    console.error(message, error);
+    return res.status(500).json({ message, error });
+  }
+
+  if (!org) return res.status(404).json({ message: 'Org not found' });
+
+  return res.status(200).json(org);
 };
