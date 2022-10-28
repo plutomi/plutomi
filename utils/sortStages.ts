47c84dcecb23f21daa3b6f0623afbb3f781f3501
Jose Valerio <contact@josevalerio.com>
2022-10-27 21:43:54 -0400

    Fixed sorting algo

--- utils/sortStages.ts
+++ utils/sortStages.ts
@@ -1,4 +1,5 @@
 import { IndexableProperties } from '../@types/indexableProperties';
+import { StageEntity } from '../models';
 import { findInTargetArray } from './findInTargetArray';
 
 interface GetAdjacentStagesBasedOnPositionProps {
@@ -7,7 +8,7 @@ interface GetAdjacentStagesBasedOnPositionProps {
   /**
    * This must be a *SORTED* list of the other stages
    */
-  otherSortedStages: Stage[];
+  otherSortedStages: StageEntity[];
   stageIdBeingMoved: string;
 }
 interface AdjacentStagesResult {
@@ -15,12 +16,12 @@ interface AdjacentStagesResult {
   newPreviousStageId: string | undefined;
 }
 
-export const sortStages = (unsortedStagesInOpening: Stage[]): Stage[] => {
+export const sortStages = (unsortedStagesInOpening: StageEntity[]): StageEntity[] => {
   // No need to sort on these conditions
   if (!unsortedStagesInOpening.length) return [];
   if (unsortedStagesInOpening.length === 1) return unsortedStagesInOpening;
 
-  let firstStage: Stage;
+  let firstStage: StageEntity;
   let firstStageIndex: number;
 
   unsortedStagesInOpening.find((stage, idx) => {
@@ -32,16 +33,17 @@ export const sortStages = (unsortedStagesInOpening: Stage[]): Stage[] => {
     }
   });
 
-  const sortedStages: Stage[] = [];
+  const sortedStages: StageEntity[] = [];
   sortedStages.push(firstStage);
 
   // Remove the first stage from the unsorted list, it is no longer needed
   unsortedStagesInOpening.splice(firstStageIndex, 1);
 
   // Push all but the first stage into an object so we can get *almost* O(1) queries
-  const mapWithStages: Record<string, Stage> = {};
+  const mapWithStages: Record<string, StageEntity> = {};
   unsortedStagesInOpening.map((stage) => {
-    mapWithStages[stage.id] = stage;
+    const stageId = findInTargetArray(IndexableProperties.Id, stage);
+    mapWithStages[stageId] = stage;
   });
 
   let reachedTheEnd = false;
@@ -50,7 +52,7 @@ export const sortStages = (unsortedStagesInOpening: Stage[]): Stage[] => {
   while (!reachedTheEnd) {
     const newNextStageId = findInTargetArray(IndexableProperties.NextStage, startingStage);
 
-    let nextStage: Stage | undefined;
+    let nextStage: StageEntity | undefined;
 
     if (newNextStageId) {
       nextStage = mapWithStages[newNextStageId.toString()];
@@ -73,17 +75,22 @@ export const getAdjacentStagesBasedOnPosition = ({
 }: GetAdjacentStagesBasedOnPositionProps): AdjacentStagesResult => {
   if (!position && position !== 0) {
     // Position not provided, add it to the end
+    const stage = otherSortedStages[otherSortedStages.length - 1];
+    const stageId = stage ? findInTargetArray(IndexableProperties.Id, stage) : undefined;
+
     return {
       newNextStageId: undefined,
-      newPreviousStageId: otherSortedStages[otherSortedStages.length - 1]?.id,
+      newPreviousStageId: stageId,
     };
   }
 
   if (position === 0) {
     // Stage wants to be first
+    const stage = otherSortedStages[0];
+    const stageId = stage ? findInTargetArray(IndexableProperties.Id, stage) : undefined;
     return {
       newPreviousStageId: undefined,
-      newNextStageId: otherSortedStages[0]?.id,
+      newNextStageId: stageId,
     };
   }
 
@@ -91,7 +98,10 @@ export const getAdjacentStagesBasedOnPosition = ({
    * ! Note: We are not allowing stage updates if the stage is dropped in the same position as it's redundant.
    * if (currentIndex === position)
    */
-  const currentIndex = otherSortedStages.findIndex((item) => item.id === stageIdBeingMoved);
+  const currentIndex = otherSortedStages.findIndex((stage) => {
+    const stageId = findInTargetArray(IndexableProperties.Id, stage);
+    stageId === stageIdBeingMoved;
+  });
 
   /**
    * We have to check if we are moving the stage:
@@ -109,9 +119,20 @@ export const getAdjacentStagesBasedOnPosition = ({
    */
 
   if (position > currentIndex) {
+    const newPreviousStage = otherSortedStages[position];
+    const newNextStage = otherSortedStages[position + 1];
+
+    const newPreviousStageId = newPreviousStage
+      ? findInTargetArray(IndexableProperties.Id, newPreviousStage)
+      : undefined;
+
+    const newNextStageId = newNextStage
+      ? findInTargetArray(IndexableProperties.Id, newNextStage)
+      : undefined;
+
     return {
-      newPreviousStageId: otherSortedStages[position]?.id,
-      newNextStageId: otherSortedStages[position + 1]?.id,
+      newPreviousStageId,
+      newNextStageId,
     };
   }
 
@@ -125,9 +146,20 @@ export const getAdjacentStagesBasedOnPosition = ({
    * Stage 3 --- Stage 1
    */
   if (position < currentIndex) {
+    const newPreviousStage = otherSortedStages[position - 1];
+    const newNextStage = otherSortedStages[position];
+
+    const newPreviousStageId = newPreviousStage
+      ? findInTargetArray(IndexableProperties.Id, newPreviousStage)
+      : undefined;
+
+    const newNextStageId = newNextStage
+      ? findInTargetArray(IndexableProperties.Id, newNextStage)
+      : undefined;
+
     return {
-      newPreviousStageId: otherSortedStages[position - 1]?.id,
-      newNextStageId: otherSortedStages[position]?.id,
+      newPreviousStageId,
+      newNextStageId,
     };
   }
 };
