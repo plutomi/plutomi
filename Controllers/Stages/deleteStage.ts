1effce0d2ac8af5329234c90e469fa2b859471d7
Jose Valerio <contact@josevalerio.com>
2022-10-27 22:20:15 -0400

    Delete stage saving

--- Controllers/Stages/deleteStage.ts
+++ Controllers/Stages/deleteStage.ts
@@ -1,147 +1,149 @@
 import { Request, Response } from 'express';
+import { Filter } from 'mongodb';
+import { IndexableProperties } from '../../@types/indexableProperties';
+import { StageEntity } from '../../models';
+import { OpeningEntity } from '../../models/Opening';
+import { collections } from '../../utils/connectToDatabase';
 // import { Opening, Stage } from '../../entities';
 import { findInTargetArray } from '../../utils/findInTargetArray';
 
 export const deleteStage = async (req: Request, res: Response) => {
   const { user } = req;
   const { openingId, stageId } = req.params;
-  // const orgId = findInTargetArray({ entity: IdxTypes.Org, targetArray: user.target });
+  const orgId = findInTargetArray(IndexableProperties.Org, user);
 
-  return res.status(200).json({ message: 'Endpoint temp disabled' });
-  // let opening: Opening;
+  let opening: OpeningEntity;
 
-  // try {
-  //   opening = await entityManager.findOne(Opening, {
-  //     id: openingId,
-  //     $and: [{ target: { id: orgId, type: IdxTypes.Org } }],
-  //   });
-  // } catch (error) {
-  //   const message = 'An error ocurred finding opening info';
-  //   console.error(message, error);
-  //   return res.status(500).json({ message, error });
-  // }
+  const openingFilter: Filter<OpeningEntity> = {
+    $and: [
+      { target: { property: IndexableProperties.Id, value: openingId } },
+      { target: { property: IndexableProperties.Org, value: orgId } },
+    ],
+  };
+  try {
+    opening = (await collections.openings.findOne(openingFilter)) as OpeningEntity;
+  } catch (error) {
+    const message = 'An error ocurred finding opening info';
+    console.error(message, error);
+    return res.status(500).json(message);
+  }
 
-  // if (!opening) {
-  //   return res.status(404).json({
-  //     message: `Hmm... it appears that the opening with ID of '${openingId}' no longer exists`,
-  //   });
-  // }
+  if (!opening) {
+    return res.status(404).json({
+      message: `Hmm... it appears that the opening with ID of '${openingId}' no longer exists`,
+    });
+  }
 
-  // let ourStage: Stage;
+  let ourStage: StageEntity;
 
-  // try {
-  //   ourStage = await entityManager.findOne(Stage, {
-  //     id: stageId,
-  //     $and: [
-  //       { target: { id: orgId, type: IdxTypes.Org } },
-  //       { target: { id: opening.id, type: IdxTypes.Opening } },
-  //     ],
-  //   });
-  // } catch (error) {
-  //   const message = 'An error ocurred finding the stage info';
-  //   console.error(message, error);
-  //   return res.status(500).json({ message, error });
-  // }
+  const stageFilter: Filter<StageEntity> = {
+    $and: [
+      { target: { property: IndexableProperties.Opening, value: openingId } },
+      { target: { property: IndexableProperties.Org, value: orgId } },
+      { target: { property: IndexableProperties.Id, value: stageId } },
+    ],
+  };
+  try {
+    ourStage = (await collections.stages.findOne(stageFilter)) as StageEntity;
+  } catch (error) {
+    const message = 'An error ocurred finding the stage info';
+    console.error(message, error);
+    return res.status(500).json(message);
+  }
 
-  // if (!ourStage) {
-  //   return res.status(404).json({
-  //     message: `Hmm... it appears that the stage with ID of '${stageId}' no longer exists`,
-  //   });
-  // }
+  if (!ourStage) {
+    return res.status(404).json({
+      message: `Hmm... it appears that the stage with ID of '${stageId}' no longer exists`,
+    });
+  }
 
-  // const oldPreviousStageId = findInTargetArray({
-  //   entity: IdxTypes.PreviousStage,
-  //   targetArray: ourStage.target,
-  // });
+  const oldPreviousStageId = findInTargetArray(IndexableProperties.Id, ourStage);
 
-  // const oldNextStageId = findInTargetArray({
-  //   entity: IdxTypes.NextStage,
-  //   targetArray: ourStage.target,
-  // });
+  const oldNextStageId = findInTargetArray(IndexableProperties.NextStage, ourStage);
 
-  // let oldPreviousStage: Stage;
-  // let oldNextStage: Stage;
+  let oldPreviousStage: StageEntity;
+  let oldNextStage: StageEntity;
 
-  // // TODO these can be done in parallel
-  // if (oldPreviousStageId) {
-  //   try {
-  //     oldPreviousStage = await entityManager.findOne(Stage, {
-  //       id: oldPreviousStageId,
-  //       $and: [
-  //         { target: { id: orgId, type: IdxTypes.Org } },
-  //         { target: { id: openingId, type: IdxTypes.Opening } },
-  //       ],
-  //     });
+  // TODO these can be done in parallel
+  if (oldPreviousStageId) {
+    try {
+      oldPreviousStage = await entityManager.findOne(Stage, {
+        id: oldPreviousStageId,
+        $and: [
+          { target: { id: orgId, type: IdxTypes.Org } },
+          { target: { id: openingId, type: IdxTypes.Opening } },
+        ],
+      });
 
-  //     const oldPreviousStageNextStageIndex = oldPreviousStage.target.findIndex(
-  //       (item) => item.type === IdxTypes.NextStage,
-  //     );
+      const oldPreviousStageNextStageIndex = oldPreviousStage.target.findIndex(
+        (item) => item.type === IdxTypes.NextStage,
+      );
 
-  //     if (oldNextStageId) {
-  //       // Set the previous stage's next stage Id to be the stage that is being deleted's next stage Id
-  //       oldPreviousStage.target[oldPreviousStageNextStageIndex] = {
-  //         id: oldNextStageId,
-  //         type: IdxTypes.NextStage,
-  //       };
-  //     } else {
-  //       oldPreviousStage.target[oldPreviousStageNextStageIndex] = {
-  //         id: undefined,
-  //         type: IdxTypes.NextStage,
-  //       };
-  //     }
+      if (oldNextStageId) {
+        // Set the previous stage's next stage Id to be the stage that is being deleted's next stage Id
+        oldPreviousStage.target[oldPreviousStageNextStageIndex] = {
+          id: oldNextStageId,
+          type: IdxTypes.NextStage,
+        };
+      } else {
+        oldPreviousStage.target[oldPreviousStageNextStageIndex] = {
+          id: undefined,
+          type: IdxTypes.NextStage,
+        };
+      }
 
-  //     entityManager.persist(oldPreviousStage);
-  //   } catch (error) {
-  //     const message = 'An error ocurred finding the previous stage info';
-  //     console.error(message, error);
-  //     return res.status(500).json({ message, error });
-  //   }
-  // }
+      entityManager.persist(oldPreviousStage);
+    } catch (error) {
+      const message = 'An error ocurred finding the previous stage info';
+      console.error(message, error);
+      return res.status(500).json({ message, error });
+    }
+  }
 
-  // if (oldNextStageId) {
-  //   try {
-  //     oldNextStage = await entityManager.findOne(Stage, {
-  //       id: oldNextStageId,
-  //       $and: [
-  //         { target: { id: orgId, type: IdxTypes.Org } },
-  //         { target: { id: openingId, type: IdxTypes.Opening } },
-  //       ],
-  //     });
+  if (oldNextStageId) {
+    try {
+      oldNextStage = await entityManager.findOne(Stage, {
+        id: oldNextStageId,
+        $and: [
+          { target: { id: orgId, type: IdxTypes.Org } },
+          { target: { id: openingId, type: IdxTypes.Opening } },
+        ],
+      });
 
-  //     const oldNextStagePreviousStageIndex = oldNextStage.target.findIndex(
-  //       (item) => item.type === IdxTypes.PreviousStage,
-  //     );
+      const oldNextStagePreviousStageIndex = oldNextStage.target.findIndex(
+        (item) => item.type === IdxTypes.PreviousStage,
+      );
 
-  //     if (oldPreviousStage) {
-  //       // Set the next stage's previous stage Id to be the stage that is being deleted's previous stage Id
-  //       oldNextStage.target[oldNextStagePreviousStageIndex] = {
-  //         id: oldPreviousStageId,
-  //         type: IdxTypes.PreviousStage,
-  //       };
-  //     } else {
-  //       oldNextStage.target[oldNextStagePreviousStageIndex] = {
-  //         id: undefined,
-  //         type: IdxTypes.PreviousStage,
-  //       };
-  //     }
+      if (oldPreviousStage) {
+        // Set the next stage's previous stage Id to be the stage that is being deleted's previous stage Id
+        oldNextStage.target[oldNextStagePreviousStageIndex] = {
+          id: oldPreviousStageId,
+          type: IdxTypes.PreviousStage,
+        };
+      } else {
+        oldNextStage.target[oldNextStagePreviousStageIndex] = {
+          id: undefined,
+          type: IdxTypes.PreviousStage,
+        };
+      }
 
-  //     entityManager.persist(oldNextStage);
-  //   } catch (error) {
-  //     const message = 'An error ocurred finding the previous stage info';
-  //     console.error(message, error);
-  //     return res.status(500).json({ message, error });
-  //   }
-  // }
+      entityManager.persist(oldNextStage);
+    } catch (error) {
+      const message = 'An error ocurred finding the previous stage info';
+      console.error(message, error);
+      return res.status(500).json({ message, error });
+    }
+  }
 
-  // entityManager.remove(ourStage);
-  // opening.totalStages - +opening.totalStages - 1;
+  entityManager.remove(ourStage);
+  opening.totalStages - +opening.totalStages - 1;
 
-  // try {
-  //   await entityManager.flush();
-  //   return res.status(200).json({ message: 'Stage deleted!' });
-  // } catch (error) {
-  //   const message = 'Error ocurred deleting stage';
-  //   console.error(message, error);
-  //   return res.status(500).json({ message, error });
-  // }
+  try {
+    await entityManager.flush();
+    return res.status(200).json({ message: 'Stage deleted!' });
+  } catch (error) {
+    const message = 'Error ocurred deleting stage';
+    console.error(message, error);
+    return res.status(500).json({ message, error });
+  }
 };
