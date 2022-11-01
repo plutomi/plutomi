e96bed8e6082c3e924831206d4279d3c98d0da56
Jose Valerio <contact@josevalerio.com>
2022-11-01 02:38:14 -0400

    Cleanup

--- utils/generateId.ts
+++ utils/generateId.ts
@@ -3,9 +3,16 @@ import { customAlphabet } from 'nanoid';
 /**
  * Generates a clean ID with 0-9 and all lower case a-z
  * @param length Length of ID - default 24
+ * @param fullAlphabet Whether to include caps and '-' and '_' keys
  * @returns
  */
-export const generateId = (length?: number) => {
-  const nanoid = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz', length ?? 24);
+export const generateId = (length?: number, fullAlphabet?: boolean) => {
+  let alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+  if (fullAlphabet) {
+    alphabet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+  }
+
+  const nanoid = customAlphabet(alphabet, length ?? 24);
   return nanoid();
 };
