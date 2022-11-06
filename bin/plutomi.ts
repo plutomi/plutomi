62dd31c562e221a77e077da9c3c7072dd92d33e4
Jose Valerio <contact@josevalerio.com>
2022-11-06 18:58:09 -0500

    Removed other stacks

--- bin/plutomi.ts
+++ bin/plutomi.ts
@@ -2,10 +2,6 @@
 import 'source-map-support';
 import * as cdk from 'aws-cdk-lib';
 import AppStack from '../lib/AppStack';
-// TODO bring these back
-import EventBridgeStack from '../lib/EventBridgeStack';
-import DeleteChildrenMachineStack from '../lib/DeleteChildrenMachineStack';
-import WebhooksMachineStack from '../lib/WebhooksMachineStack';
 import StorageStack from '../lib/StorageStack';
 import { env } from '../env';
 
@@ -13,18 +9,3 @@ const app = new cdk.App();
 const { bucket } = new StorageStack(app, `${env.deploymentEnvironment}-StorageStack`);
 
 new AppStack(app, `${env.deploymentEnvironment}-AppStack`, {});
-
-// const { DeleteChildrenMachine } = new DeleteChildrenMachineStack(
-//   app,
-//   `${env.deploymentEnvironment}-DeleteChildrenMachineStack`,
-// );
-
-// const { WebhooksMachine } = new WebhooksMachineStack(
-//   app,
-//   `${env.deploymentEnvironment}-WebhooksMachineStack`,
-// );
-
-// new EventBridgeStack(app, `${env.deploymentEnvironment}-EventBridgeStack`, {
-//   DeleteChildrenMachine,
-//   WebhooksMachine,
-// });
