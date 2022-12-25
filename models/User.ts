2bae6222a08b583fee8171ca883717a21c243a96
Jose Valerio <contact@josevalerio.com>
2022-12-25 03:11:06 +0300

    Added comment clarifying

--- models/User.ts
+++ models/User.ts
@@ -1,14 +1,21 @@
 import { IndexableProperties } from '../@types/indexableProperties';
-import { AllEntities } from '../utils';
+import { AllEntities, EntityPrefix } from '../utils';
 import { BaseEntity } from './Base';
 
+export type UniqueUserId = `${EntityPrefix.User}${string}`;
 export type UserTargetArray = [
   { id: AllEntities.User; type: IndexableProperties.Entity },
-  { id: string; type: IndexableProperties.Id },
+  { id: UniqueUserId; type: IndexableProperties.Id },
   { id: string; type: IndexableProperties.Email },
 ];
 
 export interface UserEntity extends BaseEntity {
+  /**
+   * For most entities, this would be nested like ORG#orgId#APPLICATION#applicationId
+   * Top level items will have duplicate data here and in the uniqueId due to the polymorphic design
+   */
+  _id: UniqueUserId;
+  uniqueId: UniqueUserId;
   org: string | null;
   firstName: string;
   lastName: string;
