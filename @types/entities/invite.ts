efbdbdc246111555954520f79ca0bb9cf167cc9f
Jose Valerio <contact@josevalerio.com>
2023-03-01 23:54:44 -0500

    invites entity

--- @types/entities/invite.ts
+++ @types/entities/invite.ts
@@ -1,30 +1,17 @@
+import { AllEntityNames } from './allEntityNames';
 import { BaseEntity } from './baseEntity';
-
-export type InvitesTargetArray = [
-  { id: AllEntityNames.Invite; type: IndexableProperties.Entity },
-  { id: string; type: IndexableProperties.Id },
-  /**
-   * Get all invites for a given email
-   */
-  { id: string; type: IndexableProperties.Email },
-  /**
-   * Get all items related to this user, ! TODO might not be needed if we create it under the _id field
-   */
-  { id: string; type: IndexableProperties.User },
-];
 
 export interface InviteEntity extends BaseEntity<AllEntityNames.Invite> {
   userId: string;
   org: string;
   /**
-   * Display name for the org
+   * Display name and created by denormalized for convenience
    */
   orgName: string;
   createdBy: {
-    name: string | null;
+    name: string | null; // TODO Why would this ever be null?
     email: string;
   };
   recipientName: string | null;
   expiresAt: Date;
-  target: InvitesTargetArray;
 }
