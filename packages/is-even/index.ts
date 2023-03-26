db5a4832fb8f1b3a569a8e5a5abb3f667d470e2f
Jose Valerio <contact@josevalerio.com>
2023-03-26 16:05:08 -0400

    is even is odd

--- packages/is-even/index.ts
+++ packages/is-even/index.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 export const isEven = (x: number) => {
   const axiosInstance = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
+    baseURL: 'https://api.github.com',
     timeout: 1000,
     headers: { 'X-Custom-Header': 'foobar' },
   });
-  console.log(typeof axiosInstance);
+
   return x % 2 === 0;
 };
