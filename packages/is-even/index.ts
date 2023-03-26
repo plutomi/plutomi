404e4b4b904911af7ba836a03ad6d1499068f2c4
Jose Valerio <contact@josevalerio.com>
2023-03-26 15:20:33 -0400

    Axios

--- packages/is-even/index.ts
+++ packages/is-even/index.ts
@@ -1,1 +1,11 @@
-export const isEven = (x: number) => x % 2 === 0;
+import axios from 'axios';
+
+export const isEven = (x: number) => {
+  const axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    timeout: 1000,
+    headers: { 'X-Custom-Header': 'foobar' },
+  });
+  console.log(typeof axiosInstance);
+  return x % 2 === 0;
+};
