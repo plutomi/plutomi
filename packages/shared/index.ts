22e7ad7c23b8e994f9e7550b3adec11a06910399
Jose Valerio <contact@josevalerio.com>
2023-03-26 18:40:05 -0400

    basic setup

---
+++ packages/shared/index.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export type Result = {
+  name: string;
+  y?: string;
+};
+
+export const api = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/vnd.github.v3+json',
+  },
+});
---
+++ packages/web/pages/api/hello.ts
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import { Result } from '@/../shared' // ! TODO Add rule to not import across packages
+import { Result } from '@plutomi/shared';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Result>
+) {
+  res.status(200).json({ name: 'John De' });
+}
