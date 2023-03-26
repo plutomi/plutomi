6cb22ae92d82957fae37d931ccafc257d4dc0736
Jose Valerio <contact@josevalerio.com>
2023-03-26 01:31:52 -0400

    init web

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -1,6 +1,4 @@
-import {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -11,5 +9,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
--- packages/web/pages/api/hello.ts
+++ packages/web/pages/api/hello.ts
@@ -1,10 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { a } from '@plutomi/shared';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ name: string }>
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: a });
+  res.status(200).json({ name: 'John Doe' })
 }
