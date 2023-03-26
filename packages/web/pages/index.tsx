48ea7f53bbb8a968f5063207ee02e1bd69435fa8
Jose Valerio <contact@josevalerio.com>
2023-03-26 01:48:10 -0400

    nx test

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Get started by editing&nbsp; -
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
@@ -26,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +119,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
