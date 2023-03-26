0aef644f544863d0c77859088f353aaf7c93e855
Jose Valerio <contact@josevalerio.com>
2023-03-25 20:50:10 -0400

    saving

--- packages/web/pages/api/hello.ts
+++ packages/web/pages/api/hello.ts
@@ -1,9 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { a } from '@plutomi/shared';
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ name: string }>
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json({ name: a });
 }
