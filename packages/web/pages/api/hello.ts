07cd3c361ee533ddcdee8481fdd2b5a96ee9bcfc
Jose Valerio <contact@josevalerio.com>
2023-03-26 19:44:11 -0400

    more eslint rules

--- packages/web/pages/api/hello.ts
+++ packages/web/pages/api/hello.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // import { Result } from '@/../shared' // ! TODO Add rule to not import across packages
 // import { Result } from '@plutomi/shared';
-import { Result } from "@/../shared";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Result } from "@plutomi/shared";
 
 export default function handler(
   req: NextApiRequest,
