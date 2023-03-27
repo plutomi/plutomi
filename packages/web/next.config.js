51784cc6716092d3e6a5e47adfe58ffe367c28a6
Jose Valerio <contact@josevalerio.com>
2023-03-26 21:50:22 -0400

    reset, using yarn workspaces

--- packages/web/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // ! TODO: Add standalone mode
-};
-
-module.exports = nextConfig;
--- packages/web/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
-}
