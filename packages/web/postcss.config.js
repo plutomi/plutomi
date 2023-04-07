a471f2b9639dd1dfcede467599246dad73fe69cb
Jose Valerio <contact@josevalerio.com>
2023-04-07 00:40:33 -0400

    sving

--- packages/web/postcss.config.js
+++ packages/web/postcss.config.js
@@ -1,6 +1,8 @@
+const { join } = require("path");
+
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: { config: join(__dirname, "tailwind.config.js") },
     autoprefixer: {},
   },
 };
--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const { join } = require("path");
+
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    join(__dirname, "./app/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "./pages/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "./components/**/*.{js,ts,jsx,tsx}"),
   ],
   theme: {
     extend: {},
