757a6c11e47ad2ea7999d63d115fd82eeee1c5b7
Jose Valerio <contact@josevalerio.com>
2023-04-13 21:28:25 -0400

    Fixed up linter issues in web and api

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+export const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
