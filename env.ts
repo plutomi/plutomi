4be4a83b1530428f1e6a8ec001565668d18451c5
Jose Valerio <contact@josevalerio.com>
2023-04-15 16:04:54 -0400

    Parsed schema

--- env.ts
+++ env.ts
@@ -5,15 +5,15 @@ dotenv.config();
 
 const defaultPort = 3000;
 const desiredDomain = "plutomi.com";
-const deploymentEnvironments = ["prod", "stage", "dev"] as const;
+const deploymentEnvironments = ["prod", "stage"] as const;
 const nodeEnv = ["development", "production"] as const;
 
 /**
- * All environment variables in the app. Each package then Picks the ones it needs.
+ * All environment variables in the app. Each package then picks the ones it needs.
  * The reason we do this is so that we can have a single source of truth for all env vars, and that
- * place is in infra package when things get built / deployed.
+ * place is in infra package when things get built / deployed. Locally, we can use .env files.
  */
-export const sharedEnvSchema = z
+const sharedEnvSchema = z
   .object({
     PORT: z.coerce
       .number()
@@ -22,7 +22,7 @@ export const sharedEnvSchema = z
       .gte(1024)
       .lte(65535)
       .default(defaultPort),
-    NODE_ENV: z.enum(nodeEnv),
+    NODE_ENV: z.enum(nodeEnv).default("development"),
     DEPLOYMENT_ENVIRONMENT: z.enum(deploymentEnvironments)
   })
   .transform((env) => {
@@ -33,3 +33,16 @@ export const sharedEnvSchema = z
 
     return { ...env, BASE_URL, DOMAIN, API_URL };
   });
+
+const parsed = sharedEnvSchema.safeParse(process.env);
+
+if (!parsed.success) {
+  parsed.error.issues.forEach((issue) => {
+    console.error("\n❌ Invalid environment variable:");
+    console.error(issue);
+  });
+
+  process.exit(1);
+}
+
+export const env = parsed.data;
