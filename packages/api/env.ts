8a7fc7141e88049f0442e05f8957f81295d0923e
Jose Valerio <contact@josevalerio.com>
2023-04-15 02:46:17 -0400

    Verified linter is working

--- packages/api/env.ts
+++ packages/api/env.ts
@@ -5,8 +5,6 @@ import { z } from "zod";
 
 dotenv.config();
 
-const x = "";
-
 const schema = z.object({
   PORT: z.coerce.number().int().positive().gte(1024).lte(65535),
   NODE_ENV: z.enum(["development", "production"])
