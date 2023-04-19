a38a2deb5d3db7f53f2a68e2a14ddb20e8f3f20e
Jose Valerio <contact@josevalerio.com>
2023-04-18 23:55:53 -0400

    Cleanup env

--- packages/api/env.ts
+++ packages/api/env.ts
@@ -1,25 +1,7 @@
-/* eslint no-console: 0 */
+import { allEnvVariables } from "@plutomi/infra";
 
-import * as dotenv from "dotenv";
-import { z } from "zod";
-
-dotenv.config();
-
-const schema = z.object({
-  PORT: z.coerce.number().int().positive().gte(1024).lte(65535),
-  NODE_ENV: z.enum(["development", "production"])
+export const apiEnvSchema = allEnvVariables.pick({
+  PORT: true,
+  NODE_ENV: true
+  // Add any other environment variables needed for API
 });
-
-const parsed = schema.safeParse(process.env);
-
-if (!parsed.success) {
-  parsed.error.issues.forEach((issue) => {
-    console.error("\n❌ Invalid environment variable:");
-    console.error(issue);
-  });
-
-  process.exit(1);
-}
-
-export const env = parsed.data;
-export type EnvironmentVariables = typeof env;
