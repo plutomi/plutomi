7055d043f352dfd270fc2684231c152db66f3b6c
Jose Valerio <contact@josevalerio.com>
2023-04-21 01:59:19 -0400

    Setup env package

--- packages/infra/bin/infra.ts
+++ packages/infra/bin/infra.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support";
 import * as cdk from "aws-cdk-lib";
-import { env } from "../env";
+import { env } from "@plutomi/env";
 import { PlutomiStack } from "../lib/plutomiStack";
 
 const plutomiApp = new cdk.App();
