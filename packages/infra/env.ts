72d7f9706d3edee5bfd2b706f3a0543da5c6e431
Jose Valerio <contact@josevalerio.com>
2023-04-22 16:20:08 -0400

    infra dotenv

--- packages/infra/env.ts
+++ packages/infra/env.ts
@@ -1,4 +1,7 @@
+import * as dotenv from "dotenv";
 import { allEnvVariablesSchema, parseEnv } from "@plutomi/env";
+
+dotenv.config();
 
 export const env = parseEnv({
   envSchema: allEnvVariablesSchema,
