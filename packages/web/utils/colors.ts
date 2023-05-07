15a7043c5971724571ec5fb817dc7682d91d8252
Jose Valerio <contact@josevalerio.com>
2023-05-07 20:22:25 +0300

    Creating utils and styling colors

---
+++ packages/web/utils/colors.ts
@@ -0,0 +1,10 @@
+// Custom Colors
+export const colors = {
+  success: "#12c924",
+  warning: "#ff7b00",
+  error: "#ff3336",
+  info: "#2d8cef",
+  bgBase: "#ffffff",
+  textBase: "#141415",
+  primary: "#1e4aa2"
+};
