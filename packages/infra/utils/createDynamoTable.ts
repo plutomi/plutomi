12deabfe2e0b93e78d12a9cc5bbc02773582f5f7
Jose Valerio <contact@josevalerio.com>
2023-05-13 15:29:11 +0300

    Dynamo permissions

--- packages/infra/utils/createDynamoTable.ts
+++ packages/infra/utils/createDynamoTable.ts
@@ -1,14 +1,19 @@
 import { RemovalPolicy, type Stack } from "aws-cdk-lib";
 import { BillingMode, Table, AttributeType } from "aws-cdk-lib/aws-dynamodb";
+import type { IRole } from "aws-cdk-lib/aws-iam";
 import { env } from "../env";
 
 type CreateDynamoTableProps = {
   stack: Stack;
+  taskRole: IRole;
 };
 
 const tableName = `${env.DEPLOYMENT_ENVIRONMENT}-plutomi-wait-list`;
 
-export const createDynamoTable = ({ stack }: CreateDynamoTableProps) => {
+export const createDynamoTable = ({
+  stack,
+  taskRole
+}: CreateDynamoTableProps) => {
   const table = new Table(stack, tableName, {
     tableName,
     partitionKey: { name: "PK", type: AttributeType.STRING },
@@ -16,5 +21,8 @@ export const createDynamoTable = ({ stack }: CreateDynamoTableProps) => {
     billingMode: BillingMode.PAY_PER_REQUEST,
     removalPolicy: RemovalPolicy.DESTROY
   });
+
+  table.grantWriteData(taskRole);
+
   return table;
 };
