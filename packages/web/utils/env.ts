11683344971b1708c664a35c46ec85ef3ceb30f7
Jose Valerio <contact@josevalerio.com>
2023-05-15 00:07:39 +0300

    should throw on env

--- packages/web/utils/env.ts
+++ packages/web/utils/env.ts
@@ -2,5 +2,7 @@ import { webEnvSchema, parseEnv, SchemaEnvironment } from "@plutomi/env";
 
 export const env = parseEnv({
   envSchema: webEnvSchema,
-  schemaEnvironment: SchemaEnvironment.WEB
+  schemaEnvironment: SchemaEnvironment.WEB,
+  // When building, NextJS will throw an error if we don't have these env vars set.
+  shouldThrow: false
 });
