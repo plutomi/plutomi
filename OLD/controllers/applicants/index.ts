32c15c0f49a2663637c8b00597346e928bb16629
Jose Valerio <contact@josevalerio.com>
2023-05-22 03:34:17 +0300

    more deletion

--- OLD/controllers/applicants/index.ts
+++
@@ -1,5 +0,0 @@
-// import { getApplicantById } from './getApplicantById';
-// import { getApplicantsInStage } from './getApplicantsInStage';
-// import { createApplicant } from './createApplicant';
-
-// export { createApplicant, getApplicantsInStage, getApplicantById };
