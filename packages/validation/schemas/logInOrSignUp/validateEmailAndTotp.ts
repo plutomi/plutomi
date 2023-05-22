40690321a7e0f230a26d882715a25731f8bb7896
Jose Valerio <contact@josevalerio.com>
2023-05-22 03:44:52 +0300

    Cleaned up uneeded code

--- packages/validation/schemas/logInOrSignUp/validateEmailAndTotp.ts
+++ packages/validation/schemas/logInOrSignUp/validateEmailAndTotp.ts
@@ -1,12 +1,7 @@
-import type { z } from "zod";
 import { baseSchema as totpBaseSchema } from "./totpCode";
 import { baseSchema as emailBaseSchema } from "./email";
 
 // When validating the code, we need to send the email as well as the code
 const baseSchema = emailBaseSchema.merge(totpBaseSchema);
 
-export const UISchema = baseSchema;
-export type UIValues = z.infer<typeof UISchema>;
-
 export const APISchema = baseSchema;
-export type APIValues = z.infer<typeof APISchema>;
