4d8c7549cd29e6695ae266cf39b4e0cdcb971c2e
Jose Valerio <contact@josevalerio.com>
2023-05-22 04:07:58 +0300

    Cleanup

--- packages/api/controllers/index.ts
+++ packages/api/controllers/index.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { waitList } from "./waitList";
 import { health } from "./health";
 import { fourOhFour } from "./404";
 import { totpCodes } from "./totpCodes";
+import { waitList } from "./waitList";
 
 const API = Router();
 
