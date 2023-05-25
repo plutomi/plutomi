03bad356ab5f2c4ff17217a70908a6b68eddd062
Jose Valerio <contact@josevalerio.com>
2023-05-26 00:46:54 +0300

    Added with session to users/get endpoint

--- packages/api/controllers/users/me/get.ts
+++ packages/api/controllers/users/me/get.ts
@@ -1,12 +1,5 @@
 import type { RequestHandler } from "express";
 
 export const get: RequestHandler = async (req, res) => {
-  try {
-  } catch (error) {
-    // ! TODO: Logging
-
-    await req.items.findOne({
-      _id: req.user._id
-    });
-  }
+  res.send(req.user);
 };
--- packages/api/controllers/users/me/index.ts
+++ packages/api/controllers/users/me/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { get } from "./get";
+import { withSession } from "../../../middleware";
 
 export const me = Router();
 
-me.get("/", get);
+me.get("/", withSession, get);
