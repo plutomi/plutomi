a6cd27cf803336b5b9316e9ded740b7169d19f81
Jose Valerio <contact@josevalerio.com>
2023-05-25 21:08:28 +0300

    Custom alphabet for sessions

--- packages/api/utils/generatePlutomiId.ts
+++ packages/api/utils/generatePlutomiId.ts
@@ -1,6 +1,6 @@
 import { AllEntityNames, type PlutomiId } from "@plutomi/shared";
 import ksuid from "ksuid";
-import { nanoid } from "nanoid";
+import { customAlphabet } from "nanoid";
 
 type GenerateIdProps<T extends AllEntityNames> = {
   date: Date;
@@ -11,12 +11,15 @@ export const generatePlutomiId = <T extends AllEntityNames>({
   date,
   entity
 }: GenerateIdProps<T>): PlutomiId<T> => {
-  let id = ksuid.randomSync(date).string;
+  // Do not include a timestamp or -_ in the sessionId
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const nanoid = customAlphabet(characters, 50);
 
-  if (entity === AllEntityNames.SESSION) {
-    // Do not include a timestamp in the session id
-    // TODO Custom alphabet no _ or -
-    id = nanoid(50);
-  }
+  const id =
+    entity === AllEntityNames.SESSION
+      ? nanoid()
+      : ksuid.randomSync(date).string;
+
   return `${entity}_${id}`;
 };
