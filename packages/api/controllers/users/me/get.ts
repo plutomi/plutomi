8b148221d9d8dec2cb7c6057dbbd01f5adc32cfd
Jose Valerio <contact@josevalerio.com>
2023-05-26 18:00:00 +0300

    Saving

--- packages/api/controllers/users/me/get.ts
+++ packages/api/controllers/users/me/get.ts
@@ -1,7 +1,5 @@
 import type { RequestHandler } from "express";
 
 export const get: RequestHandler = async (req, res) => {
-  res.status(400).json({ message: "test" });
-  return;
   res.send(req.user);
 };
