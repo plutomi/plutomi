7059a3c73e0b126bfd628854a5bdd5de8393e8d5
Jose Valerio <contact@josevalerio.com>
2023-05-29 13:05:29 +0300

    Cleaned up waitlist user

--- packages/shared/@types/entities/waitListUser.ts
+++ packages/shared/@types/entities/waitListUser.ts
@@ -1,12 +1,12 @@
 import type { Email } from "../email";
-import type { RelatedToType, RelatedToArray } from "../indexableProperties";
+import type { RelatedToArray } from "../indexableProperties";
 import type { IdPrefix } from "./idPrefix";
 import type { BaseEntity } from "./baseEntity";
 
 type WaitListUserRelatedToArray = [
-  ...RelatedToArray<IdPrefix.WAIT_LIST_USER>,
+  ...RelatedToArray<IdPrefix.WAIT_LIST_USER>
   // Get a user by email
-  { id: Email; type: RelatedToType.WAIT_LIST_USERS }
+  // { id: Email; type: RelatedToType.WAIT_LIST_USERS }
 ];
 
 export type WaitListUser = BaseEntity<IdPrefix.WAIT_LIST_USER> & {
