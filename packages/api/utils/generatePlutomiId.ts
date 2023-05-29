1b0fde7b1ccfbf8d476a9d9ceba90c48b9b662df
Jose Valerio <contact@josevalerio.com>
2023-05-29 12:38:47 +0300

    Sessions

--- packages/api/utils/generatePlutomiId.ts
+++ packages/api/utils/generatePlutomiId.ts
@@ -11,12 +11,12 @@ export const generatePlutomiId = <T extends IdPrefix>({
   date,
   entity
 }: GenerateIdProps<T>): PlutomiId<T> => {
-  // Do not include a timestamp or -_ in the sessionId
   const characters =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   const nanoid = customAlphabet(characters, 50);
 
   const id =
+    // Do not include a timestamp or -_ in the sessionId
     entity === IdPrefix.SESSION ? nanoid() : ksuid.randomSync(date).string;
 
   return `${entity}_${id}`;
