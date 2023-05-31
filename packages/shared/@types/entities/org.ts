78727564e4143b3b44d530c3537b18e0de9298e6
Jose Valerio <contact@josevalerio.com>
2023-05-31 22:56:12 +0300

    Cleanup

--- packages/shared/@types/entities/org.ts
+++ packages/shared/@types/entities/org.ts
@@ -6,11 +6,7 @@ import type { BaseEntity } from "./baseEntity";
  * An org is the parent of all workspaces and users, and therefore the parent of all memberships.
  */
 
-type MembershipRelatedToArray = [
-  ...RelatedToArray<IdPrefix.ORG>
-  // Get all members of an org
-  //   { id: PlutomiId<IdPrefix.ORG>; type: RelatedToType.MEMBERSHIPS }
-];
+type MembershipRelatedToArray = [...RelatedToArray<IdPrefix.ORG>];
 
 export type Org = BaseEntity<IdPrefix.ORG> & {
   createdBy: string;
