f51d76f43134c87e49d8b94b8037f6401f47d171
Jose Valerio <contact@josevalerio.com>
2023-06-01 15:29:02 +0300

    Cleanup

--- packages/shared/@types/sessionStatus.ts
+++ packages/shared/@types/sessionStatus.ts
@@ -3,5 +3,7 @@ export enum SessionStatus {
   LOGGED_OUT = "LOGGED_OUT",
   EXPIRED = "EXPIRED",
   REVOKED = "REVOKED",
+  // This doesn't necessarily invalidate the session, but it does invalidate the user's ability to use the session.
+  // They are granted a new one when they create a new workspace.
   SWITCHED_WORKSPACE = "SWITCHED_WORKSPACE"
 }
