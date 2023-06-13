ebe6bcc258887f9cf1db70b8205846955b53fab6
Jose Valerio <contact@josevalerio.com>
2023-06-13 03:25:46 +0300

    files

--- packages/database/utils/index.ts
+++ packages/database/utils/index.ts
@@ -173,12 +173,12 @@ export const load = async () => {
           ]
         };
 
-        const stagesPerApplication = [];
         for (
           let stages = 0;
           stages < randomNumberInclusive(3, 30);
           stages += 1
         ) {
+          const applicantsInStage = [];
           const stageId = `stage_${nanoid()}`;
 
           const newStage = {
@@ -197,12 +197,125 @@ export const load = async () => {
               { id: org._id, type: "stage" }
             ]
           };
-          stagesPerApplication.push(newStage);
+
+          applicationsAndStages.push(newStage);
+          // ! TODO: Add applicants here
+
+          const applicantsPerStage = randomNumberInclusive(1000, 100000);
+
+          for (
+            let applicantsI = 0;
+            applicantsI < applicantsPerStage;
+            applicantsI += 1
+          ) {
+            const applicantData = [];
+
+            const applicantId = `applicant_${nanoid()}`;
+            const applicant = {
+              _id: applicantId,
+              name: faker.person.fullName(),
+              email: faker.internet.email(),
+              entityType: "applicant",
+              org: workspace.org,
+              workspace: workspace._id,
+              application: applicationId,
+              stage: stageId,
+              metadata: {
+                vehicle: faker.vehicle.vehicle(),
+                date_of_birth: faker.date.past(),
+                phone_number: faker.phone.number(),
+                tags: Array.from({ length: 5 }, () => faker.word.adjective()),
+                address: faker.location.streetAddress(),
+                city: faker.location.city(),
+                state: faker.location.state(),
+                zip_code: faker.location.zipCode(),
+                country: faker.location.country(),
+                description: faker.lorem.paragraph(),
+                avatar: faker.image.avatar(),
+                website: faker.internet.url(),
+                long_description: faker.lorem.paragraphs({ min: 2, max: 100 }),
+                files: Array.from(
+                  { length: randomNumberInclusive(3, 10) },
+                  () => faker.system.fileName()
+                ),
+                notes: Array.from(
+                  { length: randomNumberInclusive(3, 10) },
+                  () => faker.lorem.paragraph()
+                )
+              },
+              relatedTo: [
+                { id: applicantId, type: "self" },
+                { id: "applicant", type: "entity" },
+                { id: workspace._id, type: "applicant" },
+                { id: applicationId, type: "applicant" },
+                { id: stageId, type: "applicant" },
+                { id: org._id, type: "applicant" }
+              ]
+            };
+
+            const filesForApplicant = [];
+            // Create files
+            for (let i = 0; i < randomNumberInclusive(1, 10); i += 1) {
+              const fileId = `file_${nanoid()}`;
+              const file = {
+                _id: fileId,
+                entityType: "file",
+                name: faker.system.fileName(),
+                applicant: applicantId,
+                org: org._id,
+                workspace: workspace._id,
+                relatedTo: [
+                  { id: fileId, type: "self" },
+                  { id: "file", type: "entity" },
+                  { id: applicantId, type: "file" },
+                  { id: workspace._id, type: "file" },
+                  { id: org._id, type: "file" }
+                ]
+              };
+
+              filesForApplicant.push(file);
+            }
+
+            const notesForApplicant = [];
+            // Create note
+            for (let i = 0; i < randomNumberInclusive(1, 5); i += 1) {
+              const noteId = `note_${nanoid()}`;
+              const note = {
+                _id: noteId,
+                entityType: "note",
+                applicant: applicantId,
+                org: org._id,
+                workspace: workspace._id,
+                relatedTo: [
+                  { id: noteId, type: "self" },
+                  { id: "note", type: "entity" },
+                  { id: applicantId, type: "note" },
+                  { id: workspace._id, type: "note" },
+                  { id: org._id, type: "note" }
+                ]
+              };
+
+              notesForApplicant.push(note);
+            }
+
+            applicantData.push([
+              applicant,
+              ...notesForApplicant,
+              ...filesForApplicant
+            ]);
+
+            try {
+              // @ts-expect-error yeah
+              // eslint-disable-next-line no-await-in-loop
+              await items.insertMany(applicantData);
+              console.log(`inserting applicant`);
+            } catch (error) {
+              console.error(`ERROR INSERTIN GAPPLINGATNS ${error}`);
+            }
+          }
         }
 
-        applicationsAndStages.push(
-          ...[newApplication, ...stagesPerApplication]
-        );
+        applicationsAndStages.push(newApplication);
       }
 
       // @ts-expect-error yeah
@@ -235,120 +348,6 @@ export const load = async () => {
       }
       // @ts-expect-error yeah
       await items.insertMany(webhooksPerWorkspace);
-
-      const totalBatches = randomNumberInclusive(10, 10);
-      console.log(`Total batches: ${totalBatches}`);
-      for (let batchI = 0; batchI < totalBatches; batchI += 1) {
-        console.log(`Batch I ${batchI}`);
-        const applicantsInThisBatch = [];
-        const filesInBatch = [];
-        const notesInBatch = [];
-        const applicantsPerBatch = randomNumberInclusive(10000, 10000);
-        for (
-          let applicantsI = 0;
-          applicantsI < applicantsPerBatch;
-          applicantsI += 1
-        ) {
-          const applicantId = `applicant_${nanoid()}`;
-          const applicant = {
-            _id: applicantId,
-            name: faker.person.fullName(),
-            email: faker.internet.email(),
-            entityType: "applicant",
-            org: workspace.org,
-            workspace: workspace._id,
-            metadata: {
-              vehicle: faker.vehicle.vehicle(),
-              date_of_birth: faker.date.past(),
-              phone_number: faker.phone.number(),
-              tags: Array.from({ length: 5 }, () => faker.word.adjective()),
-              address: faker.location.streetAddress(),
-              city: faker.location.city(),
-              state: faker.location.state(),
-              zip_code: faker.location.zipCode(),
-              country: faker.location.country(),
-              description: faker.lorem.paragraph(),
-              avatar: faker.image.avatar(),
-              website: faker.internet.url(),
-              long_description: faker.lorem.paragraphs({ min: 2, max: 100 }),
-              files: Array.from({ length: randomNumberInclusive(3, 10) }, () =>
-                faker.system.fileName()
-              ),
-              notes: Array.from({ length: randomNumberInclusive(3, 10) }, () =>
-                faker.lorem.paragraph()
-              )
-            },
-            relatedTo: [
-              { id: applicantId, type: "self" },
-              { id: "applicant", type: "entity" },
-              { id: workspace._id, type: "applicant" },
-              { id: org._id, type: "applicant" }
-            ]
-          };
-
-          const filesForApplicant = [];
-          // Create files
-          for (let i = 0; i < randomNumberInclusive(1, 10); i += 1) {
-            const fileId = `file_${nanoid()}`;
-            const file = {
-              _id: fileId,
-              entityType: "file",
-              name: faker.system.fileName(),
-              applicant: applicantId,
-              org: org._id,
-              workspace: workspace._id,
-              relatedTo: [
-                { id: fileId, type: "self" },
-                { id: "file", type: "entity" },
-                { id: applicantId, type: "file" },
-                { id: workspace._id, type: "file" },
-                { id: org._id, type: "file" }
-              ]
-            };
-
-            filesForApplicant.push(file);
-          }
-
-          const notesForApplicant = [];
-          // Create note
-          for (let i = 0; i < randomNumberInclusive(1, 5); i += 1) {
-            const noteId = `note_${nanoid()}`;
-            const note = {
-              _id: noteId,
-              entityType: "note",
-              applicant: applicantId,
-              org: org._id,
-              workspace: workspace._id,
-              relatedTo: [
-                { id: noteId, type: "self" },
-                { id: "note", type: "entity" },
-                { id: applicantId, type: "note" },
-                { id: workspace._id, type: "note" },
-                { id: org._id, type: "note" }
-              ]
-            };
-
-            notesForApplicant.push(note);
-          }
-
-          // eslint-disable-next-line no-await-in-loop
-          applicantsInThisBatch.push(applicant);
-          notesInBatch.push(...notesForApplicant);
-          filesInBatch.push(...filesForApplicant);
-        }
-
-        try {
-          // @ts-expect-error yeah
-          // eslint-disable-next-line no-await-in-loop
-          await items.insertMany([
-            ...applicantsInThisBatch,
-            ...notesInBatch,
-            ...filesInBatch
-          ]);
-        } catch (error) {
-          console.error(`ERROR INSERTIN GAPPLINGATNS ${error}`);
-        }
-      }
     }
   }
 };
