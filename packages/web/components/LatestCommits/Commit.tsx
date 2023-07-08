e94ea93285cdbc29412a102d7fb0a29c8f95bb31
Jose Valerio <contact@josevalerio.com>
2023-07-08 17:42:37 -0400

    Saving

--- packages/web/components/LatestCommits/Commit.tsx
+++ packages/web/components/LatestCommits/Commit.tsx
@@ -16,43 +16,49 @@ export const Commit: React.FC<CommitType> = ({
   name,
   message,
   image,
-  date,
-  email
+  date
 }) => {
   const handleCardClick = () => {
-    window.open(url, "_blank");
+    window.open(url, "_blank", "norel noreferrer");
   };
 
   // TODO add lines changed
   return (
-    <div className="group p-2 bg-white w-full max-w-3xl rounded-md shadow-md hover:shadow-lg transition ease-in-out duration-150 hover:scale-102 cursor-pointer">
-      <div className="flex items-center">
-        <div className="shrink-0">
-          <img
-            className="inline-block h-16 w-16 md:h-24 md:w-24 rounded-lg "
-            src={image}
-            alt={`@${username} GitHub avatar`}
-          />
-        </div>
-        <div className="px-3 max-w-2xl">
-          <p className="text-slate-500 text-sm">
-            {String(new Date(date).toLocaleDateString())}
-          </p>
-          <p className="text-lg font-semibold text-slate-700 ">
-            {name} -{" "}
-            <a
-              target="_blank"
-              href={`https://github.com/${username}`}
-              rel="noopener noreferrer"
-              className="transition ease-in-out duration-200 text-blue-500 hover:text-blue-600 hover:underline "
-            >
-              @{username}
-            </a>
-          </p>
+ 
+      <div className="group p-2 bg-white w-full max-w-3xl rounded-md shadow-md hover:shadow-lg transition ease-in-out duration-150 hover:scale-102 cursor-pointer" onClick={handleCardClick}>
+        <div className="flex items-center">
+          <div className="shrink-0">
+            <img
+              className="inline-block h-16 w-16 md:h-24 md:w-24 rounded-lg "
+              src={image}
+              alt={`@${username} GitHub avatar`}
+            />
+          </div>
+          <div className="px-3 max-w-2xl">
+            <p className="text-slate-500 text-sm">
+              {String(new Date(date).toLocaleDateString())}
+            </p>
+            <p className="text-lg font-semibold text-slate-700 ">
+              {name} -{" "}
+              <span
+                onClick={(e) => {
+                  e.stopPropagation();
+                  window.open(
+                    `https://github.com/${username}`,
+                    "_blank",
+                    "noopener noreferrer"
+                  );
+                }}
+                className="transition ease-in-out duration-200 text-blue-500 hover:text-blue-600 hover:underline "
+              >
+                @{username}
+              </span>
+            </p>
 
-          <p className="text-slate-500 line-clamp-2">{message}</p>
+            <p className="text-slate-500 line-clamp-2">{message}</p>
+          </div>
+          a
         </div>
       </div>
-    </div>
   );
 };
