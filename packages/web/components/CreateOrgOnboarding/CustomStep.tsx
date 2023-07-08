4ba2b73a7f85129fc199e32e41a2a4d12d269fd7
Jose Valerio <contact@josevalerio.com>
2023-07-08 17:26:26 -0400

    Cleanup file

--- packages/web/components/CreateOrgOnboarding/CustomStep.tsx
+++ packages/web/components/CreateOrgOnboarding/CustomStep.tsx
@@ -1,37 +1,14 @@
-import { Button, TextInput, Flex } from "@mantine/core";
+import { TextInput, Flex } from "@mantine/core";
 
-const FormItem = ({ children, setStep, form }) => {
-  const handleClick = () => {
-    if (form.validate().hasErrors) {
-      console.log(`FORM HAS ERRORS`);
-      console.log(JSON.stringify(form.errors, null, 2));
-      return;
-    }
-    console.log("Setting next");
-    setStep((prev) => prev + 1);
-  };
-
-  return (
-    <div>
-      {children}
-      <Button onClick={handleClick}>Next CUSTOM</Button>
-    </div>
-  );
-};
-
-export const CustomStep = ({ form, setStep }: { form: any; setStep: any }) => {
-  const x = "";
-  return (
-    <FormItem form={form} setStep={setStep}>
-      <Flex>
-        <TextInput
-          required
-          label="Name"
-          name="name"
-          placeholder="e.g. Stephen King"
-          {...form.getInputProps("name")}
-        />
-      </Flex>
-    </FormItem>
-  );
-};
+// TODO: Remove
+export const CustomStep = ({ form }: { form: any }) => (
+  <Flex>
+    <TextInput
+      required
+      label="Name"
+      name="name"
+      placeholder="e.g. Stephen King"
+      {...form.getInputProps("name")}
+    />
+  </Flex>
+);
