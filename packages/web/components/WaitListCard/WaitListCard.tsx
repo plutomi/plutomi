9da2a4ae44c31e465537f3fb9cafff7e15d14479
Jose Valerio <contact@josevalerio.com>
2023-07-08 01:41:58 -0400

    Saving input

--- packages/web/components/WaitListCard/WaitListCard.tsx
+++ packages/web/components/WaitListCard/WaitListCard.tsx
@@ -89,7 +89,21 @@ export const WaitListCard: React.FC = () => {
             </p>
 
             {subscribe.isSuccess ? (
-              <p className="text-green-400">YEEEHAW</p>
+              <div className="rounded-md bg-green-50 p-4">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    {/* <CheckCircleIcon
+                      className="h-5 w-5 text-green-400"
+                      aria-hidden="true"
+                    /> */}
+                  </div>
+                  <div className="ml-3">
+                    <p className="text-sm font-medium text-green-800">
+                      Awesome, you&apos;ve been added to our waitlist! 🚀
+                    </p>
+                  </div>
+                </div>
+              </div>
             ) : (
               <>
                 <form
