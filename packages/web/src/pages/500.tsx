0eaa44a748d35d63535bef368657f3e3c8fc2935
Jose Valerio <contact@josevalerio.com>
2023-09-04 17:45:18 -0400

    Custom resource code

--- packages/web/src/pages/500.tsx
+++ packages/web/src/pages/500.tsx
@@ -16,7 +16,7 @@ export const FiveHundred: NextPage = () => (
           href="/"
           className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
         >
-          Go back home
+          Go back home :)
         </Link>
       </div>
     </div>
