7af0c3159b1531625538a466767ea5522215a698
Jose Valerio <contact@josevalerio.com>
2023-10-15 08:56:47 -0400

    edge

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   experimental: {
     // https://github.com/vercel/next.js/issues/13045
-    externalDir: true
+    externalDir: true,
+    runtime: "edge"
   },
   images: {
     remotePatterns: [
