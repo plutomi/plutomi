c795e6ec2a672eb3d88b3c8ddf378d3c14d6eefc
Jose Valerio <contact@josevalerio.com>
2023-10-15 23:28:44 -0400

    Saving

--- packages/api/main.go
+++ packages/api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 	"time"
@@ -18,14 +17,6 @@ func main() {
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(message)
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
