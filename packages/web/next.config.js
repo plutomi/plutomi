a1f664046ee393a64a85d8179e90b6a50382ddda
Jose Valerio <contact@josevalerio.com>
2023-10-21 13:57:22 -0400

    fallback

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api(.*)",
-        destination: "/this-is-not-the-api-url-you-are-looking-for",
+        source: "/api/(.*)",
+        destination: "/api-fallback",
       },
     ];
   },
