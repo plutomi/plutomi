a2c60ab8b7dd7b6960602c867eb543ae95032e03
Jose Valerio <contact@josevalerio.com>
2023-10-21 11:02:47 -0400

    edge

--- plutomi-cf/next.config.js
+++ plutomi-cf/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    runtime: "experimental-edge"
-  }
+  reactStrictMode: true
+  // experimental: {
+  //   runtime: "experimental-edge"
+  // }
 };
 
 module.exports = nextConfig;
--- plutomi-cf/src/pages/server.tsx
+++ plutomi-cf/src/pages/server.tsx
@@ -1,6 +1,6 @@
 
 
-export const runtime = 'edge';
+export const runtime = 'experimental-edge';
 
 
 
