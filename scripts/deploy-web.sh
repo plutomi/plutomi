758a083eefc3f6d4b3e8e49913ad7f8c550ecb20
Jose Valerio <contact@josevalerio.com>
2023-10-21 15:03:44 -0400

    staging

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Ensure an environment argument is provided
-if [ -z "$1" ]; then
-  echo "Please provide an environment name: 'staging' or 'production'"
-  exit 1
-fi
-
 # Navigate to the WEB directory
 cd packages/web
 
 # Run the npm deploy command with the provided environment
-ENV=$1 npm run pages:deploy
+un pages:deploy
