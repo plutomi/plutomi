b5f0cddc7585d439c21edc4345b772e56c530a5d
Jose Valerio <contact@josevalerio.com>
2023-10-21 14:14:29 -0400

    Some cleanup

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "githubusercontent.com",
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
         source: "/api/(.*)",
+        // Show a custom page when a user navigates to the API route for NextJS
         destination: "/api-fallback",
       },
     ];
