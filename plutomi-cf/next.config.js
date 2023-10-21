7ff4cd8e9498e6f5d2dd90535be78897e3e29229
Jose Valerio <contact@josevalerio.com>
2023-10-21 11:00:03 -0400

    experimental

--- plutomi-cf/next.config.js
+++ plutomi-cf/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    runtime: "experimental-edge"
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
