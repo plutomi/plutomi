b52db5ec7625d04f057c1f985f0718751a4b3094
Jose Valerio <contact@josevalerio.com>
2023-10-22 21:43:31 -0400

    Init cdk ses

---
+++ packages/aws/test/email.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Email from '../lib/email-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/email-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Email.EmailStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
