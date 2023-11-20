14fdb17bf8af2b11fee222a4b88f0e8ef9edc9a3
Jose Valerio <contact@josevalerio.com>
2023-11-19 23:24:21 -0500

    basic

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -1,68 +1,13 @@
 use axum::{routing::get, Router};
 
 pub mod utils;
-use crate::utils::ksuid::{KsuidMs, KsuidLike};
-
-
-use std::time::{SystemTime, UNIX_EPOCH};
-use time::{Duration, OffsetDateTime};
-
-#[derive(Debug)]
-enum Entities {
-    Org,
-}
-
-impl Entities {
-    fn to_lowercase(&self) -> String {
-        format!("{:?}", self).to_lowercase()
-    }
-}
-fn current_time() -> i64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_millis() as i64
-}
-
-fn create_plutomi_id(entity: Entities, timestamp_ms: Option<i64>) -> String {
-    let timestamp_ms = match timestamp_ms {
-        Some(timestamp_ms) => timestamp_ms,
-        None => current_time(),
-    };
-
-    let timestamp_seconds = timestamp_ms / 1000; // Convert milliseconds to seconds
-    let additional_nanos = (timestamp_ms % 1000) * 1_000_000; // Convert the remainder to nanoseconds
-
-    let timestamp = OffsetDateTime::from_unix_timestamp(timestamp_seconds as i64)
-        .expect("Invalid timestamp")
-        + Duration::nanoseconds(additional_nanos as i64);
-
-    format!(
-        "{}_{}",
-        entity.to_lowercase(),
-        KsuidMs::new(Some(timestamp), None)
-    )
-}
-
-
 
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { 
-        
-        // let x = create_plutomi_id(Entities::Org, None);
-        // "Hello from rust!"
-
-        let y = KsuidMs::new(None, None);
-
-    // for i in 0..1000000 {
-    //     let y = KsuidMs::new(None, None);
-    //     println!("Hello from rust 2! {} {}", y, y.timestamp());
-    //  }
+   
     
-    
-        // Return hello from rust and x concatenated
         format!("Hello from rust prod:D")
     }
     ));
--- packages/api/src/utils/ksuid.rs
+++ packages/api/src/utils/ksuid.rs
@@ -1,197 +1,57 @@
-/**
- * A lot of this code is from https://github.com/svix/rust-ksuid - MIT Licensed
- *
- * This code removes the regular 1 second implementation and only uses the ms implementation.
- * It also changes the ms granularity from 4ms to 1ms.
- */
-use core::fmt;
-use std::{error, str::FromStr};
-
 use byteorder::{BigEndian, ByteOrder};
+use rand::Rng;
 use time::OffsetDateTime;
 
-#[cfg(feature = "serde")]
-use serde::de::{self, Deserialize, Deserializer, Visitor};
-#[cfg(feature = "serde")]
-use serde::ser::{Serialize, Serializer};
+// Add the appropriate crate for base62 encoding here
+use base62; // Make sure this crate is included in your Cargo.toml
 
-pub const KSUID_EPOCH: i64 = 1_400_000_000;
-
+const CUSTOM_EPOCH: i64 = 1_600_000_000;
+const TOTAL_BYTES: usize = 20;
+const TIMESTAMP_BYTES: usize = 6;
+const PAYLOAD_BYTES: usize = TOTAL_BYTES - TIMESTAMP_BYTES;
 const BASE_62_CHARS: &[u8; 62] = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-const TOTAL_BYTES: usize = 20;
-
-#[derive(Debug)]
-pub struct Error(String);
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.0.fmt(f)
-    }
+enum EntityType {
+    User,
+    Org,
 }
 
-impl error::Error for Error {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        None
-    }
-}
-
-fn timestamp_millis(dt: &OffsetDateTime) -> i64 {
-    (dt.unix_timestamp_nanos() / 1_000_000) as i64
-}
-
-pub trait KsuidLike {
-    /// The type of the Ksuid struct beind implemented
-    type Type;
-
-    /// The number of bytes used for timestamp (`TIMESTAMP_BYTES + PAYLOAD_BYTES == 20`)
-    const TIMESTAMP_BYTES: usize;
-    /// The number of bytes used for payload (`TIMESTAMP_BYTES + PAYLOAD_BYTES == 20`)
-    const PAYLOAD_BYTES: usize;
-
-    fn new(timestamp: Option<OffsetDateTime>, payload: Option<&[u8]>) -> Self::Type;
-
-    fn from_seconds(timestamp: Option<i64>, payload: Option<&[u8]>) -> Self::Type;
-
-    fn timestamp(&self) -> OffsetDateTime;
-
-    fn timestamp_seconds(&self) -> i64 {
-        self.timestamp().unix_timestamp()
-    }
-
-    fn payload(&self) -> &[u8] {
-        &self.bytes()[Self::TIMESTAMP_BYTES..]
-    }
-
-    fn from_bytes(bytes: [u8; TOTAL_BYTES]) -> Self::Type;
-
-    fn bytes(&self) -> &[u8; TOTAL_BYTES];
-
-    fn to_base62(&self) -> String {
-        format!(
-            "{:0>27}",
-            base_encode::to_string(self.bytes(), 62, BASE_62_CHARS).unwrap()
-        )
-    }
-
-    fn from_base62(s: &str) -> Result<Self::Type, Error> {
-        if let Some(loaded) = base_encode::from_str(s, 62, BASE_62_CHARS) {
-            // Get the last TOTAL_BYTES
-            let loaded = if loaded.len() > TOTAL_BYTES {
-                &loaded[loaded.len() - TOTAL_BYTES..]
-            } else {
-                &loaded[..]
-            };
-            let mut buf = [0u8; TOTAL_BYTES];
-            if loaded.len() != TOTAL_BYTES {
-                Err(Error(format!(
-                    "Got ksuid of unexpected length {}",
-                    loaded.len()
-                )))
-            } else {
-                buf.copy_from_slice(loaded);
-                Ok(Self::from_bytes(buf))
-            }
-        } else {
-            Err(Error("Failed to decode".to_owned()))
+impl EntityType {
+    fn prefix(&self) -> &str {
+        match self {
+            EntityType::User => "user_",
+            EntityType::Org => "org_",
         }
     }
 }
 
-#[derive(Debug, PartialOrd, Ord, Clone, Copy, PartialEq, Eq)]
-pub struct KsuidMs([u8; TOTAL_BYTES]);
-impl KsuidMs {
-    const U64_BYTES: usize = 8;
-
-    pub fn new_raw(timestamp: u64, payload: Option<&[u8]>) -> Self {
-        let mut buf = [0u8; TOTAL_BYTES];
-        let mut timestamp_buf = [0u8; Self::U64_BYTES];
-        BigEndian::write_u64(&mut timestamp_buf, timestamp);
-        // We only want the TIMESTAMP_BYTES least significant bytes
-        // OLD:
-        // buf[..Self::TIMESTAMP_BYTES].copy_from_slice(
-        //     &timestamp_buf[Self::U64_BYTES - Self::TIMESTAMP_BYTES..Self::U64_BYTES],
-        // );
-        buf[..Self::TIMESTAMP_BYTES]
-            .copy_from_slice(&timestamp_buf[Self::U64_BYTES - Self::TIMESTAMP_BYTES..]);
-        if let Some(payload) = payload {
-            buf[Self::TIMESTAMP_BYTES..].copy_from_slice(payload);
-        } else {
-            getrandom::getrandom(&mut buf[Self::TIMESTAMP_BYTES..]).unwrap();
-        }
-        Self::from_bytes(buf)
-    }
-
-    pub fn from_millis(timestamp: Option<i64>, payload: Option<&[u8]>) -> Self {
-        let timestamp_ms =
-            timestamp.unwrap_or_else(|| timestamp_millis(&OffsetDateTime::now_utc()));
-        let timestamp_s = (timestamp_ms / 1_000) - KSUID_EPOCH;
-        let timestamp_ms = timestamp_ms % 1_000;
-        // OLD:
-        // let timestamp: i64 = ((timestamp_s << 8) & 0xFFFFFFFF00) | timestamp_ms;
-        let timestamp = (timestamp_s << 10) | timestamp_ms;
-
-        Self::new_raw(timestamp as u64, payload)
-    }
-
-    pub fn timestamp_millis(&self) -> i64 {
-        timestamp_millis(&self.timestamp())
-    }
-
-    pub fn timestamp_raw(&self) -> u64 {
-        // Remove two bytes from the result (as we are only u48, not u64, and then mask the result)
-        // OLD:
-        // BigEndian::read_u64(self.bytes()) >> ((Self::U64_BYTES - Self::TIMESTAMP_BYTES) * 8)
-        BigEndian::read_u64(self.bytes()) >> ((Self::U64_BYTES - Self::TIMESTAMP_BYTES) * 8)
-    }
+fn timestamp_millis() -> u64 {
+    let now = OffsetDateTime::now_utc();
+    let timestamp_s = (now.unix_timestamp() - CUSTOM_EPOCH) as u64;
+    let timestamp_ms = now.millisecond() as u64;
+    (timestamp_s << 10) | timestamp_ms // 38 bits for seconds, 10 bits for milliseconds
 }
 
-impl KsuidLike for KsuidMs {
-    type Type = KsuidMs;
-    const TIMESTAMP_BYTES: usize = 6;
-    const PAYLOAD_BYTES: usize = 14;
+pub fn generate_ksuid(entity_type: EntityType) -> String {
+    let mut rng = rand::thread_rng();
+    let mut ksuid = [0u8; TOTAL_BYTES];
+    
+    // Timestamp (6 bytes)
+    BigEndian::write_u48(&mut ksuid[0..TIMESTAMP_BYTES], timestamp_millis());
 
-    fn new(timestamp: Option<OffsetDateTime>, payload: Option<&[u8]>) -> Self {
-        let timestamp = timestamp.map(|x| timestamp_millis(&x));
-        Self::from_millis(timestamp, payload)
-    }
+    // Random payload (14 bytes)
+    rng.fill(&mut ksuid[TIMESTAMP_BYTES..]);
 
-    fn from_seconds(timestamp: Option<i64>, payload: Option<&[u8]>) -> Self {
-        let timestamp = timestamp.map(|x| x * 1_000);
-        Self::from_millis(timestamp, payload)
-    }
+    // Base62 encoding
+    let base62_encoded = base62::encode(&ksuid);
 
-    fn from_bytes(bytes: [u8; TOTAL_BYTES]) -> Self {
-        Self(bytes)
-    }
-
-    fn bytes(&self) -> &[u8; TOTAL_BYTES] {
-        &self.0
-    }
-
-    fn timestamp(&self) -> OffsetDateTime {
-        let timestamp = self.timestamp_raw() as i64;
-        // OLD:
-        // let seconds = ((timestamp >> 8) + KSUID_EPOCH) as i128;
-        // let ns = (1_000_000 * (((timestamp & 0xFF) << 2) % 1_000)) as i128;
-
-        let seconds = ((timestamp >> 10) + KSUID_EPOCH) as i128;
-        let ns = (1_000_000 * (timestamp & 0x3FF)) as i128;
-
-        OffsetDateTime::from_unix_timestamp_nanos(seconds * 1_000_000_000 + ns).unwrap()
-    }
+    format!("{}{}", entity_type.prefix(), base62_encoded)
 }
 
-impl FromStr for KsuidMs {
-    type Err = Error;
+fn main() {
+    let user_ksuid = generate_ksuid(EntityType::User);
+    let org_ksuid = generate_ksuid(EntityType::Org);
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Self::from_base62(s)
-    }
-}
-
-impl fmt::Display for KsuidMs {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.to_base62())
-    }
+    println!("User KSUID: {}", user_ksuid);
+    println!("Organization KSUID: {}", org_ksuid);
 }
