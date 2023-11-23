c1d06ed47b6b49e14829a93e5567a84db9f69071
Jose Valerio <contact@josevalerio.com>
2023-11-23 15:33:13 -0500

    env working

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -1,27 +1,20 @@
 use axum::{routing::get, Router};
 use controllers::{health_check::health_check, not_found::not_found};
 use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
 
 mod controllers;
 mod env;
 mod utils;
 
-#[derive(Serialize, Deserialize)]
-struct Person {
-    name: String,
-}
-
 #[tokio::main]
 async fn main() {
-    // Load .env if available
+    // Load .env if available (used in development)
     dotenv().ok();
 
     let app = Router::new().nest(
         "/api",
         Router::new()
             .route("/health", get(health_check))
-            // .nest("/others", Router::new().route("/:id", get(get_user_by_id)))
             .fallback(not_found),
     );
 
