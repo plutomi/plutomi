df3484dc3acbf8f1a2c82c578eb2797780212d54
Jose Valerio <contact@josevalerio.com>
2023-11-24 20:47:38 -0500

    api docs

--- packages/web/src/pages/apiDocs.tsx
+++ packages/web/src/pages/apiDocs.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
-import Link from "next/link";
 
-export const FiveHundred: NextPage = () => (
+export const ApiDocs: NextPage = () => (
   <main className="grid min-h-full place-items-center  px-6 py-24 sm:py-32 lg:px-8">
     <div className="text-center">
      API DOCS WILL GO HERE :D
@@ -9,4 +8,4 @@ export const FiveHundred: NextPage = () => (
   </main>
 );
 
-export default FiveHundred;
+export default ApiDocs;
