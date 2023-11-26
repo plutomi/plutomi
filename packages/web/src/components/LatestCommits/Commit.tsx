a124ed3e0e143043cbc9758f27e211d9e2e87988
Jose Valerio <contact@josevalerio.com>
2023-11-26 14:59:31 -0500

    saving

--- packages/web/src/components/LatestCommits/Commit.tsx
+++ packages/web/src/components/LatestCommits/Commit.tsx
@@ -9,6 +9,7 @@ export type CommitType = {
   email: string;
   message: string;
 };
+
 export const Commit: React.FC<CommitType> = ({
   url,
   username,
@@ -17,10 +18,18 @@ export const Commit: React.FC<CommitType> = ({
   image,
   date
 }) => {
+  const handleCardClick = () => {
+    window.open(url, "_blank", "noopener noreferrer");
+  };
+
+  // TODO add lines changed
   return (
-    <div
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
       className="p-2 bg-white w-full max-w-3xl rounded-md shadow-md hover:shadow-lg transition ease-in-out duration-150 hover:scale-102 cursor-pointer"
-      onClick={() => window.open(url, "_blank", "noopener noreferrer")}
+      onClick={handleCardClick}
     >
       <div className="flex items-center">
         <div className="shrink-0">
@@ -36,19 +45,24 @@ export const Commit: React.FC<CommitType> = ({
           </p>
           <p className="text-lg font-semibold text-slate-700 ">
             {name} -{" "}
-            <a
-              href={`https://github.com/${username}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition ease-in-out duration-200 text-blue-500 hover:text-blue-600 hover:underline"
-              onClick={(e) => e.stopPropagation()}
+            <span
+              role="presentation"
+              onClick={(e) => {
+                e.stopPropagation();
+                window.open(
+                  `https://github.com/${username}`,
+                  "_blank",
+                  "noopener noreferrer"
+                );
+              }}
+              className="transition ease-in-out duration-200 text-blue-500 hover:text-blue-600 hover:underline "
             >
               @{username}
-            </a>
+            </span>
           </p>
           <p className="text-slate-500 line-clamp-2">{message}</p>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
