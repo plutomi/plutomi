83dbccd3f1ed991bd67578427b78110ea8f06b15
Jose Valerio <contact@josevalerio.com>
2023-12-13 16:53:08 +0300

    Sample here

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -1,14 +1,19 @@
 use axum::{
+    http::Request,
     routing::{get, post},
     Extension, Router,
 };
-
 use controllers::{create_totp, health_check, not_found};
+use core::panic;
 use dotenv::dotenv;
 use std::sync::Arc;
-use tower_http::{compression::CompressionLayer, timeout::TimeoutLayer, BoxError};
-use tracing::{info, instrument};
+use tower::ServiceBuilder;
+use tower_http::{
+    compression::CompressionLayer, timeout::TimeoutLayer, trace::TraceLayer, BoxError,
+};
+use tracing::{debug, event, info, instrument, Level, Span};
 use tracing_subscriber::prelude::*;
+
 use utils::mongodb::connect_to_mongodb;
 mod controllers;
 mod entities;
@@ -27,6 +32,18 @@ async fn main() {
     // Connect to database
     let mongodb = Arc::new(connect_to_mongodb().await);
 
+    let axiom_layer = tracing_axiom::builder()
+        .with_service_name("fmt")
+        .with_dataset("plutomi-logs".to_string())
+        .with_token("XXXX".to_string())
+        .layer()
+        .unwrap();
+    let fmt_layer = tracing_subscriber::fmt::layer().pretty();
+    tracing_subscriber::registry()
+        .with(fmt_layer.compact())
+        .with(axiom_layer)
+        .init();
+
     // Routes
     let totp_routes = Router::new().route("/totp", post(create_totp));
 
@@ -36,26 +53,47 @@ async fn main() {
             .merge(totp_routes)
             .route("/health", get(health_check))
             .fallback(not_found)
-            .layer(Extension(mongodb))
-            .layer(CompressionLayer::new())
-            .layer(TimeoutLayer::new(std::time::Duration::from_secs(5))),
+            .layer(
+                ServiceBuilder::new()
+                    // https://docs.rs/axum/latest/axum/middleware/index.html#ordering
+                    .layer(TraceLayer::new_for_http().on_request(
+                        |request: &Request<_>, span: &Span| {
+                            // debug!("TEST LOG {} {}", request.method(), request.uri(),);
+                            log();
+                        },
+                    ))
+                    .layer(TimeoutLayer::new(std::time::Duration::from_secs(5)))
+                    .layer(CompressionLayer::new())
+                    .layer(Extension(mongodb)),
+            ),
     );
 
     // Bind address
     let addr = match "[::]:8080".parse::<std::net::SocketAddr>() {
         Ok(addr) => addr,
-        Err(_) => {
+        Err(e) => {
             // TODO: Log error
-            panic!("Error parsing address");
+            panic!("Error parsing address: {}", e);
+        }
+    };
+
+    // Bind listener
+    let listener = match tokio::net::TcpListener::bind(&addr).await {
+        Ok(listener) => listener,
+        Err(e) => {
+            // TODO: Log error
+            panic!("Error binding address: {}", e);
         }
     };
 
     // Start the server
-    match axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-    {
-        Ok(_) => println!("Listening on {}", &addr),
+    match axum::serve(listener, app).await {
+        Ok(_) => {
+            println!("Listening on {}", &addr);
+            log();
+            log();
+            log();
+        }
         Err(e) => {
             // TODO: Log error
             panic!("Error binding address: {}", e)
