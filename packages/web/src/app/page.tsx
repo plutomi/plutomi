369f0ba15dab5d96f6dc89bb9ec2bfe7676d864b
Jose Valerio <contact@josevalerio.com>
2023-12-13 04:28:24 +0300

    some paddings

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -6,14 +6,20 @@ import { UseCaseSegment } from "@/components/UseCases/UseCaseSegment";
 export default function Page() {
   return (
     <div className="w-full h-full flex justify-center">
-      <div className="flex flex-col my-32  items-center space-y-4 ">
+      <div className="py-32">
         <LandingHero />
-        <>
+        <div className="p-6 lg:p-0 lg:pt-4 w-full  flex flex-col items-center space-y-4">
           <UseCaseSegment />
-          <UseCaseCards />
-          <LatestCommits />
-        </>
-        <HomepageFooter />
+
+          <div className="w-full ">
+            <UseCaseCards />
+          </div>
+
+          <div className="pt-8 flex flex-col items-center space-y-4">
+            <LatestCommits />
+            <HomepageFooter />
+          </div>
+        </div>
       </div>
     </div>
   );
