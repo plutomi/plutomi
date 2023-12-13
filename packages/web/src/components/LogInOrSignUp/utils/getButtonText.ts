a1a0d4a4063c3975b4ca0a17a750e596f5c49ec2
Jose Valerio <contact@josevalerio.com>
2023-12-13 04:03:53 +0300

    btn txt

---
+++ packages/web/src/components/LogInOrSignUp/utils/getButtonText.ts
@@ -0,0 +1,29 @@
+import type { AuthContext } from "../../../hooks";
+
+type GetButtonTextProps = {
+  codeIsSending: boolean;
+  codeIsVerifying: boolean;
+  authContext: AuthContext;
+  step: number;
+};
+
+export const getButtonText = ({
+  codeIsSending,
+  codeIsVerifying,
+  step,
+  authContext
+}: GetButtonTextProps) => {
+  if (step === 1) {
+    return codeIsSending ? "Sending..." : "Send";
+  }
+
+  if (step === 2) {
+    if (codeIsVerifying) {
+      return authContext === "login" ? "Logging in..." : "Signing up...";
+    }
+    return authContext === "login" ? "Log in" : "Sign up";
+  }
+
+  // This should never be triggered, but just in case
+  return "Continue";
+};
