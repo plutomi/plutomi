f408257504203a8454da72858b4b94225ee1dbda
jlittlewood-scottlogic <jlittlewood@scottlogic.com>
2024-01-11 15:16:46 +0000

    updated to be rem instead of px

--- packages/web/src/components/UseCases/UseCaseSegment.tsx
+++ packages/web/src/components/UseCases/UseCaseSegment.tsx
@@ -6,14 +6,14 @@ export const UseCaseSegment: React.FC = () => {
 
   const isSelected = (title: UseCase) => useCase === title;
   return (
-    <div className=" flex justify-center flex-col lg:flex-row text-center space-x-0 lg:space-x-1 space-y-1 lg:space-y-0 rounded-lg bg-slate-200  p-1 shadow">
+    <div className=" flex justify-center flex-col lg:flex-row text-center space-x-0 lg:space-x-1 space-y-1 lg:space-y-0 rounded-[0.675rem] bg-slate-200  p-1 shadow">
       {Object.values(UseCase).map((title) => (
         <button
           key={title}
           onClick={() => {
             setUseCase(title);
           }}
-          className={`rounded-[0.375rem] py-3 px-4 text-lg text-slate-500 font-medium  ${
+          className={`rounded-md py-3 px-4 text-lg text-slate-500 font-medium  ${
             isSelected(title)
               ? "bg-white shadow-md text-slate-700 "
               : "hover:bg-slate-100 transition ease-in-out duration-100"
