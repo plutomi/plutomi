0dbe3efb509edbe5b7758a66b0722acab78f3215
Jose Valerio <contact@josevalerio.com>
2024-02-17 20:02:04 -0500

    AWS cleanup

--- packages/aws/lib/plutomi-stack.ts
+++ packages/aws/lib/plutomi-stack.ts
@@ -1,57 +1,16 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { configureEmails } from "./configureEmails";
-import { createVpc } from "./createVpc";
-import { createTaskRole } from "./createTaskRole";
-import { createTaskDefinition } from "./createTaskDefinition";
-import { createFargateService } from "./createFargateService";
 import { env } from "../utils/env";
-import { createEventBus } from "./createEventBus";
-import { createEventsConsumer } from "./createPlutomiEventsConsumer";
-import { createEmailEventsConsumer } from "./createEmailEventsConsumer";
-import { disableCloudWatchLogging } from "./disableLogging";
+import { configureSES } from "./configureSES";
 
 export class PlutomiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const eventBus = createEventBus({ stack: this });
-    const { vpc, natGatewayProvider } = createVpc({ stack: this });
-
-    const { emailEventsConsumer, configurationSet } = createEmailEventsConsumer(
-      {
-        stack: this,
-        eventBus,
-        vpc,
-      }
-    );
-
-    configureEmails({
+    configureSES({
       stack: this,
-      configurationSet,
     });
 
-    const taskRole = createTaskRole({ stack: this });
-    const taskDefinition = createTaskDefinition({ stack: this, taskRole });
-
-    const plutomiEventsConsumer = createEventsConsumer({
-      stack: this,
-      eventBus,
-      vpc,
-    });
-
-    const fargateService = createFargateService({
-      stack: this,
-      taskDefinition,
-      vpc,
-      natGatewayProvider,
-      eventBus,
-    });
-
-    disableCloudWatchLogging({
-      fargateService,
-      emailEventsConsumer,
-      plutomiEventsConsumer,
-    });
+    // ! TODO: S3 bucket
   }
 }
