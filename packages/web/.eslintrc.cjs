b8e97a57a2e79452d8fc3fd0563ab8d4d3547fee
Jose Valerio <contact@josevalerio.com>
2024-02-20 00:10:39 -0500

    more stuff

--- packages/web/.eslintrc.cjs
+++ packages/web/.eslintrc.cjs
@@ -14,14 +14,53 @@ module.exports = {
     es6: true
   },
 
+  
   // Base config
-  extends: ["eslint:recommended"],
-
+  extends: ["next/core-web-vitals",
+  "plugin:eslint-plugin-next-on-pages/recommended",
+  "eslint:recommended",
+  "eslint-plugin-react/recommended",
+  "eslint-config-standard-with-typescript",
+  "airbnb",
+  "airbnb-typescript/base",
+  "prettier",
+  "plugin:eslint-plugin-no-template-curly-in-string-fix"
+],
+  rules: {
+    "@typescript-eslint/no-floating-promises": "error",
+    "quotes": [2, "double", { "avoidEscape": true }],
+    "comma-dangle": ["error", "never"],
+    "no-console": "warn",
+    "react/jsx-props-no-spreading": "off",
+    "max-params": ["error", 3],
+    "prefer-destructuring": ["error", { "object": true, "array": false }],
+    "no-implicit-globals": ["error", { "lexicalBindings": true }],
+    "no-lonely-if": "error",
+    "import/order": "error",
+    "sort-imports": "off",
+    "no-void": ["off"],
+    "prefer-template": "error",
+    "curly": ["error", "all"],
+    "react/display-name": "off",
+    "@typescript-eslint/dot-notation": "error",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "no-undef-init": "error",
+    "import/no-default-export": "off",
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "react/function-component-definition": [
+      2,
+      {
+        "namedComponents": "arrow-function",
+        "unnamedComponents": "arrow-function"
+      }
+    ]
+  },
   overrides: [
     // React
     {
       files: ["**/*.{js,jsx,ts,tsx}"],
-      plugins: ["react", "jsx-a11y"],
+      plugins: ["react", "jsx-a11y", "eslint-plugin-next-on-pages", "@typescript-eslint"],
       extends: [
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
@@ -43,6 +82,7 @@ module.exports = {
       }
     },
 
+    
     // Typescript
     {
       files: ["**/*.{ts,tsx}"],
