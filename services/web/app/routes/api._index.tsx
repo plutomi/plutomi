e67d2c4bc1848056938c30c237ef6e70ecddbe1e
Jose Valerio <contact@josevalerio.com>
2024-06-12 19:22:58 -0400

    hot reloading on web

--- services/web/app/routes/api._index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Docs() {
-  <h1>We should never hit this directly</h1>;
-}
--- services/web/app/utils/env.ts
+++ services/web/app/utils/env.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dir = path.resolve(__dirname, "../../../.env");
+const dir = path.resolve(__dirname, "../../../../.env");
 
 dotenv.config({ path: dir });
 
