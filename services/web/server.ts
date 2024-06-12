7a248ff1876f3aeced44476210fc0db43ac6140a
Jose Valerio <contact@josevalerio.com>
2024-06-12 00:49:26 -0400

    server web

--- services/web/server.ts
+++ services/web/server.ts
@@ -1,30 +1,12 @@
-
-
 import { createRequestHandler } from "@remix-run/express";
 import { broadcastDevReady } from "@remix-run/node";
 import express from "express";
 import * as build from "./build/index.js";
 import { env } from "utils/env.js";
-const environment = process.env.ENVIRONMENT || "development";
 
 const app = express();
 app.use(express.static("public"));
 app.set("trust proxy", true);
-
-app.get("/ips", (req, res) => {
-  // TODO remove
-  const clientIP = req.connection.remoteAddress;
-  const forwardedFor = req.headers["x-forwarded-for"];
-  const socket = req.socket.remoteAddress;
-
-  res.json({
-    clientIP,
-    forwardedFor,
-    socket,
-    headers: req.headers
-  });
-  return;
-});
 
 const redirectToDocs = [
   "/api",
@@ -35,15 +17,13 @@ const redirectToDocs = [
 ];
 app.use(async (req, res, next) => {
   if (redirectToDocs.includes(req.path)) {
-    // TODO do the same thing in API - only thing used in the web app is the first one since /api/ trailing slash gets stopped at Traefik
-    return res.redirect(301, `${env.BASE_WEB_URL}/docs/api`);
+    return res.redirect(308, `${env.BASE_WEB_URL}/docs/api`);
   }
   next();
 });
 
 app.get("/health", async (req, res) => {
-  res.json({ status: "ok" });
-  return;
+  return res.json({ status: "ok" });
 });
 
 app.all(
@@ -63,12 +43,9 @@ const server = app.listen(3000, "0.0.0.0", () => {
 });
 
 const gracefulShutdown = async () => {
-  console.log("Closing server gracefully.", new Date().toISOString());
   server.close(() => {
     console.log("HTTP server closed.", new Date().toISOString());
   });
-
-  // Delay for 3 seconds to allow for cleanup
 
   // Wait for existing connections to close
   await new Promise((resolve) => setTimeout(resolve, 8000));
