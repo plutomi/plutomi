8c9adbb7ef205cd90ac8e8ec5d8030b8a01ed9af
Jose Valerio <contact@josevalerio.com>
2024-06-12 17:14:13 -0400

    added some colors

--- services/web/app/components/HomepageFooter/HomepageFooter.tsx
+++ services/web/app/components/HomepageFooter/HomepageFooter.tsx
@@ -1,7 +1,7 @@
 import EmailButton from "./EmailButton";
 
 export const HomepageFooter: React.FC = () => (
-  <footer className="bg-white rounded-[0.75rem] shadow max-w-2xl  py-2 px-3 flex w-full items-center justify-between">
+  <footer className="bg-white rounded-[0.75rem] shadow max-w-2xl  py-2 px-3 space-x-4 flex w-full items-center justify-between">
     <p className="text-lg text-slate-700 font-bold ">Plutomi Inc.</p>
     <EmailButton />
   </footer>
--- services/web/tailwind.config.ts
+++ services/web/tailwind.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        github: {
+          500: "#333"
+        }
+      }
+    }
   },
   plugins: []
 } satisfies Config;
