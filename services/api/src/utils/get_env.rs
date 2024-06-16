e3bb1485d67cfdac968a328a5badfe808b51e20b
Jose Valerio <contact@josevalerio.com>
2024-06-16 16:00:08 -0400

    lgr

--- services/api/src/utils/get_env.rs
+++ services/api/src/utils/get_env.rs
@@ -8,22 +8,43 @@ pub struct Env {
     pub DATABASE_URL: String,
     pub BASE_WEB_URL: String,
     pub ENVIRONMENT: String,
+    pub AXIOM_DATASET: Option<String>,
+    pub AXIOM_TOKEN: Option<String>,
+    pub AXIOM_ORG_ID: Option<String>,
 }
 
-fn get_key_from_env(key: &str) -> String {
-    match env::var(key) {
-        Ok(val) => val,
-        Err(e) => {
-            // TODO: Log error
-            panic!("Error getting key '{}' from env: {}", key, e)
-        }
+impl Env {
+    // Used in logger.rs
+    pub fn axiom_configured(&self) -> bool {
+        self.AXIOM_DATASET.is_some() && self.AXIOM_TOKEN.is_some() && self.AXIOM_ORG_ID.is_some()
     }
+}
+
+// Panic if key is not found in env and no default_value is provided
+fn get_key_from_env(key: &str, default_value: Option<&str>) -> String {
+    env::var(key).unwrap_or_else(|e| {
+        default_value.map(|v| v.to_string()).unwrap_or_else(|| {
+            panic!(
+                "Error getting key '{}' from env: {} and no default_value provided",
+                key, e
+            )
+        })
+    })
+}
+
+// Return None if key is not found in env
+fn get_optional_key_from_env(key: &str) -> Option<String> {
+    env::var(key).ok()
 }
 
 pub fn get_env() -> Env {
     Env {
-        DATABASE_URL: get_key_from_env("DATABASE_URL"),
-        BASE_WEB_URL: get_key_from_env("BASE_WEB_URL"),
-        ENVIRONMENT: get_key_from_env("ENVIRONMENT"),
+        // Required
+        DATABASE_URL: get_key_from_env("DATABASE_URL", None),
+        ENVIRONMENT: get_key_from_env("ENVIRONMENT", Some("development")),
+        BASE_WEB_URL: get_key_from_env("BASE_WEB_URL", Some("http://localhost:3000")),
+        AXIOM_DATASET: get_optional_key_from_env("AXIOM_DATASET"),
+        AXIOM_TOKEN: get_optional_key_from_env("AXIOM_TOKEN"),
+        AXIOM_ORG_ID: get_optional_key_from_env("AXIOM_ORG_ID"),
     }
 }
