50d9ed9d5922ada6a589d34e6857218d8bee7ace
Jose Valerio <contact@josevalerio.com>
2024-09-16 00:29:24 -0400

    u

--- services/api/src/controllers/health_check.rs
+++ services/api/src/controllers/health_check.rs
@@ -2,12 +2,12 @@ use crate::AppState;
 use axum::{extract::State, http::StatusCode, Extension, Json};
 use mongodb::{bson::doc, options::FindOneOptions};
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde_json::json;
 use shared::{
     get_current_time::get_current_time,
     logger::{LogLevel, LogObject},
 };
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 use time::OffsetDateTime;
 
 #[derive(Serialize, Deserialize)]
@@ -20,6 +20,7 @@ pub struct HealthCheckResponse {
 
 pub async fn health_check<'a>(
     State(state): State<Arc<AppState>>,
+    Extension(request_id): Extension<String>,
 ) -> (StatusCode, Json<HealthCheckResponse>) {
     let options: FindOneOptions = {
         let mut options = FindOneOptions::default();
@@ -45,12 +46,15 @@ pub async fn health_check<'a>(
             level: LogLevel::Error,
             _time: get_current_time(OffsetDateTime::now_utc()),
             message: "Failed to connect to database for health check".to_string(),
-            data: None,
+            data: Some(json!({
+                "request_id": request_id,
+            })),
             error: Some(json!(e.to_string())),
             request: None,
             response: None,
         });
     }
+
     let response: HealthCheckResponse = HealthCheckResponse {
         message: "Saul Goodman",
         database,
@@ -59,13 +63,13 @@ pub async fn health_check<'a>(
     };
 
     state.logger.log(LogObject {
-        level: match database {
-            true => LogLevel::Info,
-            false => LogLevel::Error,
-        },
+        level: LogLevel::Info,
         _time: get_current_time(OffsetDateTime::now_utc()),
         message: "Health check response".to_string(),
-        data: None,
+        data: Some(json!({
+            "request_id": request_id,
+            "response": &response,
+        })),
         error: None,
         request: None,
         response: None,
--- services/api/src/controllers/not_found.rs
+++ services/api/src/controllers/not_found.rs
@@ -1,50 +1,46 @@
-use std::sync::Arc;
-
+use crate::structs::{api_error::ApiError, app_state::AppState};
 use axum::{
     extract::{OriginalUri, State},
     http::{Method, StatusCode},
     response::IntoResponse,
     Extension,
 };
-use hyper::HeaderMap;
-use serde_json::{json, Value};
+use serde_json::json;
 use shared::{
     get_current_time::get_current_time,
     logger::{LogLevel, LogObject},
 };
-
-use crate::{
-    structs::{api_error::ApiError, app_state::AppState},
-    utils::get_header_value::get_header_value,
-};
+use std::sync::Arc;
 
 pub async fn not_found(
     OriginalUri(uri): OriginalUri,
     method: Method,
-    headers: HeaderMap,
     Extension(request_id): Extension<String>,
     State(state): State<Arc<AppState>>,
 ) -> impl IntoResponse {
     let not_found_message = format!("Route at: '{} {}' not found", method, uri.path());
 
     let status = StatusCode::NOT_FOUND;
+
+    state.logger.log(LogObject {
+        level: LogLevel::Error,
+        _time: get_current_time(time::OffsetDateTime::now_utc()),
+        message: not_found_message.clone(),
+        data: Some(json!({
+            "request_id": request_id,
+        })),
+        error: None,
+        request: None,
+        response: None,
+    });
+
     let api_error = ApiError {
         message: not_found_message.clone(),
         plutomi_code: None,
         status_code: status.as_u16(),
         docs_url: None,
-        request_id: request_id,
+        request_id,
     };
-
-    state.logger.log(LogObject {
-        level: LogLevel::Error,
-        _time: get_current_time(time::OffsetDateTime::now_utc()),
-        message: not_found_message,
-        data: None,
-        error: None,
-        request: None,
-        response: None,
-    });
 
     api_error.into_response()
 }
