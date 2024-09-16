d59cd9e6c100b0e7786ffd81690b2708ac06be71
Jose Valerio <contact@josevalerio.com>
2024-09-16 00:25:21 -0400

    ah

--- services/api/src/utils/timeout.rs
+++ services/api/src/utils/timeout.rs
@@ -24,18 +24,14 @@ const MAX_REQUEST_DURATION: Duration = Duration::from_secs(10);
 pub async fn timeout(
     State(state): State<Arc<AppState>>,
     request: Request,
-    headers: HeaderMap,
     next: Next,
     Extension(request_id): Extension<String>,
 ) -> impl IntoResponse {
-    let (parts, body) = request.into_parts();
-
     // Call the next middleware and timeout after 10 seconds
     // Send a response if the timeout is hit
     match tokio::time::timeout(MAX_REQUEST_DURATION, next.run(request)).await {
         Ok(response) => response,
         Err(_) => {
-            let status = StatusCode::REQUEST_TIMEOUT;
             let message = "Request took too long to process. Please try again.".to_string();
             state.logger.log(LogObject {
                 level: LogLevel::Error,
@@ -43,26 +39,20 @@ pub async fn timeout(
                 message: message.clone(),
                 data: Some(json!({
                     "request_id": request_id,
-                    "method": parts.method.to_string(),
-                    "path": parts.uri.path(),
-                    "query": parts.uri.query().unwrap_or(""),
-                    "headers": headers,
-                    "body": body::to_bytes(body).await.unwrap_or_default(),
                 })),
                 error: None,
                 request: None,
                 response: None,
             });
 
-            let api_error = ApiError {
+            ApiError {
                 message,
                 request_id,
-                status_code: status.as_u16(),
+                status_code: StatusCode::REQUEST_TIMEOUT.as_u16(),
                 docs_url: None,
                 plutomi_code: None,
-            };
-
-            api_error.into_response()
+            }
+            .into_response()
         }
     }
 }
