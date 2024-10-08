71bfdcafb7f0c3c73e768368a849dbd1496e1d05
Jose Valerio <contact@josevalerio.com>
2024-10-08 00:08:47 -0400

    millionlint

--- services/web/vite.config.ts
+++ services/web/vite.config.ts
@@ -1,16 +1,18 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import MillionLint from "@million/lint";
 
 export default defineConfig({
   plugins: [
+    MillionLint.vite(),
     remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
+        v3_throwAbortReason: true
+      }
     }),
-    tsconfigPaths(),
-  ],
+    tsconfigPaths()
+  ]
 });
