c5acb182f1b2730dacc1ea093db4a691f2687de2
Jose Valerio <contact@josevalerio.com>
2024-10-11 17:39:37 -0400

    startup log

--- services/migrator/src/main.rs
+++ services/migrator/src/main.rs
@@ -1,4 +1,23 @@
-fn main() {
+use shared::{
+    get_current_time::get_current_time,
+    get_env::get_env,
+    logger::{LogLevel, LogObject, Logger, LoggerContext},
+};
+use time::OffsetDateTime;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     dotenvy::dotenv().ok();
-    println!("Running migrations! TBD :)");
+
+    let logger = Logger::init(LoggerContext { caller: "migrator" });
+
+    logger.log(LogObject {
+        level: LogLevel::Info,
+        message: "Running migrations!".to_string(),
+        _time: get_current_time(OffsetDateTime::now_utc()),
+        data: None,
+        error: None,
+        request: None,
+        response: None,
+    });
 }
