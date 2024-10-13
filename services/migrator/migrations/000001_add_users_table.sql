660c0339d20c9a25c6189e0cf0c51a5c85b690ca
Jose Valerio <contact@josevalerio.com>
2024-10-13 03:13:57 -0400

    slight tweaks

--- services/migrator/migrations/000001_add_users_table.sql
+++ services/migrator/migrations/000001_add_users_table.sql
@@ -1,7 +1,12 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(254) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
   -- https://bugs.mysql.com/bug.php?id=103228
   created_at DATETIME NOT NULL,
   updated_at DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS users;
