6c837a261424ad3600d4fe01fb7c2e337ab2d5b8
Jose Valerio <contact@josevalerio.com>
2024-10-13 03:03:23 -0400

    datetime

--- services/migrator/migrations/000001_add_users_table.sql
+++ services/migrator/migrations/000001_add_users_table.sql
@@ -1,7 +1,7 @@
--- Add a users table in mysql
 CREATE TABLE users (
   id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   email VARCHAR(255) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  -- https://bugs.mysql.com/bug.php?id=103228
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
