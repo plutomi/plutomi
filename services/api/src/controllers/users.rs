01f5913b5c2f528e84367cd60b5c78708b2b591b
Jose Valerio <contact@josevalerio.com>
2024-10-13 04:06:03 -0400

    query cache

--- services/api/src/controllers/users.rs
+++ services/api/src/controllers/users.rs
@@ -1,6 +1,8 @@
-use axum::{response::IntoResponse, Json};
+use crate::structs::{api_response::ApiResponse, app_state::AppState};
+use axum::{response::IntoResponse, Extension, Json};
+use hyper::StatusCode;
 use serde::{Deserialize, Serialize};
-use crate::structs::app_state::AppState;
+use serde_json::json;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct NewUser {
@@ -9,10 +11,43 @@ struct NewUser {
     email: String,
 }
 
-pub async fn post_users(state: AppState, Json(user): Json<NewUser>) -> impl IntoResponse {
-
-    // TODO: Implement the post_users controller
-
-    // Return an HTTP 200 response with a JSON body containing the user's ID
-    // and email address
+pub async fn post_users(
+    state: AppState,
+    Json(user): Json<NewUser>,
+    Extension(request_id): Extension<String>,
+) -> impl IntoResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO users (first_name, last_name, email) 
+        VALUES ($1, $2, $3)
+        "#,
+        new_user.first_name,
+        new_user.last_name,
+        new_user.email
+    )
+    .execute(&state.db)
+    .await
+    {
+        Ok(_) => {
+            let message = "User created successfully".to_string();
+            ApiResponse::success(
+                json!({
+                    "message": message,
+                }),
+                request_id.clone(),
+                StatusCode::NOT_FOUND,
+            )
+        }
+        Err(e) => {
+            let message = format!("Failed to insert user: {}", e);
+            ApiResponse::error(
+                message,
+                StatusCode::INTERNAL_SERVER_ERROR,
+                request_id.clone(),
+                Some("TODO add docs. Maybe submit a PR? >.<".to_string()),
+                None,
+                json!({}),
+            )
+        }
+    }
 }
