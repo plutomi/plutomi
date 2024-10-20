582920d102df780e8771a929f40e5dad48914fc4
Jose Valerio <contact@josevalerio.com>
2024-10-20 11:48:55 -0400

    cleaned up healthcheck res

--- services/api/src/controllers/health_check.rs
+++ services/api/src/controllers/health_check.rs
@@ -19,7 +19,7 @@ pub async fn health_check(
     State(state): State<Arc<AppState>>,
     Extension(request_id): Extension<String>,
 ) -> (StatusCode, Json<HealthCheckResponse>) {
-    let (mysql_check, kafka_check) = tokio::join!(
+    let (mysql, kafka) = tokio::join!(
         async {
             sqlx::query!(
                 r#"
@@ -28,70 +28,23 @@ pub async fn health_check(
             )
             .fetch_one(&*state.mysql)
             .await
+            .is_ok()
         },
-        tokio::task::spawn_blocking({
-            let producer = state.kafka.producer.clone();
-            move || {
-                producer
-                    .client()
-                    .fetch_metadata(None, Duration::from_secs(2))
-            }
-        }),
+        async {
+            state
+                .kafka
+                .producer
+                .client()
+                .fetch_metadata(None, Duration::from_secs(2))
+                .ok()
+                .is_some()
+        }
     );
-    let kafka_metadata = kafka_check.ok().and_then(|res| res.ok());
-
-    // Log the health check response and metadata
-    if let Some(metadata) = &kafka_metadata {
-        // Extract brokers
-        let brokers: Vec<_> = metadata
-            .brokers()
-            .into_iter()
-            .map(|broker| {
-                serde_json::json!({
-                    "id": broker.id(),
-                    "host": broker.host(),
-                    "port": broker.port(),
-                })
-            })
-            .collect();
-
-        // Extract topics
-        let topics: Vec<_> = metadata
-            .topics()
-            .into_iter()
-            .map(|topic| {
-                serde_json::json!({
-                    "name": topic.name(),
-                    "partitions": topic.partitions().len(),
-                })
-            })
-            .collect();
-
-        state.logger.info(LogObject {
-            message: "Health check response".to_string(),
-            data: Some(serde_json::json!({
-                "kafka": {
-                    "brokers": brokers,
-                    "topics": topics,
-                },
-            })),
-            ..Default::default()
-        });
-    } else {
-        // Metadata is not available
-        state.logger.info(LogObject {
-            message: "Health check response".to_string(),
-            data: Some(serde_json::json!({
-                "kafka": null,
-            })),
-            ..Default::default()
-        });
-    }
 
     let response: HealthCheckResponse = HealthCheckResponse {
         message: "Saul Goodman",
-        mysql: mysql_check.is_ok(),
-        kafka: kafka_metadata.is_some(),
+        mysql,
+        kafka,
         environment: state.env.ENVIRONMENT.clone(),
         docs_url: format!("{}/docs/api", state.env.BASE_WEB_URL),
     };
