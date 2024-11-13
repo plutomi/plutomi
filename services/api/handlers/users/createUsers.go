f98e9a1de13fba4c8dc4676747128e28e1cfcdbd
Jose Valerio <contact@josevalerio.com>
2024-11-13 01:24:39 -0500

    removed old api

--- services/api/handlers/users/createUsers.go
+++ services/api/handlers/users/createUsers.go
@@ -1,43 +1,4 @@
-package users
-
-import (
-	"encoding/json"
-	"net/http"
-	"plutomi/api/types"
-	ctx "plutomi/shared/context"
-
-	"github.com/go-chi/render"
-)
-
-type DBUser struct {
-	ID       int    `db:"id" json:"id"`
-	Username string `db:"username" json:"username"`
-	Email    string `db:"email" json:"email"`
-}
-
-type CreateUserRequest struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-}
-
-type PlutomiUserCreatedResponse struct {
-	Message string `json:"message"`
-	User    DBUser `json:"user"`
-}
-
-func CreateUsers(w http.ResponseWriter, r *http.Request, ctx *ctx.Context) {
-	var req CreateUserRequest
-
-	// Parse JSON from request body
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		render.Status(r, http.StatusBadRequest)
-		res := types.BasePlutomiResponse{
-			Message: "Invalid request body when creating a user",
-			DocsUrl: "https://plutomi.com/docs/api/users/create",
-		}
-		render.JSON(w, r, res)
-		return
-	}
+x
 
 	// Prepare statement for inserting user into database
 	stmt, err := ctx.MySQL.Preparex("INSERT INTO users (username, email) VALUES (?, ?)")
