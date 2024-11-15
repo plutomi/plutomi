46829118621c6fa54a4eb05afd3abfae0fd23003
Jose Valerio <contact@josevalerio.com>
2024-11-15 01:41:57 -0500

    moved migrator over

---
+++ services/migrator/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	ctx "plutomi/shared/context"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"go.uber.org/zap"
+)
+
+func main() {
+	ctx := ctx.InitContext("migrator")
+	defer ctx.Logger.Sync()
+	defer ctx.MySQL.Close()
+
+	m, err := migrate.New(
+		"file://migrations",
+		fmt.Sprintf("mysql://%s", ctx.Env.MySQLUrl),
+	)
+
+	if err != nil {
+		ctx.Logger.Fatal("Failed to create migration instance", zap.String("error", err.Error()))
+	}
+
+	defer m.Close()
+
+	err = m.Up()
+
+	if err != nil {
+
+		if err.Error() != "no change" {
+			ctx.Logger.Fatal("Failed to run migrations", zap.String("error", err.Error()))
+		}
+	}
+
+	ctx.Logger.Info("Migrations ran successfully")
+}
